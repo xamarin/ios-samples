53fa4bb6e0275dce3e287e3fd46c10135c816b8a
Mike Krüger <mike@Caprica.local>
2011-08-17 08:23:23 +0200

    Added ViewTransitions example.

---
+++ ViewTransitions/ViewTransitions/Classes/ViewTransitionsViewController.designer.cs
@@ -0,0 +1,20 @@
+// WARNING
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace ViewTransitions
+{
+	[Register ("ViewTransitionsViewController")]
+	partial class ViewTransitionsViewController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UIView imageView { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIBarButtonItem nextTransitionButton { get; set; }
+	}
+}
