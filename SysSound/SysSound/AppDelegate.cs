fa4691f2f2bf0cfe59393e48a7848b5a5cf28438
hugoware <hugo@hugoware.net>
2011-09-16 14:14:40 -0500

    Adding SysSound sample ready for MonoTouch

---
+++ SysSound/SysSound/AppDelegate.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System.Drawing;
+
+namespace SysSound {
+	
+	/// <summary>
+	/// The UIApplicationDelegate for the application. This class is responsible for launching the 
+	/// User Interface of the application, as well as listening (and optionally responding) to 
+	/// application events from iOS.
+	/// </summary>
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate {
+		
+		// class-level declarations
+		UIWindow window;
+		SysSoundViewController _SysSoundViewController;
+		
+		/// <summary>
+		/// This method is invoked when the application has loaded and is ready to run. In this 
+		/// method you should instantiate the window, load the UI into it and then make the window
+		/// visible.
+		/// </summary>
+		/// <remarks>
+		/// You have 5 seconds to return from this method, or iOS will terminate your application.
+		/// </remarks>
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options) {
+			
+			// create a new window instance based on the screen size
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			
+			// If you have defined a view, add it here:
+			
+			
+			// window.AddSubview (navigationController.View);
+			this._SysSoundViewController = new SysSoundViewController();
+			this._SysSoundViewController.View.Frame = new RectangleF(0.0f, 20.0f, 320.0f, 460.0f);
+			this.window.AddSubview(this._SysSoundViewController.View);
+			
+			// make the window visible
+			window.MakeKeyAndVisible ();
+			
+			return true;
+		}
+	}
+}
