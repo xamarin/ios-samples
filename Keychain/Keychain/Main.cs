0b8fa336372968362c4b7017f696078312fe0107
Sebastien Pouliot <sebastien@xamarin.com>
2011-10-26 17:04:08 -0400

    remove errors from MD console while executing. Fix bug #1396

--- Keychain/Keychain/Main.cs
+++ Keychain/Keychain/Main.cs
@@ -7,6 +7,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using MonoTouch.Security;
@@ -24,35 +25,43 @@ namespace Keychain
 	// The name AppDelegate is referenced in the MainWindow.xib file.
 	public partial class AppDelegate : UIApplicationDelegate
 	{
+		UIViewController viewController;
+		
 		// This method is invoked when the application has loaded its UI and its ready to run
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
-			var rec = new SecRecord (SecKind.GenericPassword){
-				Generic = NSData.FromString ("foo")
-			};
+			ThreadPool.QueueUserWorkItem (delegate {
+				window.BeginInvokeOnMainThread (delegate {
+					var rec = new SecRecord (SecKind.GenericPassword){
+						Generic = NSData.FromString ("foo")
+					};
 			
-			SecStatusCode res;
-			var match = SecKeyChain.QueryAsRecord (rec, out res);
-			if (res == SecStatusCode.Success)
-				DisplayMessage ("Key found, password is: {0}", match.ValueData);
-			else
-				DisplayMessage ("Key not found: {0}", res);
-			
-			var s = new SecRecord (SecKind.GenericPassword) {
-				Label = "Item Label",
-				Description = "Item description",
-				Account = "Account",
-				Service = "Service",
-				Comment = "Your comment here",
-				ValueData = NSData.FromString ("my-secret-password"),
-				Generic = NSData.FromString ("foo")
-			};
-			
-			var err = SecKeyChain.Add (s);
-			
-			if (err != SecStatusCode.Success && err != SecStatusCode.DuplicateItem)
-				DisplayMessage ("Error adding record: {0}", err);
-			
+					SecStatusCode res;
+					var match = SecKeyChain.QueryAsRecord (rec, out res);
+					if (res == SecStatusCode.Success)
+						DisplayMessage ("Key found, password is: {0}", match.ValueData);
+					else
+						DisplayMessage ("Key not found: {0}", res);
+					
+					var s = new SecRecord (SecKind.GenericPassword) {
+						Label = "Item Label",
+						Description = "Item description",
+						Account = "Account",
+						Service = "Service",
+						Comment = "Your comment here",
+						ValueData = NSData.FromString ("my-secret-password"),
+						Generic = NSData.FromString ("foo")
+					};
+					
+					var err = SecKeyChain.Add (s);
+					
+					if (err != SecStatusCode.Success && err != SecStatusCode.DuplicateItem)
+						DisplayMessage ("Error adding record: {0}", err);
+				});
+			});
+						
+			viewController = new UIViewController ();
+			window.RootViewController = viewController;
 			window.MakeKeyAndVisible ();
 			
 			return true;
