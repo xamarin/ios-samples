d51e298b269e90fe0ec2470acb57788f2e2719e9
Sebastien Pouliot <sebastien@xamarin.com>
2012-01-13 14:30:28 -0500

    Do not dispose of the ABPeoplePickerNavigationController with a using block since it will be needed later

--- MonoCatalog-MonoDevelop/AddressBookController.xib.cs
+++ MonoCatalog-MonoDevelop/AddressBookController.xib.cs
@@ -17,55 +17,59 @@ namespace MonoCatalog
 		{
 		}
 	
-		public override void ViewDidLoad ()
+		protected override void Dispose (bool disposing)
 		{
+			if (p != null) {
+				p.Dispose ();
+				p = null;
+			}
+			base.Dispose (disposing);
 		}
 	
-		public override void ViewWillAppear (bool animated)
+		ABPeoplePickerNavigationController GetPicker ()
 		{
+			if (p != null)
+				return p;
+
+			p = new ABPeoplePickerNavigationController ();
+			p.SelectPerson += (o, e) => {
+				Console.Error.WriteLine ("# select Person: {0}", e.Person);
+				toString.Text   = e.Person.ToString ();
+				firstName.Text  = e.Person.FirstName;
+				lastName.Text   = e.Person.LastName;
+				property.Text   = "";
+				identifier.Text = "";
+				e.Continue = selectProperty.On;
+				if (!e.Continue)
+					DismissModalViewControllerAnimated (true);
+			};
+			p.PerformAction += (o, e) => {
+				Console.Error.WriteLine ("# perform action; person={0}", e.Person);
+				toString.Text   = e.Person.ToString ();
+				firstName.Text  = e.Person.FirstName;
+				lastName.Text   = e.Person.LastName;
+				property.Text   = e.Property.ToString ();
+				identifier.Text = e.Identifier.HasValue ? e.Identifier.ToString () : "";
+				e.Continue = performAction.On;
+				if (!e.Continue)
+					DismissModalViewControllerAnimated (true);
+			};
+			p.Cancelled += (o, e) => {
+				Console.Error.WriteLine ("# select Person cancelled.");
+				toString.Text   = "Cancelled";
+				firstName.Text  = "";
+				lastName.Text   = "";
+				property.Text   = "";
+				identifier.Text = "";
+				DismissModalViewControllerAnimated (true);
+			};
+			return p;
 		}
 	
-		public override void ViewWillDisappear (bool animated)
-		{
-		}
-	
-	  partial void showPicker (MonoTouch.UIKit.UIButton sender)
+		partial void showPicker (MonoTouch.UIKit.UIButton sender)
 		{
 			Console.Error.WriteLine ("# Select Contacts pushed!");
-			using (p = new ABPeoplePickerNavigationController ()) {
-				p.SelectPerson += (o, e) => {
-					Console.Error.WriteLine ("# select Person: {0}", e.Person);
-					toString.Text   = e.Person.ToString ();
-					firstName.Text  = e.Person.FirstName;
-					lastName.Text   = e.Person.LastName;
-					property.Text   = "";
-					identifier.Text = "";
-					e.Continue = selectProperty.On;
-					if (!e.Continue)
-						DismissModalViewControllerAnimated (true);
-				};
-				p.PerformAction += (o, e) => {
-					Console.Error.WriteLine ("# perform action; person={0}", e.Person);
-					toString.Text   = e.Person.ToString ();
-					firstName.Text  = e.Person.FirstName;
-					lastName.Text   = e.Person.LastName;
-					property.Text   = e.Property.ToString ();
-					identifier.Text = e.Identifier.HasValue ? e.Identifier.ToString () : "";
-					e.Continue = performAction.On;
-					if (!e.Continue)
-						DismissModalViewControllerAnimated (true);
-				};
-				p.Cancelled += (o, e) => {
-					Console.Error.WriteLine ("# select Person cancelled.");
-					toString.Text   = "Cancelled";
-					firstName.Text  = "";
-					lastName.Text   = "";
-					property.Text   = "";
-					identifier.Text = "";
-					DismissModalViewControllerAnimated (true);
-				};
-				PresentModalViewController (p, true);
-			}
+			PresentModalViewController (GetPicker (), true);
 		}
 	}
 }
