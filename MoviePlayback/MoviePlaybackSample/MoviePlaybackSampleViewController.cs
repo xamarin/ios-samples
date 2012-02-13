30fb7b826346e1af258c8e17d93b9be861fbe98e
alexrp <xtzgzorex@gmail.com>
2012-02-13 20:46:45 +0100

    Commit the Sound and MoviePlayback samples.

---
+++ MoviePlayback/MoviePlaybackSample/MoviePlaybackSampleViewController.cs
@@ -0,0 +1,76 @@
+using MonoTouch.UIKit;
+using System.Drawing;
+using System;
+using MonoTouch.Foundation;
+using MonoTouch.MediaPlayer;
+
+namespace MoviePlaybackSample
+{
+	public partial class MoviePlaybackSampleViewController : UIViewController
+	{
+		//class level declarations
+		MPMoviePlayerController mp = new MPMoviePlayerController();
+		
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public MoviePlaybackSampleViewController ()
+			: base (UserInterfaceIdiomIsPhone ? "MoviePlaybackSampleViewController_iPhone" : "MoviePlaybackSampleViewController_iPad", null)
+		{
+		}
+	
+		public override void ViewDidLoad ()
+		{
+			
+			base.ViewDidLoad ();
+
+			//Button Events
+			playMovieButton.TouchUpInside += delegate {
+			
+			try{
+				
+				//Set already instantiated MPMoviePlayerController to playback from Movies/file.m4v
+				mp = new MPMoviePlayerController (NSUrl.FromFilename("Movies/file.m4v"));
+				
+				//enable AirPlay
+				mp.AllowsAirPlay = true;
+				
+				//Add the MPMoviePlayerController View
+				this.View.AddSubview(mp.View);
+				
+				//set the view to be full screen and show animated
+				mp.SetFullscreen(true, true);
+			
+				//MPMoviePlayer must be set to PrepareToPlay before playback
+				mp.PrepareToPlay();
+				
+				//Play Movie
+				mp.Play();
+							
+				}catch{
+					
+				Console.WriteLine("There was a problem playing back Video");
+					
+				}		
+				
+			};
+						
+		}
+		
+		public override void ViewDidUnload ()
+		{
+			base.ViewDidUnload ();
+		}
+		
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
+		{
+			// Return true for supported orientations
+			if (UserInterfaceIdiomIsPhone) {
+				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+			} else {
+				return true;
+			}
+		}
+	}
+}
---
+++ Sound/README.md
@@ -0,0 +1,10 @@
+Sound
+=====
+
+Sound is a simple example of how to record an AAC file using the iPhone
+microphone or externally attached recording hardware and then play it back.
+
+Authors
+-------
+
+James Bentley, Bryan Costanich
