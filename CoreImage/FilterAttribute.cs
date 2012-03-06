9205e77862d8d0922cbc62332e4273d9bfe1cbee
Eric Beisecker <eric.beisecker@xamarin.com>
2012-03-06 18:28:07 -0500

    Added CoreImage samples.

---
+++ CoreImage/FilterAttribute.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace coreimage
+{
+	public class FilterAttribute : Attribute
+	{
+		public FilterAttribute ()
+		{
+		}
+	}
+}
+
---
+++ CoreImage/MainViewController.designer.cs
@@ -0,0 +1,22 @@
+// WARNING
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+
+namespace coreimage
+{
+	[Register ("MainViewController")]
+	partial class MainViewController
+	{
+		[Action ("showInfo:")]
+		partial void showInfo (MonoTouch.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
+
