f23a53371a37e35ba72775f69372387293819ad4
Steve Millar <steve.millar@infinitekdev.com>
2012-03-19 18:02:11 -0400

    Added MediaCapture directory containing solution and project files

---
+++ MediaCapture/MediaCapture/AppDelegate.cs
@@ -0,0 +1,38 @@
+//
+// how to capture still images, video and audio using iOS AVFoundation and the AVCAptureSession
+// 
+// This sample handles all of the low-level AVFoundation and capture graph setup required to capture and save media.  This code also exposes the
+// capture, configuration and notification capabilities in a more '.Netish' way of programming.  The client code will not need to deal with threads, delegate classes
+// buffer management, or objective-C data types but instead will create .NET objects and handle standard .NET events.  The underlying iOS concepts and classes are detailed in 
+// the iOS developer online help (TP40010188-CH5-SW2).
+//
+// https://developer.apple.com/library/mac/#documentation/AudioVideo/Conceptual/AVFoundationPG/Articles/04_MediaCapture.html#//apple_ref/doc/uid/TP40010188-CH5-SW2
+//
+// Enhancements, suggestions and bug reports can be sent to steve.millar@infinitekdev.com
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace MediaCapture
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		UIWindow window;
+		private RootViewController rootView = null;
+		
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			rootView = new RootViewController();
+			window.AddSubview( rootView.View );
+			window.MakeKeyAndVisible ();
+			return true;
+		}
+		
+	}
+}
+
