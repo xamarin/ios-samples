af478bf8072e7fb35194e2af930734af8a9e1ee7
Rolf Bjarne Kvinge <rolf@xamarin.com>
2012-05-04 12:51:47 +0200

    Add new sample - CustomInputStream

---
+++ CustomInputStream/AppDelegate.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using MonoTouch.Dialog;
+
+namespace InputStreamTest
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		UIWindow window;
+		DialogViewController dvc;
+		NativeUploader uploader;
+		StyledStringElement status;
+		StyledStringElement upload;
+
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+			
+			dvc = new DialogViewController (new RootElement ("InputStreamTest") 
+			{
+				new Section () {
+					(upload = new StyledStringElement ("Upload", Upload)),
+					(status = new StyledStringElement ("")),
+				}
+			});
+			
+			upload.Alignment = UITextAlignment.Center;
+			status.Alignment = UITextAlignment.Center;
+			
+			window.RootViewController = dvc;
+			window.MakeKeyAndVisible ();
+			
+			return true;
+		}
+		
+		void Upload ()
+		{
+			TcpListener listener = new TcpListener (new IPEndPoint (IPAddress.Loopback, 0));
+			listener.Start ();
+			
+			Console.WriteLine ("Listening on: {0}", listener.LocalEndpoint);
+			
+			uploader = new NativeUploader ();
+			uploader.UploadStream ("http://127.0.0.1:" + ((IPEndPoint) listener.LocalEndpoint).Port.ToString (), 1000, () =>
+			{
+				Console.WriteLine ("Upload completed.");
+			});
+			
+			listener.BeginAcceptSocket ((IAsyncResult res) =>
+			{
+				ThreadPool.QueueUserWorkItem ((v) => 
+				{
+					using (var socket = listener.EndAcceptSocket (res)) {
+						byte [] buffer = new byte[1024];
+						int read;
+						
+						// receive headers
+						read = socket.Receive (buffer);
+						BeginInvokeOnMainThread (() => { status.Caption = "Received headers..."; dvc.ReloadData (); });
+						Console.WriteLine ("\n" + System.Text.ASCIIEncoding.ASCII.GetString (buffer, 0, read));
+						
+						// send 100 Continue
+						socket.Send (System.Text.ASCIIEncoding.ASCII.GetBytes (@"HTTP/1.1 100 Continue\r\n\r\n"));
+						
+						// receive data
+						read = socket.Receive (buffer);
+						BeginInvokeOnMainThread (() => { status.Caption = "Received data"; dvc.ReloadData (); });
+						Console.WriteLine ("\n" + System.Text.ASCIIEncoding.ASCII.GetString (buffer, 0, read));
+						
+						// send 200 OK
+						socket.Send (System.Text.ASCIIEncoding.ASCII.GetBytes (@"HTTP/1.1 200 OK\r\n\r\n"));
+					}
+					
+					listener.Stop ();
+				});
+			}, null);
+		}
+		
+		static void Main (string[] args)
+		{
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+	}
+}
+
---
+++ CustomInputStream/ZInputStream.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Runtime.InteropServices;
+
+using MonoTouch.CoreFoundation;
+using MonoTouch.Foundation;
+
+namespace InputStreamTest
+{
+	[Preserve (AllMembers = true)]
+	public class ZInputStream : NSInputStream
+	{
+		NSStreamStatus status;
+		long read_length;
+		long total_length;
+		
+		public ZInputStream (long total_length) : base ()
+		{
+			status = NSStreamStatus.NotOpen;
+			this.total_length = total_length;
+		}
+		
+		public override NSStreamStatus Status {
+			get {
+				return status;
+			}
+		}
+		
+		public override void Open ()
+		{
+			status = NSStreamStatus.Open;
+			Notify (CFStreamEventType.OpenCompleted);
+		}
+		
+		public override void Close ()
+		{
+			status = NSStreamStatus.Closed;
+		}
+		
+		public override bool HasBytesAvailable ()
+		{
+			return total_length > read_length;
+		}
+	
+		public override int Read (IntPtr buffer, uint len) 
+		{
+			int actual = Math.Min ((int) len, (int) (total_length - read_length));
+			
+			byte [] bytes = new byte [actual];
+			for (int i = 0; i < actual; i++)
+				bytes [i] = (byte) 'z';
+			
+			read_length += actual;
+			
+			Marshal.Copy (bytes, 0, buffer, actual);
+			
+			if (actual == 0)
+				Notify (CFStreamEventType.EndEncountered);
+			
+			return actual;
+		}
+		
+		protected unsafe override bool GetBuffer (out IntPtr buffer, out uint len)
+		{
+			// Just call the base implemention (which will return false)
+			return base.GetBuffer (out buffer, out len);
+		}
+		
+		protected override bool SetCFClientFlags (CFStreamEventType inFlags, IntPtr inCallback, IntPtr inContextPtr)
+		{
+			// Just call the base implementation, which knows how to handle everything.
+			return base.SetCFClientFlags (inFlags, inCallback, inContextPtr);
+		}
+		
+		public override void ScheduleInCFRunLoop (IntPtr runloop, string mode)
+		{
+			Notify (CFStreamEventType.HasBytesAvailable);
+		}
+		
+		public override void UnscheduleInCFRunLoop (IntPtr runloop, string mode)
+		{
+			// Nothing to do here
+		}
+	}
+}
