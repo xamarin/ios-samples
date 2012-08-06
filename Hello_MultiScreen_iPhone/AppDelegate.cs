1a761239cbf4f52666327e5b2ce7e62a4cc0f43d
Mike Bluestein <mike.bluestein@gmail.com>
2012-08-06 17:41:04 -0400

    Remove class variable declaration for UINavigationController

--- Hello_MultiScreen_iPhone/AppDelegate.cs
+++ Hello_MultiScreen_iPhone/AppDelegate.cs
@@ -11,7 +11,6 @@ namespace Hello_MultiScreen_iPhone
 	{
 		//---- declarations
 		UIWindow window;
-		UINavigationController rootNavigationController;
 		
 		// This method is invoked when the application has loaded its UI and it is ready to run
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
@@ -19,14 +18,14 @@ namespace Hello_MultiScreen_iPhone
 			this.window = new UIWindow (UIScreen.MainScreen.Bounds);
 			
 			//---- instantiate a new navigation controller
-			this.rootNavigationController = new UINavigationController();
+			var rootNavigationController = new UINavigationController();
 			//---- instantiate a new home screen
 			HomeScreen homeScreen = new HomeScreen();
 			//---- add the home screen to the navigation controller (it'll be the top most screen)
-			this.rootNavigationController.PushViewController(homeScreen, false);
+			rootNavigationController.PushViewController(homeScreen, false);
 			
 			//---- set the root view controller on the window. the nav controller will handle the rest
-			this.window.RootViewController = this.rootNavigationController;
+			this.window.RootViewController = rootNavigationController;
 			
 			this.window.MakeKeyAndVisible ();
 			
