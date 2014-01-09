8c82140d48afcb7cf1ee1e55fe794082f3eb5ee4
olegoid <olegoid91@gmail.com>
2014-01-09 23:18:54 +0400

    [CoreAnimation] BasicUIViewAnimationScreen: Remove commented "old ways" of animation and empty summaries

--- CoreAnimation/Screens/iPad/BasicUIViewAnimation/BasicUIViewAnimationScreen.xib.cs
+++ CoreAnimation/Screens/iPad/BasicUIViewAnimation/BasicUIViewAnimationScreen.xib.cs
@@ -42,35 +42,6 @@ namespace Example_CoreAnimation.Screens.iPad.BasicUIViewAnimation
 			base.ViewDidLoad ();
 			
 			btnClickMe.TouchUpInside += (s, e) => {
-				
-				
-				//==== OLD WAY
-//				// begin our animation block. the name allows us to refer to it later
-//				UIView.BeginAnimations("ImageMove");
-//				
-//				// move the image one way or the other
-//				if(imgToAnimate.Frame.Y == 64)
-//				{
-//					imgToAnimate.Frame = new System.Drawing.RectangleF(
-//						imgToAnimate.Frame.X, imgToAnimate.Frame.Y + 400,
-//						imgToAnimate.Frame.Size.Width, imgToAnimate.Frame.Size.Height);
-//				}
-//				else
-//				{
-//					imgToAnimate.Frame = new System.Drawing.RectangleF(
-//						imgToAnimate.Frame.X, imgToAnimate.Frame.Y - 400,
-//						imgToAnimate.Frame.Size.Width, imgToAnimate.Frame.Size.Height);
-//				}
-//				
-//				// finish our animation block
-//				UIView.CommitAnimations();
-				
-				
-				//===== NEW WAY
-				
-				// basic prototypes:
-				//UIView.Animate(0.2, () => { /* code to animate */ });
-				//UIView.Animate(0.2, delegate() { /* code to animate */ });
 				UIView.Animate(0.2, () => {
 					// move the image one way or the other
 					if(imgToAnimate.Frame.Y == 64) {
@@ -87,10 +58,7 @@ namespace Example_CoreAnimation.Screens.iPad.BasicUIViewAnimation
 			};
 		}
 		
-		
-		/// <summary>
-		/// 
-		/// </summary>
+
 		public void AddContentsButton (UIBarButtonItem button)
 		{
 			button.Title = "Contents";
