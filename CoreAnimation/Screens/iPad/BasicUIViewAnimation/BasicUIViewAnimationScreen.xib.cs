ab930fbaef7b54b47b5e99052266408ca8438b25
olegoid <olegoid91@gmail.com>
2014-01-13 21:43:24 +0400

    [CoreAnimation] BasicUIViewAnimationScreen changes

--- CoreAnimation/Screens/iPad/BasicUIViewAnimation/BasicUIViewAnimationScreen.xib.cs
+++ CoreAnimation/Screens/iPad/BasicUIViewAnimation/BasicUIViewAnimationScreen.xib.cs
@@ -1,31 +1,32 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+using System.Drawing;
 
 namespace Example_CoreAnimation.Screens.iPad.BasicUIViewAnimation
 {
 	public partial class BasicUIViewAnimationScreen : UIViewController, IDetailView
 	{
+		public event EventHandler ContentsButtonClicked;
+
 		#region Constructors
 
-		// The IntPtr and initWithCoder constructors are required for controllers that need 
+		// The IntPtr and initWithCoder constructors are required for controllers that need
 		// to be able to be created from a xib rather than from managed code
-
-		public BasicUIViewAnimationScreen (IntPtr handle) : base(handle)
+		public BasicUIViewAnimationScreen (IntPtr handle) : base (handle)
 		{
 			Initialize ();
 		}
 
-		[Export("initWithCoder:")]
-		public BasicUIViewAnimationScreen (NSCoder coder) : base(coder)
+		[Export ("initWithCoder:")]
+		public BasicUIViewAnimationScreen (NSCoder coder) : base (coder)
 		{
 			Initialize ();
 		}
 
-		public BasicUIViewAnimationScreen () : base("BasicUIViewAnimationScreen", null)
+		public BasicUIViewAnimationScreen () : base ("BasicUIViewAnimationScreen", null)
 		{
 			Initialize ();
 		}
@@ -33,43 +34,34 @@ namespace Example_CoreAnimation.Screens.iPad.BasicUIViewAnimation
 		void Initialize ()
 		{
 		}
-		
+
 		#endregion
-		
-		
+
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			
+
+			btnContents.TouchUpInside += (sender, e) => {
+				if (ContentsButtonClicked != null) {
+					ContentsButtonClicked (sender, e);
+				}
+			};
+
 			btnClickMe.TouchUpInside += (s, e) => {
-				UIView.Animate(0.2, () => {
+				UIView.Animate (0.2, () => {
 					// move the image one way or the other
-					if(imgToAnimate.Frame.Y == 64) {
-						imgToAnimate.Frame = new System.Drawing.RectangleF (
+					if (imgToAnimate.Frame.Y == 73) {
+						imgToAnimate.Frame = new RectangleF (
 							imgToAnimate.Frame.X, imgToAnimate.Frame.Y + 400,
 							imgToAnimate.Frame.Size.Width, imgToAnimate.Frame.Size.Height);
-					}
-					else {
-						imgToAnimate.Frame = new System.Drawing.RectangleF (
+					} else {
+						imgToAnimate.Frame = new RectangleF (
 							imgToAnimate.Frame.X, imgToAnimate.Frame.Y - 400,
 							imgToAnimate.Frame.Size.Width, imgToAnimate.Frame.Size.Height);
 					}					
 				});
 			};
 		}
-		
-
-		public void AddContentsButton (UIBarButtonItem button)
-		{
-			button.Title = "Contents";
-			tlbrMain.SetItems(new UIBarButtonItem[] { button }, false );
-		}
-		
-		public void RemoveContentsButton ()
-		{
-			tlbrMain.SetItems(new UIBarButtonItem[0], false);
-		}
-
 	}
 }
 
