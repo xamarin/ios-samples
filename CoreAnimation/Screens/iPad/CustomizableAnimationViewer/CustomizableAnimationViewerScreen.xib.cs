e1dd35d09998c960a9f9b6c500b0075cd255c461
olegoid <olegoid91@gmail.com>
2014-01-13 21:45:19 +0400

    [CoreAnimation] CustomizableAnimationViewerScreen changes

--- CoreAnimation/Screens/iPad/CustomizableAnimationViewer/CustomizableAnimationViewerScreen.xib.cs
+++ CoreAnimation/Screens/iPad/CustomizableAnimationViewer/CustomizableAnimationViewerScreen.xib.cs
@@ -1,32 +1,33 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 using MonoTouch.ObjCRuntime;
+using System.Drawing;
 
 namespace Example_CoreAnimation.Screens.iPad.CustomizableAnimationViewer
 {
 	public partial class CustomizableAnimationViewerScreen : UIViewController, IDetailView
 	{
+		public event EventHandler ContentsButtonClicked;
+
 		#region Constructors
 
-		// The IntPtr and initWithCoder constructors are required for controllers that need 
+		// The IntPtr and initWithCoder constructors are required for controllers that need
 		// to be able to be created from a xib rather than from managed code
-
-		public CustomizableAnimationViewerScreen (IntPtr handle) : base(handle)
+		public CustomizableAnimationViewerScreen (IntPtr handle) : base (handle)
 		{
 			Initialize ();
 		}
 
-		[Export("initWithCoder:")]
-		public CustomizableAnimationViewerScreen (NSCoder coder) : base(coder)
+		[Export ("initWithCoder:")]
+		public CustomizableAnimationViewerScreen (NSCoder coder) : base (coder)
 		{
 			Initialize ();
 		}
 
-		public CustomizableAnimationViewerScreen () : base("CustomizableAnimationViewerScreen", null)
+		public CustomizableAnimationViewerScreen () : base ("CustomizableAnimationViewerScreen", null)
 		{
 			Initialize ();
 		}
@@ -34,34 +35,47 @@ namespace Example_CoreAnimation.Screens.iPad.CustomizableAnimationViewer
 		void Initialize ()
 		{
 		}
-		
+
 		#endregion
-		
+
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			
+
+			btnContents.TouchUpInside += (sender, e) => {
+				if (ContentsButtonClicked != null)
+					ContentsButtonClicked (sender, e);
+			};
+
 			btnStart.TouchUpInside += (s, e) => {
 				
 				// begin our animation block. the name allows us to refer to it later
 				UIView.BeginAnimations ("ImageMove");
 
-				UIView.SetAnimationDidStopSelector (new Selector("AnimationStopped"));
+				UIView.SetAnimationDidStopSelector (new Selector ("AnimationStopped"));
 				UIView.SetAnimationDelegate (this); //NOTE: you need this for the selector b.s.
 				
 				// animation delay
-				UIView.SetAnimationDelay ( (double)sldrDelay.Value);
+				UIView.SetAnimationDelay ((double)sldrDelay.Value);
 				
 				// animation duration
-				UIView.SetAnimationDuration ( (double)sldrDuration.Value);
+				UIView.SetAnimationDuration ((double)sldrDuration.Value);
 				
 				// animation curve
 				UIViewAnimationCurve curve = UIViewAnimationCurve.EaseInOut;
 				switch (sgmtCurves.SelectedSegment) {
-					case 0: curve = UIViewAnimationCurve.EaseInOut; break;
-					case 1: curve = UIViewAnimationCurve.EaseIn; break;
-					case 2: curve = UIViewAnimationCurve.EaseOut; break;
-					case 3: curve = UIViewAnimationCurve.Linear; break;
+				case 0:
+					curve = UIViewAnimationCurve.EaseInOut;
+					break;
+				case 1:
+					curve = UIViewAnimationCurve.EaseIn;
+					break;
+				case 2:
+					curve = UIViewAnimationCurve.EaseOut;
+					break;
+				case 3:
+					curve = UIViewAnimationCurve.Linear;
+					break;
 				}
 				UIView.SetAnimationCurve (curve);
 				
@@ -72,36 +86,25 @@ namespace Example_CoreAnimation.Screens.iPad.CustomizableAnimationViewer
 				UIView.SetAnimationRepeatAutoreverses (swtchAutoReverse.On);
 				
 				// move the image one way or the other
-				if (imgToAnimate.Frame.Y == 190) {
-					imgToAnimate.Frame = new System.Drawing.RectangleF (
-						imgToAnimate.Frame.X, imgToAnimate.Frame.Y + 400,
-						imgToAnimate.Frame.Size.Width, imgToAnimate.Frame.Size.Height);
+				if (imgToAnimate.Frame.Y == 214) {
+					imgToAnimate.Frame = new RectangleF (
+						new PointF (imgToAnimate.Frame.X, imgToAnimate.Frame.Y + 400),
+						imgToAnimate.Frame.Size);
 				} else {
-					imgToAnimate.Frame = new System.Drawing.RectangleF (
-						imgToAnimate.Frame.X, imgToAnimate.Frame.Y - 400,
-						imgToAnimate.Frame.Size.Width, imgToAnimate.Frame.Size.Height);
+					imgToAnimate.Frame = new RectangleF (
+						new PointF (imgToAnimate.Frame.X, imgToAnimate.Frame.Y - 400),
+						imgToAnimate.Frame.Size);
 				}
 				
 				// finish our animation block
 				UIView.CommitAnimations ();
 			};
 		}
-		
+
 		[Export]
 		public void AnimationStopped (string name, NSNumber numFinished, IntPtr context)
 		{
-			Console.WriteLine("Animation completed");
-		}
-
-		public void AddContentsButton (UIBarButtonItem button)
-		{
-			button.Title = "Contents";
-			tlbrMain.SetItems (new UIBarButtonItem[] { button }, false );
-		}
-		
-		public void RemoveContentsButton ()
-		{
-			tlbrMain.SetItems (new UIBarButtonItem[0], false);
+			Console.WriteLine ("Animation completed");
 		}
 	}
 }
--- CoreAnimation/Screens/iPad/CustomizableAnimationViewer/CustomizableAnimationViewerScreen.xib.designer.cs
+++ CoreAnimation/Screens/iPad/CustomizableAnimationViewer/CustomizableAnimationViewerScreen.xib.designer.cs
@@ -1,16 +1,20 @@
 // WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace Example_CoreAnimation.Screens.iPad.CustomizableAnimationViewer
 {
 	[Register ("CustomizableAnimationViewerScreen")]
 	partial class CustomizableAnimationViewerScreen
 	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnContents { get; set; }
+
 		[Outlet]
 		MonoTouch.UIKit.UIButton btnStart { get; set; }
 
@@ -27,12 +31,52 @@ namespace Example_CoreAnimation.Screens.iPad.CustomizableAnimationViewer
 		MonoTouch.UIKit.UISlider sldrDuration { get; set; }
 
 		[Outlet]
-		MonoTouch.UIKit.UITextField txtRepeateCount { get; set; }
-
-		[Outlet]
-		MonoTouch.UIKit.UIToolbar tlbrMain { get; set; }
-
-		[Outlet]
 		MonoTouch.UIKit.UISwitch swtchAutoReverse { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UITextField txtRepeateCount { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (btnStart != null) {
+				btnStart.Dispose ();
+				btnStart = null;
+			}
+
+			if (imgToAnimate != null) {
+				imgToAnimate.Dispose ();
+				imgToAnimate = null;
+			}
+
+			if (sgmtCurves != null) {
+				sgmtCurves.Dispose ();
+				sgmtCurves = null;
+			}
+
+			if (sldrDelay != null) {
+				sldrDelay.Dispose ();
+				sldrDelay = null;
+			}
+
+			if (sldrDuration != null) {
+				sldrDuration.Dispose ();
+				sldrDuration = null;
+			}
+
+			if (txtRepeateCount != null) {
+				txtRepeateCount.Dispose ();
+				txtRepeateCount = null;
+			}
+
+			if (swtchAutoReverse != null) {
+				swtchAutoReverse.Dispose ();
+				swtchAutoReverse = null;
+			}
+
+			if (btnContents != null) {
+				btnContents.Dispose ();
+				btnContents = null;
+			}
+		}
 	}
 }
