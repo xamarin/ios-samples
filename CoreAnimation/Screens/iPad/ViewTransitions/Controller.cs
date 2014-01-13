04169bd8c3b38bd3f8a018c67fd231d5bb723de7
olegoid <olegoid91@gmail.com>
2014-01-13 22:26:06 +0400

    [CoreAnimation] Rename fields, set background to white, add subviews rotation

--- CoreAnimation/Screens/iPad/ViewTransitions/Controller.cs
+++ CoreAnimation/Screens/iPad/ViewTransitions/Controller.cs
@@ -1,74 +1,59 @@
 using System;
+using System.Drawing;
 using MonoTouch.CoreFoundation;
 using MonoTouch.UIKit;
-using System.Drawing;
 
 namespace Example_CoreAnimation.Screens.iPad.ViewTransitions
 {
 	public class Controller : UIViewController, IDetailView
 	{
-		protected UIToolbar toolbar;
-		protected TransitionViewController controller1;
-		protected BackTransitionViewController controller2;
-		
-		public Controller ()
-		{
-		}
-		
+		public event EventHandler ContentsButtonClicked;
+
+		private TransitionViewController transitionViewController;
+		private BackTransitionViewController backViewController;
+
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 
-			toolbar = new UIToolbar (new RectangleF (0, 0, this.View.Frame.Width, 44));
-			this.View.Add (toolbar);
+			View.BackgroundColor = UIColor.White;
+			var mainFrame = new RectangleF (0f, 44f, View.Frame.Width, View.Frame.Height - 44f);
 			
-			RectangleF mainFrame = new RectangleF (0, toolbar.Frame.Height, this.View.Frame.Width, this.View.Frame.Height - toolbar.Frame.Height);
-			
-			controller1 = new TransitionViewController ();
-			controller1.View.Frame = mainFrame;
-			controller2 = new BackTransitionViewController ();
-			controller2.View.Frame = mainFrame;
-			
-			View.AddSubview (controller1.View);
-			
-			// controller2.View.Hidden = true;
-			
-			 controller1.TransitionClicked += (s, e) => {
-				UIView.Animate (1, 0,  controller1.SelectedTransition, () => {
-					controller1.View.RemoveFromSuperview ();
-					View.AddSubview (controller2.View);
-					// controller1.View.Hidden = false;
-					// controller2.View.Hidden = true;
-				}, null);
-				UIView.BeginAnimations("ViewChange");
-//				UIView.SetAnimationTransition(UIViewAnimationTransition.FlipFromLeft, this.View, true);
-//				{
-//					 controller1.View.RemoveFromSuperview();
-//					this.View.AddSubview( controller2.View);
-//				}
-//				UIView.CommitAnimations();
+			transitionViewController = new TransitionViewController ();
+			transitionViewController.View.Frame = mainFrame;
+			backViewController = new BackTransitionViewController ();
+			backViewController.View.Frame = mainFrame;
 
+			View.AddSubview (transitionViewController.View);
+			
+			transitionViewController.TransitionClicked += (s, e) => {
+				UIView.Animate (1, 0, transitionViewController.SelectedTransition, () => {
+					transitionViewController.View.RemoveFromSuperview ();
+					View.AddSubview (backViewController.View);
+				}, null);
+				UIView.BeginAnimations ("ViewChange");
+			};
+
+			transitionViewController.ContentsClicked += () => {
+				if (ContentsButtonClicked != null) {
+					ContentsButtonClicked (null, null);
+				}
 			};
 			
-			 controller2.BackClicked += (s, e) => {
-				UIView.Animate(.75, 0,  controller1.SelectedTransition, () => {
-					controller2.View.RemoveFromSuperview ();
-					View.AddSubview (controller1.View);
+			backViewController.BackClicked += (s, e) => {
+				UIView.Animate (.75, 0, transitionViewController.SelectedTransition, () => {
+					backViewController.View.RemoveFromSuperview ();
+					View.AddSubview (transitionViewController.View);
 				}, null);
 			};
 		}
-		
-		public void AddContentsButton (UIBarButtonItem button)
-		{
-			button.Title = "Contents";
-			 toolbar.SetItems(new UIBarButtonItem[] { button }, false );
-		}
-		
-		public void RemoveContentsButton ()
-		{
-			 toolbar.SetItems(new UIBarButtonItem[0], false);
-		}
 
+		public override void WillRotate (UIInterfaceOrientation toInterfaceOrientation, double duration)
+		{
+			transitionViewController.WillRotate (toInterfaceOrientation, duration);
+			backViewController.WillRotate (toInterfaceOrientation, duration);
+			base.WillRotate (toInterfaceOrientation, duration);
+		}
 	}
 }
 
