e543a934ab23670ae874feb9bfff41cf1184faff
olegoid <olegoid91@gmail.com>
2014-01-13 22:02:26 +0400

    [CoreAnimation] Remove ViewWillAppear, ViewWillDisappear overrides. Update code style according to guidelines

--- CoreAnimation/Screens/iPad/NavTable/MasterNavTableViewController.cs
+++ CoreAnimation/Screens/iPad/NavTable/MasterNavTableViewController.cs
@@ -6,58 +6,44 @@ using Example_CoreAnimation.Code.NavigationTable;
 namespace Example_CoreAnimation.Screens.iPad.NavTable
 {
 	public class MasterNavTableViewController : UITableViewController
-	{		
+	{
 		public event EventHandler<RowClickedEventArgs> RowClicked;
-		
-		// declare vars
-		List<NavItemGroup> navItems = new List<NavItemGroup> ();
-		NavItemTableSource tableSource;
-		
+
+		private List<NavItemGroup> navItems;
+		private NavItemTableSource tableSource;
+
 		public MasterNavTableViewController () : base (UITableViewStyle.Grouped)
 		{
+			navItems = new List<NavItemGroup> ();
 		}
-		
-		public override void ViewWillAppear (bool animated)
-		{
-			base.ViewWillAppear (animated);
-			// hide the nav bar when this controller appears
-			//this.NavigationController.SetNavigationBarHidden (true, true);
-		}
-		
-		public override void ViewWillDisappear (bool animated)
-		{
-			base.ViewWillDisappear (animated);
-			// show the nav bar when other controllers appear
-			//this.NavigationController.SetNavigationBarHidden (false, true);
-		}
-		
+
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 			
 			// create the navigation items
-			NavItemGroup navGroup = new NavItemGroup ("UIView Animations");
+			var navGroup = new NavItemGroup ("UIView Animations");
+			navGroup.Items = new List<NavItem> () {
+				new NavItem ("Basic Animation", "", typeof(BasicUIViewAnimation.BasicUIViewAnimationScreen)),
+				new NavItem ("Animation Customizer", "", typeof(CustomizableAnimationViewer.CustomizableAnimationViewerScreen)),
+				new NavItem ("Transitions", "", typeof(ViewTransitions.Controller)),
+				new NavItem ("Implicit Layer Animation", "", typeof(LayerAnimation.ImplicitAnimationScreen)),
+				new NavItem ("Explicit Layer Animation", "", typeof(LayerAnimation.LayerAnimationScreen))
+			};
+
 			navItems.Add (navGroup);
-			navGroup.Items.Add (new NavItem ("Basic Animation", "", typeof (BasicUIViewAnimation.BasicUIViewAnimationScreen)));
-			navGroup.Items.Add (new NavItem ("Animation Customizer", "", typeof (CustomizableAnimationViewer.CustomizableAnimationViewerScreen)));
-			navGroup.Items.Add (new NavItem ("Transitions", "", typeof (ViewTransitions.Controller)));
-			navGroup.Items.Add (new NavItem ("Implicit Layer Animation", "", typeof (LayerAnimation.ImplicitAnimationScreen)));
-			navGroup.Items.Add (new NavItem ("Explicit Layer Animation", "", typeof (LayerAnimation.LayerAnimationScreen)));
-			
+
 			// create a table source from our nav items
 			tableSource = new NavItemTableSource (navItems);
 			
 			// set the source on the table to our data source
-			base.TableView.Source =  tableSource;
+			base.TableView.Source = tableSource;
 			
-			tableSource.RowClicked += (object sender, RowClickedEventArgs e) => {
-				if(this.RowClicked != null)
-					this.RowClicked(sender, e);
+			tableSource.RowClicked += (sender, e) => {
+				if (RowClicked != null)
+					RowClicked (sender, e);
 			};
 		}
-
 	}
-
-
 }
 
