4bc8418d956bcd1fe29eeb4f6631e88df1794a20
olegoid <olegoid91@gmail.com>
2014-01-28 01:20:17 +0400

    [Rotation] Add iPad Method3SwapViews Controller.cs + changes to .csproj file

---
+++ Rotation/HandlingRotation/Screens/iPad/Method3SwapViews/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+
+namespace HandlingRotation.Screens.iPad.Method3SwapViews
+{
+	public class Controller : UIViewController
+	{
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			switch (InterfaceOrientation) {
+			case UIInterfaceOrientation.LandscapeLeft:
+			case UIInterfaceOrientation.LandscapeRight:
+				NSBundle.MainBundle.LoadNib ("LandscapeView", this, null);
+				break;
+			case UIInterfaceOrientation.Portrait:
+			case UIInterfaceOrientation.PortraitUpsideDown:
+				NSBundle.MainBundle.LoadNib ("PortraitView", this, null);
+				break;
+			}
+		}
+	}
+}
+
