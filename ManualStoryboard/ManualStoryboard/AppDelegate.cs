a6b7c817a7fb327e1367b50e4bed10e844525952
Amy Burns <amy.burns87@gmail.com>
2014-05-19 18:12:06 +0100

    Removing storyboard from info.plist

--- ManualStoryboard/ManualStoryboard/AppDelegate.cs
+++ ManualStoryboard/ManualStoryboard/AppDelegate.cs
@@ -12,21 +12,18 @@ namespace ManualStoryboard
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
-		// class-level declarations
-		public override UIWindow Window {
-			get;
-			set;
-		}
-
+		UIWindow window;
 		public static UIStoryboard Storyboard = UIStoryboard.FromName ("MainStoryboard", null);
 		public static UIViewController initialViewController;
 
 		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
 		{
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+
 			initialViewController = Storyboard.InstantiateInitialViewController () as UIViewController;
 
-			Window.RootViewController = initialViewController;
-			Window.MakeKeyAndVisible ();
+			window.RootViewController = initialViewController;
+			window.MakeKeyAndVisible ();
 			return true;
 		}
 
