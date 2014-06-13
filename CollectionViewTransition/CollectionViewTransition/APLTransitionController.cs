096b304d7226c3252cf042b73838887713a98ec5
Vincent Dondain <vincent.dondain@me.com>
2014-06-13 12:00:27 -0400

    Fix for Bug 18382 - [iOS]"CollectionViewTransition" sample is getting freeze when we try to Zoom In/Out the images - I rewrote the method that handle the gesture recognizer, it is now closer to the Apple sample method and works with a different logic than before.

--- CollectionViewTransition/CollectionViewTransition/APLTransitionController.cs
+++ CollectionViewTransition/CollectionViewTransition/APLTransitionController.cs
@@ -25,6 +25,8 @@ namespace CollectionViewTransition {
 
 		public UINavigationControllerOperation NavigationOperation { get; set; }
 
+		// required method for view controller transitions, called when the system needs to set up
+		// the interactive portions of a view controller transition and start the animations
 		void InteractionBegan (PointF point)
 		{
 			UIViewController viewController = ((APLCollectionViewController)navigationController.TopViewController).NextViewControllerAtPoint (point);
@@ -33,6 +35,16 @@ namespace CollectionViewTransition {
 			} else {
 				navigationController.PopViewControllerAnimated (true);
 			}
+		}
+
+		public void AnimateTransition (IUIViewControllerContextTransitioning transitionContext)
+		{
+
+		}
+
+		public double TransitionDuration (IUIViewControllerContextTransitioning transitionContext)
+		{
+			return 1.0f;
 		}
 
 		public override void StartInteractiveTransition (IUIViewControllerContextTransitioning transitionContext)
@@ -52,10 +64,8 @@ namespace CollectionViewTransition {
 
 		void Update (float progress, UIOffset offset)
 		{
-			if (context == null)
-				return;
-
-			if ((progress != transitionLayout.TransitionProgress) || (offset != transitionLayout.Offset)) {
+			if (context != null && // we must have a valid context for updates
+				((progress != transitionLayout.TransitionProgress) || (offset != transitionLayout.Offset))) {
 				transitionLayout.SetOffset (offset);
 				transitionLayout.SetTransitionProgress (progress);
 				transitionLayout.InvalidateLayout ();
@@ -63,14 +73,17 @@ namespace CollectionViewTransition {
 			}
 		}
 
+		// called by our pinch gesture recognizer when the gesture has finished or cancelled, which
+		// in turn is responsible for finishing or cancelling the transition.
 		void EndInteraction (bool success)
 		{
 			if (context == null) {
 				HasActiveInteraction = false;
-				return;
 			}
-
-			if (success && (transitionLayout.TransitionProgress > 0.5f)) {
+			// allow for the transition to finish when it's progress has started as a threshold of 10%,
+			// if you want to require the pinch gesture with a wider threshold, change it it a value closer to 1.0
+			//
+			else if ((transitionLayout.TransitionProgress > 0.1f) && success) {
 				collectionView.FinishInteractiveTransition ();
 				context.FinishInteractiveTransition ();
 			} else {
@@ -79,61 +92,53 @@ namespace CollectionViewTransition {
 			}
 		}
 
+		// action method for our pinch gesture recognizer
 		public void HandlePinch (UIPinchGestureRecognizer sender)
 		{
-			if (sender.NumberOfTouches < 2)
-				return;
-
-			PointF point1 = sender.LocationOfTouch (0, sender.View);
-			PointF point2 = sender.LocationOfTouch (1, sender.View);
-			float distance = (float) Math.Sqrt ((point1.X - point2.X) * (point1.X - point2.X) +
-			                                    (point1.Y - point2.Y) * (point1.Y - point2.Y));
-			PointF point = sender.LocationInView (sender.View);
-
-			if (sender.State == UIGestureRecognizerState.Began) {
-				if (HasActiveInteraction)
-					return;
-
-				initialPinchDistance = distance;
-				initialPinchPoint = point;
-				HasActiveInteraction = true;
-				InteractionBegan (point);
-				return;
-			}
-
-			if (!HasActiveInteraction)
-				return;
-
-			switch (sender.State) {
-			case UIGestureRecognizerState.Changed:
-				float offsetX = point.X - initialPinchPoint.X;
-				float offsetY = point.Y - initialPinchPoint.Y;
-				float distanceDelta = distance - initialPinchDistance;
-
-				if (NavigationOperation == UINavigationControllerOperation.Pop)
-					distanceDelta = -distanceDelta;
-
-				SizeF size = collectionView.Bounds.Size;
-				float dimension = (float)Math.Sqrt (size.Width * size.Width + size.Height * size.Height);
-				float progress = (float) Math.Max (Math.Min (distanceDelta / dimension, 1.0), 0.0);
-				Update (progress, new UIOffset (offsetX, offsetY));
-				break;
-			case UIGestureRecognizerState.Ended:
+			// here we want to end the transition interaction if the user stops or finishes the pinch gesture
+			if (sender.State == UIGestureRecognizerState.Ended) {
 				EndInteraction (true);
-				break;
-			case UIGestureRecognizerState.Cancelled:
+			} else if (sender.State == UIGestureRecognizerState.Cancelled) {
 				EndInteraction (false);
-				break;
+			} else if (sender.NumberOfTouches == 2) {
+				// here we expect two finger touch
+				PointF point = sender.LocationInView (sender.View); // get the main touch point
+				PointF point1 = sender.LocationOfTouch (0, sender.View); // return the locations of each gesture’s touches in the local coordinate system of a given view
+				PointF point2 = sender.LocationOfTouch (1, sender.View);
+				float distance = (float)Math.Sqrt ((point1.X - point2.X) * (point1.X - point2.X) +
+				                (point1.Y - point2.Y) * (point1.Y - point2.Y));
+
+				if (sender.State == UIGestureRecognizerState.Began) {
+					// start the pinch in our out
+					if (!HasActiveInteraction) {
+						initialPinchDistance = distance;
+						initialPinchPoint = point;
+						HasActiveInteraction = true; // the transition is in active motion
+						InteractionBegan (point);
+					}
+				}
+
+				if (HasActiveInteraction) {
+					if (sender.State == UIGestureRecognizerState.Changed) { 
+						// update the progress of the transtition as the user continues to pinch
+						float offsetX = point.X - initialPinchPoint.X;
+						float offsetY = point.Y - initialPinchPoint.Y;
+
+						float distanceDelta = distance - initialPinchDistance;
+
+						if (NavigationOperation == UINavigationControllerOperation.Pop)
+							distanceDelta = -distanceDelta;
+
+						SizeF size = collectionView.Bounds.Size;
+						float dimension = (float)Math.Sqrt (size.Width * size.Width + size.Height * size.Height);
+						float progress = (float)Math.Max (Math.Min (distanceDelta / dimension, 1.0), 0.0);
+
+						// tell our UICollectionViewTransitionLayout subclass (transitionLayout)
+						// the progress state of the pinch gesture
+						Update (progress, new UIOffset (offsetX, offsetY));
+					}
+				}
 			}
-		}
-
-		public void AnimateTransition (IUIViewControllerContextTransitioning transitionContext)
-		{
-		}
-
-		public double TransitionDuration (IUIViewControllerContextTransitioning transitionContext)
-		{
-			return 1.0f;
 		}
 	}
 }
