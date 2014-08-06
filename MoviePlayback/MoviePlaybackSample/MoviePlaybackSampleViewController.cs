f0f193ea155a9f2d93bbfd6269d37e0d0007b265
nodoid <paul@all-the-johnsons.co.uk>
2014-08-06 14:47:23 +0100

    Added [Obsolete] to ViewDidLoad and ShouldRotate (#38)

--- MoviePlayback/MoviePlaybackSample/MoviePlaybackSampleViewController.cs
+++ MoviePlayback/MoviePlaybackSample/MoviePlaybackSampleViewController.cs
@@ -1,75 +1,84 @@
 using MonoTouch.UIKit;
-using System.Drawing;
 using System;
 using MonoTouch.Foundation;
 using MonoTouch.MediaPlayer;
 
 namespace MoviePlaybackSample
 {
-	public partial class MoviePlaybackSampleViewController : UIViewController
-	{
-		//class level declarations
-		MPMoviePlayerController mp = new MPMoviePlayerController();
-		
-		static bool UserInterfaceIdiomIsPhone {
-			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
-		}
+    public partial class MoviePlaybackSampleViewController : UIViewController
+    {
+        //class level declarations
+        MPMoviePlayerController mp = new MPMoviePlayerController();
 
-		public MoviePlaybackSampleViewController ()
-			: base (UserInterfaceIdiomIsPhone ? "MoviePlaybackSampleViewController_iPhone" : "MoviePlaybackSampleViewController_iPad", null)
-		{
-		}
-	
-		public override void ViewDidLoad ()
-		{
-			
-			base.ViewDidLoad ();
+        static bool UserInterfaceIdiomIsPhone
+        {
+            get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+        }
 
-			//Button Events
-			playMovieButton.TouchUpInside += delegate {
-			
-				try{
-					//Set already instantiated MPMoviePlayerController to playback from Movies/file.m4v
-					mp = new MPMoviePlayerController (NSUrl.FromFilename("Movies/file.m4v"));
-					
-					//enable AirPlay
-					mp.AllowsAirPlay = true;
-					
-					//Add the MPMoviePlayerController View
-					this.View.AddSubview(mp.View);
-					
-					//set the view to be full screen and show animated
-					mp.SetFullscreen(true, true);
+        public MoviePlaybackSampleViewController()
+            : base(UserInterfaceIdiomIsPhone ? "MoviePlaybackSampleViewController_iPhone" : "MoviePlaybackSampleViewController_iPad", null)
+        {
+        }
 
-					//Disable the pinch-to-zoom gesture
-					mp.ControlStyle = MPMovieControlStyle.Fullscreen;
+        public override void ViewDidLoad()
+        {
 			
-					//MPMoviePlayer must be set to PrepareToPlay before playback
-					mp.PrepareToPlay();
+            base.ViewDidLoad();
+
+            //Button Events
+            playMovieButton.TouchUpInside += delegate
+            {
+			
+                try
+                {
+                    //Set already instantiated MPMoviePlayerController to playback from Movies/file.m4v
+                    mp = new MPMoviePlayerController(NSUrl.FromFilename("Movies/file.m4v"));
 					
-					//Play Movie
-					mp.Play();
-				} catch {
-					Console.WriteLine("There was a problem playing back Video");
-				}		
+                    //enable AirPlay
+                    mp.AllowsAirPlay = true;
+					
+                    //Add the MPMoviePlayerController View
+                    this.View.AddSubview(mp.View);
+					
+                    //set the view to be full screen and show animated
+                    mp.SetFullscreen(true, true);
+
+                    //Disable the pinch-to-zoom gesture
+                    mp.ControlStyle = MPMovieControlStyle.Fullscreen;
+			
+                    //MPMoviePlayer must be set to PrepareToPlay before playback
+                    mp.PrepareToPlay();
+					
+                    //Play Movie
+                    mp.Play();
+                }
+                catch
+                {
+                    Console.WriteLine("There was a problem playing back Video");
+                }		
 				
-			};
+            };
 						
-		}
-		
-		public override void ViewDidUnload ()
-		{
-			base.ViewDidUnload ();
-		}
-		
-		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
-		{
-			// Return true for supported orientations
-			if (UserInterfaceIdiomIsPhone) {
-				return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
-			} else {
-				return true;
-			}
-		}
-	}
+        }
+
+        [Obsolete]
+        public override void ViewDidUnload()
+        {
+            base.ViewDidUnload();
+        }
+
+        [Obsolete]
+        public override bool ShouldAutorotateToInterfaceOrientation(UIInterfaceOrientation toInterfaceOrientation)
+        {
+            // Return true for supported orientations
+            if (UserInterfaceIdiomIsPhone)
+            {
+                return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
 }
