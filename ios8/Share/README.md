75a4c4158705d89b81c0a6e1189544e6b334b806
Vincent Dondain <vincent.dondain@xamarin.com>
2014-09-15 16:26:21 -0400

    [Share] - First commit
    - This is a share extension sample.

---
+++ ios8/Share/README.md
@@ -0,0 +1,24 @@
+Share
+=====
+
+This is an app extension sample using the Share Extension. The extension is avaible across the whole system by clicking on the iOS share button (square + arrow). It provides a simple view related to the content you wanted to share and prompts you when you tap the "post" button.
+
+Instructions
+------------
+
+To enable the extension:
+
+* Go to an app who has a share button (like Safari).
+* On the line where you see Mail, Twitter and Facebook, click on "more".
+* Enable the ShareExtension.
+
+Build Requirements
+------------------
+
+Building this sample requires Xcode 6.0 and iOS 8.0 SDK.
+
+Author
+------ 
+Copyright (C) 2014 Apple Inc. All rights reserved.
+
+Ported to Xamarin.iOS by Vincent Dondain.
---
+++ ios8/Share/ShareExtension/ShareExtensionViewController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+
+using UIKit;
+using Social;
+using Foundation;
+using CoreFoundation;
+
+namespace ShareExtension
+{
+	public partial class ShareExtensionViewController : SLComposeServiceViewController
+	{
+		public ShareExtensionViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Do any additional setup after loading the view.
+		}
+
+		public override bool IsContentValid ()
+		{
+			// Do validation of contentText and/or NSExtensionContext attachments here
+			return true;
+		}
+
+		public override void DidSelectPost ()
+		{
+			// This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+			UIAlertController alert = UIAlertController.Create ("Share extension", "This is the step where you should post the ContentText value: \"" + ContentText + "\" to your targeted service.", UIAlertControllerStyle.Alert);
+			PresentViewController (alert, true, () => {
+				DispatchQueue.MainQueue.DispatchAfter (new DispatchTime (DispatchTime.Now, 5000000000), () => {
+					ExtensionContext.CompleteRequest (null, null);
+				});
+			});
+		}
+
+		public override SLComposeSheetConfigurationItem[] GetConfigurationItems ()
+		{
+			// To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+			return new SLComposeSheetConfigurationItem[0];
+		}
+	}
+}
+
