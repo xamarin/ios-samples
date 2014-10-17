9f45f940295a9141fb9843c0bd56407ef851f995
olegoid <olegoid91@gmail.com>
2014-10-17 20:16:57 +0400

    [TableSearch] Add TableSearch sample

---
+++ ios8/TableSearch/README.md
@@ -0,0 +1,22 @@
+Table Search with UISearchController
+====================================
+
+This is an iOS sample application that demonstrates how to use UISearchController.
+A search controller manages the presentation of a search bar (in concert with the results view controller’s content).
+
+
+Build Requirements
+------------------
+
+This sample requires Xcode 6.0 or later, iOS 8.0 SDK or later
+iOS 7 or later
+
+Runtime
+------------------
+Device/simulator with iOS 8 or later
+
+Author
+------ 
+Copyright (C) 2014 Apple Inc. All rights reserved.
+
+Ported to Xamarin.iOS by Oleg Demchenko
---
+++ ios8/TableSearch/TableSearch/DetailViewController.designer.cs
@@ -0,0 +1,34 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace TableSearch
+{
+	[Register ("DetailViewController")]
+	partial class DetailViewController
+	{
+		[Outlet("price")]
+		UIKit.UILabel Price { get; set; }
+
+		[Outlet("year")]
+		UIKit.UILabel Year { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (Year != null) {
+				Year.Dispose ();
+				Year = null;
+			}
+
+			if (Price != null) {
+				Price.Dispose ();
+				Price = null;
+			}
+		}
+	}
+}
