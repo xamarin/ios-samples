0c86effe31b6a069c30ea3f7f585ab9fd132580f
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-10-17 18:12:27 +0400

    [AdaptivePhotos] new version of Xamarin.iOS handle TopLayoutGuide and TopLayoutGuide

--- ios8/AdaptivePhotos/AdaptivePhotos/Controller/ProfileViewController.cs
+++ ios8/AdaptivePhotos/AdaptivePhotos/Controller/ProfileViewController.cs
@@ -114,14 +114,14 @@ namespace AdaptivePhotos
 
 				newConstraints.AddRange (NSLayoutConstraint.FromVisualFormat ("V:|[topLayoutGuide]-[nameLabel]-[conversationsLabel]-[photosLabel]",
 					NSLayoutFormatOptions.DirectionLeadingToTrailing,
-					"topLayoutGuide", ObjCRuntime.Runtime.GetNSObject(TopLayoutGuide.Handle),
+					"topLayoutGuide", TopLayoutGuide,
 					"nameLabel", NameLabel,
 					"conversationsLabel", ConversationsLabel,
 					"photosLabel", PhotosLabel));
 
 				newConstraints.AddRange (NSLayoutConstraint.FromVisualFormat ("V:|[topLayoutGuide][imageView]|",
 					NSLayoutFormatOptions.DirectionLeadingToTrailing,
-					"topLayoutGuide", ObjCRuntime.Runtime.GetNSObject(TopLayoutGuide.Handle),
+					"topLayoutGuide", TopLayoutGuide,
 					"imageView", ImageView));
 
 				newConstraints.Add (NSLayoutConstraint.Create (ImageView, NSLayoutAttribute.Width, NSLayoutRelation.Equal, 
@@ -145,7 +145,7 @@ namespace AdaptivePhotos
 
 				newConstraints.AddRange (NSLayoutConstraint.FromVisualFormat ("V:[topLayoutGuide]-[nameLabel]-[conversationsLabel]-[photosLabel]-20-[imageView]|",
 					NSLayoutFormatOptions.DirectionLeadingToTrailing,
-					"topLayoutGuide", ObjCRuntime.Runtime.GetNSObject(TopLayoutGuide.Handle),
+					"topLayoutGuide", TopLayoutGuide,
 					"nameLabel", NameLabel,
 					"conversationsLabel", ConversationsLabel,
 					"photosLabel", PhotosLabel,
