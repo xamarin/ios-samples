9958e1da08e56ee5f2be681c1c5fc09ab1878d85
GouriKumari <gouri.kumari@xamarin.com>
2014-11-17 17:58:14 -0500

    AVTouchSample migrated to unified

--- AVTouchSample/avTouchViewController.cs
+++ AVTouchSample/avTouchViewController.cs
@@ -2,9 +2,9 @@
 using System;
 using System.IO;
 using System.Collections;
-using MonoTouch.UIKit;
-using MonoTouch.Foundation;
-using MonoTouch.AVFoundation;
+using UIKit;
+using Foundation;
+using AVFoundation;
 
 namespace avTouch
 {
