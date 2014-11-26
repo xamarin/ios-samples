4691f46604ad2cf07dcde5e8bf28d3662a5ea0bd
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-11-26 20:05:57 +0300

    [ExtensionsDemo] fixed compilation errors Xamarin.iOS 8.6.0.2

--- ExtensionsDemo/EvolveCountdownWidget/EvolveCountdownViewController.cs
+++ ExtensionsDemo/EvolveCountdownWidget/EvolveCountdownViewController.cs
@@ -11,7 +11,8 @@ namespace EvolveCountdownWidget
 {
 	public partial class EvolveCountdownViewController : UIViewController, INCWidgetProviding
 	{
-		public EvolveCountdownViewController (IntPtr handle) : base (handle)
+		public EvolveCountdownViewController (IntPtr handle)
+			: base (handle)
 		{
 		}
 
@@ -20,21 +21,20 @@ namespace EvolveCountdownWidget
 			base.ViewDidLoad ();
 
 			PreferredContentSize = new CGSize (PreferredContentSize.Width, 55f);
-            var evolveStartDate = new DateTime (2014, 10, 6);
-            var numDays = (evolveStartDate - DateTime.Now).Days;
+			var evolveStartDate = new DateTime (2014, 10, 6);
+			var numDays = (evolveStartDate - DateTime.Now).Days;
 
-            WidgetTitle.Text = String.Format ("{0} days until Evolve", numDays);
+			WidgetTitle.Text = String.Format ("{0} days until Evolve", numDays);
 
-            WidgetButton.SetTitle ("Tap here to register", UIControlState.Normal);
-                
-            WidgetButton.TouchUpInside += (sender, e) => UIApplication.SharedApplication.OpenUrl (new NSUrl ("evolveCountdown://"));
+			WidgetButton.SetTitle ("Tap here to register", UIControlState.Normal);
+
+			WidgetButton.TouchUpInside += (sender, e) => UIApplication.SharedApplication.OpenUrl (new NSUrl ("evolveCountdown://"));
 		}
 
-        //BUG: this never gets called
+		//BUG: this never gets called
 		public void WidgetPerformUpdate (Action<NCUpdateResult> completionHandler)
 		{
 			WidgetTitle.Text = "updated title";
-	
 			completionHandler (NCUpdateResult.NewData);
 		}
 	}
