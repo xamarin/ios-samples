81f004307367c14d7a8147412b9dd36096c78d3e
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-12-01 17:47:19 +0300

    [BackgroundExecution] remove empty folders. remove dependency from xib file Xamarin.iOS 8.6.0.2

--- BackgroundExecution/Screens/HomeScreen.cs
+++ BackgroundExecution/Screens/HomeScreen.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,57 +11,60 @@ namespace BackgroundExecution
 	{
 		#region Constructors
 
-		// The IntPtr and initWithCoder constructors are required for items that need 
+		// The IntPtr and initWithCoder constructors are required for items that need
 		// to be able to be created from a xib rather than from managed code
 
-		public HomeScreen (IntPtr handle) : base(handle)
+		public HomeScreen (IntPtr handle) : base (handle)
 		{
 		}
 
-		[Export("initWithCoder:")]
-		public HomeScreen (NSCoder coder) : base(coder)
+		[Export ("initWithCoder:")]
+		public HomeScreen (NSCoder coder) : base (coder)
 		{
 		}
 
-		public HomeScreen () : base("HomeScreen_iPhone", null)
+		public HomeScreen () : base ("HomeScreen_iPhone", null)
 		{
 		}
 
 		#endregion
-		
+
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			
+
 			this.Title = "Background Execution";
-			
+
 			this.btnStartLongRunningTask.TouchUpInside += (s, e) => {
-				ThreadStart ts = new ThreadStart ( () => { this.DoSomething(); });
-				ts.Invoke();
+				ThreadStart ts = new ThreadStart (() => {
+					this.DoSomething ();
+				});
+				ts.Invoke ();
 			};
 		}
-		
+
 		public void DoSomething ()
 		{
 			// register our background task
-			int taskID = (int)UIApplication.SharedApplication.BeginBackgroundTask ( () => { this.BackgroundTaskExpiring (); });
+			int taskID = (int)UIApplication.SharedApplication.BeginBackgroundTask (() => {
+				this.BackgroundTaskExpiring ();
+			});
 
 			Console.WriteLine ("Starting background task " + taskID.ToString ());
-			
+
 			// sleep for five seconds
-			Thread.Sleep (5000);			
-			
+			Thread.Sleep (5000);
+
 			Console.WriteLine ("Background task " + taskID.ToString () + " completed.");
-			
+
 			// mark our background task as complete
 			UIApplication.SharedApplication.EndBackgroundTask (taskID);
 		}
-		
+
 		public void BackgroundTaskExpiring ()
 		{
 			Console.WriteLine ("Running out of time to complete you background task!");
 		}
-		
 	}
 }
 
