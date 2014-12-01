27fc6af0850f849bd84217aaeafd062783bbe875
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-12-01 21:12:15 +0300

    [Popovers] fix arch, fix warnings
    Xamarin.iOS 8.6.0.2

--- Popovers/AppDelegate.cs
+++ Popovers/AppDelegate.cs
@@ -10,7 +10,7 @@ namespace Popovers
 	public partial class AppDelegate : UIApplicationDelegate
 	{
  		[Outlet]
-		public UIWindow Window { get; set; }
+		public override UIWindow Window { get; set; }
 
 		[Outlet]
 		public UISplitViewController SplitViewController { get; set; }
