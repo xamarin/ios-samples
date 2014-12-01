c597ce69766723b6cbc2ff0eda2d3b48a51e5422
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-12-01 22:28:43 +0300

    [SysSound] fix target version. remove unused code. fix formatting
    Xamarin.iOS 8.6.0.2

--- SysSound/Extensions/NSBundleExtensions.cs
+++ SysSound/Extensions/NSBundleExtensions.cs
@@ -1,21 +1,17 @@
 using System;
-using ObjCRuntime;
 using Foundation;
 
-namespace SysSound.Extensions {
-
+namespace SysSound.Extensions
+{
 	//helper methods for withing with bundles
-	public static class NSBundleExtensions {
-		private static readonly IntPtr _Selector_URLForResource = Selector.GetHandle ("URLForResource:withExtension:");
-		
+	public static class NSBundleExtensions
+	{
 		/// <summary>
 		/// Returns the file URL for the resource identified by the specified name and file extension.
 		/// </summary>
-		public static NSUrl URLForResource (this NSBundle source, string for_resource, string with_extension) { 
+		public static NSUrl URLForResource (this NSBundle source, string for_resource, string with_extension)
+		{
 			return source.GetUrlForResource (for_resource, with_extension);
 		}
-		
 	}
-	
 }
-
