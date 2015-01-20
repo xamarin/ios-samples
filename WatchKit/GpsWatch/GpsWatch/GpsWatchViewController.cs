d76e2a67227160d8205e48399753efdb9260c785
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-01-20 20:09:48 +0300

    [GpsWatch] copy sample to new location. Should remove on old place after doc updating

---
+++ WatchKit/GpsWatch/GpsWatch/GpsWatchViewController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Drawing;
+
+using UIKit;
+using Foundation;
+using CoreGraphics;
+using System.Linq;
+
+namespace GpsWatch
+{
+	public partial class GpsWatchViewController : UIViewController
+	{
+		UILabel lbl;
+
+		static bool UserInterfaceIdiomIsPhone {
+			get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+		}
+
+		public GpsWatchViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			Console.WriteLine ("GpsWatchViewController loaded");
+			ShowDescription ();
+		}
+
+		void ShowDescription()
+		{
+			lbl = new UILabel {
+				Lines = 0,
+				LineBreakMode = UILineBreakMode.WordWrap,
+				Text = "This is Main iOS app. It hosts watchkit app and watchkit extension. Watch extension requests location changes from MainApp Aslo and passes it to WatchApp",
+			};
+			lbl.Frame = new CGRect (0, 0, View.Frame.Width - 20, 0);
+			lbl.SizeToFit ();
+			View.AddSubview (lbl);
+
+			lbl.Center = View.Center;
+		}
+
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+		}
+
+		public override void ViewDidAppear (bool animated)
+		{
+			base.ViewDidAppear (animated);
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+		}
+
+		public override void ViewDidDisappear (bool animated)
+		{
+			base.ViewDidDisappear (animated);
+		}
+
+		#endregion
+
+	}
+}
+
---
+++ WatchKit/GpsWatch/WatchAppExtension/InterfaceController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+using WatchKit;
+using Foundation;
+using CoreLocation;
+
+namespace WatchAppExtension
+{
+	public partial class InterfaceController : WKInterfaceController
+	{
+		NSTimer timer;
+
+		public InterfaceController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void Awake (NSObject context)
+		{
+			base.Awake (context);
+
+			timer = NSTimer.CreateRepeatingScheduledTimer (5, UpdateUserInterface);
+			UpdateUserInterface (timer);
+
+			// Configure interface objects here.
+			Console.WriteLine ("{0} awake with context", this);
+		}
+
+		public override void WillActivate ()
+		{
+			// This method is called when the watch view controller is about to be visible to the user.
+			Console.WriteLine ("{0} will activate", this);
+		}
+
+		public override void DidDeactivate ()
+		{
+			// This method is called when the watch view controller is no longer visible to the user.
+			Console.WriteLine ("{0} did deactivate", this);
+		}
+
+		void UpdateUserInterface(NSTimer t)
+		{
+			WKInterfaceController.OpenParentApplication (new NSDictionary (), (replyInfo, error) => {
+				if(error != null) {
+					Console.WriteLine (error);
+					return;
+				}
+
+				var status = (CLAuthorizationStatus)((NSNumber)replyInfo["status"]).UInt32Value;
+				var longitude = ((NSNumber)replyInfo["lon"]).DoubleValue;
+				var latitude = ((NSNumber)replyInfo["lat"]).DoubleValue;
+
+				Console.WriteLine ("authorization status {0}", status);
+				switch(status) {
+					case CLAuthorizationStatus.AuthorizedAlways:
+						SetCooridinate(longitude, latitude);
+						HideWarning();
+						break;
+
+					case CLAuthorizationStatus.NotDetermined:
+						SetNotAvailable();
+						ShowWarning("Launch the iOS app first");
+						break;
+
+					case CLAuthorizationStatus.Denied:
+						SetNotAvailable();
+						ShowWarning("Enable Location Service on iPhone");
+						break;
+
+					default:
+						throw new NotImplementedException();
+				}
+			});
+		}
+
+		void SetCooridinate(double longitude, double latitude)
+		{
+			LongitudeValueLbl.SetText (longitude.ToString ());
+			LatitudeValueLbl.SetText (latitude.ToString ());
+		}
+
+		void SetNotAvailable()
+		{
+			LongitudeValueLbl.SetText ("not available");
+			LatitudeValueLbl.SetText ("not available");
+		}
+
+		void ShowWarning(string warning)
+		{
+			WarningLbl.SetText (warning);
+			WarningLbl.SetHidden (false);
+		}
+
+		void HideWarning()
+		{
+			WarningLbl.SetHidden (true);
+		}
+	}
+}
+
