d57fff24fd2d752397d4f33a1ac1fb4dcf886221
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-02-04 23:48:20 +0300

    [Lister] updated ListItem class
    Xamarin.iOS 8.6.0.51

--- ios8/Lister/ListerKit/ListItem.cs
+++ ios8/Lister/ListerKit/ListItem.cs
@@ -7,9 +7,9 @@ namespace ListerKit
 	[Register("ListItem")]
 	public class ListItem: NSObject, ICloneable, INSCoding
 	{
-		const string ListItemEncodingTextKey = "text";
-		const string ListItemEncodingCompletedKey = "completed";
-		const string ListItemEncodingUUIDKey = "uuid";
+		const string TextKey = "text";
+		const string CompletedKey = "completed";
+		const string UUIDKey = "uuid";
 
 		public string Text { get; set; }
 		public bool IsComplete { get; set; }
@@ -22,26 +22,32 @@ namespace ListerKit
 			UID = uid;
 		}
 
+		public ListItem(string text, bool isComplete)
+			: this(text, isComplete, Guid.NewGuid())
+		{
+
+		}
+
 		public ListItem(string text)
-			: this(text, false, Guid.NewGuid())
+			: this(text, false)
 		{
 		}
 
 		[Export("initWithCoder:")]
 		public ListItem(NSCoder coder)
 		{
-			Text = (string)(NSString)coder.DecodeObject (ListItemEncodingTextKey);
-			NSUuid uid = (NSUuid)coder.DecodeObject (ListItemEncodingUUIDKey);
+			Text = (string)(NSString)coder.DecodeObject (TextKey);
+			NSUuid uid = (NSUuid)coder.DecodeObject (UUIDKey);
 			UID = new Guid (uid.GetBytes());
-			IsComplete = coder.DecodeBool (ListItemEncodingCompletedKey);
+			IsComplete = coder.DecodeBool (CompletedKey);
 		}
 
 		[Export ("encodeWithCoder:")]
 		public void EncodeTo (NSCoder coder)
 		{
-			coder.Encode ((NSString)Text, ListItemEncodingTextKey);
-			coder.Encode (new NSUuid(UID.ToByteArray ()), ListItemEncodingUUIDKey);
-			coder.Encode (IsComplete, ListItemEncodingCompletedKey);
+			coder.Encode ((NSString)Text, TextKey);
+			coder.Encode (new NSUuid(UID.ToByteArray ()), UUIDKey);
+			coder.Encode (IsComplete, CompletedKey);
 		}
 
 		public void RefreshIdentity()
@@ -49,14 +55,28 @@ namespace ListerKit
 			UID = Guid.Empty;
 		}
 
-		#region ICloneable implementation
-
+		// TODO: how does app use it?
 		public object Clone ()
 		{
 			return new ListItem (Text, IsComplete, UID);
 		}
 
-		#endregion
+		// TODO: ensure that this method calls
+		public override bool IsEqual (NSObject anObject)
+		{
+			if (anObject == null)
+				return false;
+
+			if (anObject.GetType () != typeof(ListItem))
+				return false;
+
+			ListItem second = (ListItem)anObject;
+			return UID = second.UID;
+		}
+
+		public override string ToString ()
+		{
+			return string.Format ("[ListItem: Text={0}, IsComplete={1}, UID={2}]", Text, IsComplete, UID);
+		}
 	}
 }
-
