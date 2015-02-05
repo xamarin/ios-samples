e122fa367ec0b31d30d07105a205e120810c0ba8
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-02-06 00:43:38 +0300

    [Lister] started to implement Utils

---
+++ ios8/Lister/ListerKit/Presenters/ListPresenterUtilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListerKit
+{
+	public static class ListPresenterUtilities
+	{
+		void RemoveListItemsFromListItemsWithListPresenter(IListPresenter listPresenter, List<ListItem> initialListItems, List<ListItem> listItemsToRemove)
+		{
+//			NSArray *sortedListItemsToRemove = [listItemsToRemove sortedArrayUsingComparator:^NSComparisonResult(AAPLListItem *lhs, AAPLListItem *rhs) {
+//				return [initialListItems indexOfObject:lhs] > [initialListItems indexOfObject:rhs];
+//			}];
+//
+//			for (AAPLListItem *listItemToRemove in sortedListItemsToRemove) {
+//				// Use the index of the list item to remove in the current list's list items.
+//				NSInteger indexOfListItemToRemoveInOldList = [initialListItems indexOfObject:listItemToRemove];
+//
+//				[initialListItems removeObjectAtIndex:indexOfListItemToRemoveInOldList];
+//
+//				[listPresenter.delegate listPresenter:listPresenter didRemoveListItem:listItemToRemove atIndex:indexOfListItemToRemoveInOldList];
+//			}
+		}
+
+	}
+}
+
