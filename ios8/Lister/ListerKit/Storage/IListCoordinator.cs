648e06047c3a7485402998c4a781575a72e1de6c
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-02-17 19:04:24 +0300

    [Lister] add IListCoordinator and IListCoordinatorDelegate

---
+++ ios8/Lister/ListerKit/Storage/IListCoordinator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Foundation;
+
+namespace ListerKit
+{
+	// TODO: add comments
+	public interface IListCoordinator
+	{
+		IListCoordinatorDelegate Delegate { get; set; }
+
+		void StartQuery ();
+
+		void StopQuery ();
+
+		void RemoveListAtUrl(NSUrl url);
+
+		void CreateUrlForList(List list, string name);
+
+		bool CanCreateListWithName(string name);
+	}
+}
+
---
+++ ios8/Lister/ListerKit/Storage/IListCoordinatorDelegate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Foundation;
+
+namespace ListerKit
+{
+	// TODO: add comments
+	public interface IListCoordinatorDelegate
+	{
+		void DidUpdateContentsWithInsertedURLs(IEnumerable<NSUrl> insertedURLs, IEnumerable<NSUrl> removedURLs, IEnumerable<NSUrl> updatedURLs);
+
+		void DidFailRemovingListAtURL(NSUrl url, NSError error);
+
+		void DidFailCreatingListAtURL(NSUrl url, NSError error);
+	}
+}
+
