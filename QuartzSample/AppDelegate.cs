fc8fc6a22fd91d537a44dc4c91d34d339ca874ef
Dylan Kelly <dylan.kelly@xamarin.com>
2015-03-31 15:12:37 -0400

    Adjust FinishedLaunching() structure

---
+++ QuartzSample/AppDelegate.cs
@@ -0,0 +1,29 @@
+﻿using UIKit;
+using Foundation;
+
+namespace QuartzSample
+{
+	// The name AppDelegate is referenced in the MainWindow.xib file.
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		// This method is invoked when the application has loaded its UI and its ready to run
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			// If you have defined a view, add it here:
+			// window.AddSubview (navigationController.View);
+
+			navigationController.NavigationBar.BarStyle = UIBarStyle.Default;
+			window.AddSubview (navigationController.View);
+
+			window.MakeKeyAndVisible ();
+
+			return true;
+		}
+
+		// This method is required in iPhoneOS 3.0
+		public override void OnActivated (UIApplication application)
+		{
+		}
+	}
+}
+
