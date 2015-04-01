b6cace088799f5ae96aa47da05cda4d380f2b574
Dylan Kelly <dylan.kelly@xamarin.com>
2015-04-01 17:56:58 -0400

    Add FinishedLaunching()

--- SpriteTour/SpriteTour/AppDelegate.cs
+++ SpriteTour/SpriteTour/AppDelegate.cs
@@ -7,6 +7,11 @@ namespace SpriteTour {
 	[Register ("AppDelegate")]
 	public partial class AppDelegate : UIApplicationDelegate
 	{
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			return true;
+		}
+
 		// class-level declarations
 		public override UIWindow Window {
 			get;
