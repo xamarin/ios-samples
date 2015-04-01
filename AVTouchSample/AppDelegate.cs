8c8f46ffd0c0816234774bb6d72633995a136093
Dylan Kelly <dylan.kelly@xamarin.com>
2015-04-01 12:00:33 -0400

    Adjust FinishedLaunching() structure

---
+++ AVTouchSample/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using UIKit;
+using Foundation;
+
+namespace avTouch
+{
+	// The name AppDelegate is referenced in the MainWindow.xib file.
+	public partial class avTouchAppDelegate : UIApplicationDelegate
+	{
+		// This method is invoked when the application has loaded its UI and its ready to run
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			window.AddSubview (viewController.View);
+			window.MakeKeyAndVisible ();
+
+			return true;
+		}
+
+		// This method is required in iPhoneOS 3.0
+		public override void OnActivated (UIApplication application)
+		{
+		}
+	}
+}
+
--- AVTouchSample/Main.cs
+++ AVTouchSample/Main.cs
@@ -1,8 +1,3 @@
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Foundation;
 using UIKit;
 
 namespace avTouch
@@ -14,23 +9,5 @@ namespace avTouch
 			UIApplication.Main (args);
 		}
 	}
-
-	// The name AppDelegate is referenced in the MainWindow.xib file.
-	public partial class avTouchAppDelegate : UIApplicationDelegate
-	{
-		// This method is invoked when the application has loaded its UI and its ready to run
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-
-			window.AddSubview (viewController.View);
-			window.MakeKeyAndVisible ();
-
-			return true;
-		}
-
-		// This method is required in iPhoneOS 3.0
-		public override void OnActivated (UIApplication application)
-		{
-		}
-	}
+		
 }
