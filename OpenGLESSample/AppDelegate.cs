82a427bdfd598f34b9a32f6d8eb345f946c9c966
Dylan Kelly <dylan.kelly@xamarin.com>
2015-04-01 11:35:58 -0400

    Adjust FinishedLaunching() structure

---
+++ OpenGLESSample/AppDelegate.cs
@@ -0,0 +1,35 @@
+﻿using UIKit;
+using Foundation;
+
+namespace OpenGLESSample
+{
+	// The name AppDelegate is referenced in the MainWindow.xib file.
+	public partial class OpenGLESSampleAppDelegate : UIApplicationDelegate
+	{
+		// This method is invoked when the application has loaded its UI and its ready to run
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			// If you have defined a view, add it here:
+			// window.AddSubview (navigationController.View);
+
+			glView.AnimationInterval = 1.0 / 60.0;
+			glView.StartAnimation();
+
+			window.MakeKeyAndVisible ();
+
+			return true;
+		}
+
+		public override void OnResignActivation (UIApplication application)
+		{
+			glView.AnimationInterval = 1.0 / 5.0;
+		}
+
+		// This method is required in iPhoneOS 3.0
+		public override void OnActivated (UIApplication application)
+		{
+			glView.AnimationInterval = 1.0 / 60.0;
+		}
+	}
+}
+
