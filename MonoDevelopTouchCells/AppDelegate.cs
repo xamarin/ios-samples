ce3749d3e1d33ff1bf42913a0a88b49e158e0890
Dylan Kelly <dylan.kelly@xamarin.com>
2015-04-01 11:41:08 -0400

    Adjust FinishedLaunching() structure

---
+++ MonoDevelopTouchCells/AppDelegate.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Xml.Linq;
+using System.Linq;
+using Foundation;
+using UIKit;
+
+namespace MonoDevelopTouchCells
+{
+	// The name AppDelegate is referenced in the MainWindow.xib file.
+	public partial class AppDelegate : UIApplicationDelegate
+	{
+		DetailViewController detailViewController = new DetailViewController ();
+
+		// This method is invoked when the application has loaded its UI and its ready to run
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			this.InitializeTableData ();
+
+			navigationController.NavigationBar.Translucent = false;
+			// If you have defined a view, add it here:
+			// window.AddSubview (navigationController.View);
+
+			window.AddSubview (this.navigationController.View);
+
+			window.MakeKeyAndVisible ();
+
+			return true;
+		}
+
+		// This method is required in iPhoneOS 3.0
+		public override void OnActivated (UIApplication application)
+		{
+		}
+
+		public void ShowDetail (CustomCell cell)
+		{
+			this.detailViewController.ShowDetail (cell);
+			this.navigationController.PushViewController (
+				this.detailViewController, true);
+		}
+
+		void InitializeTableData ()
+		{
+			string source = Path.Combine (Directory.GetCurrentDirectory (), "data.xml");
+
+			source = "file://" + source;
+
+			XDocument xdoc = XDocument.Load (source);//Path.Combine(Directory.GetCurrentDirectory(), "data.xml"));
+
+			var items = from c in xdoc.Descendants ("item")
+				select new Item {
+				Title = (string)c.Element ("title"),
+				Checked = (bool)c.Element ("checked"),
+			};
+
+			myTableView.Delegate = new TableViewDelegate ();
+			myTableView.InvokeOnMainThread (delegate {
+				myTableView.DataSource = new DataSource (items);
+			});
+		}
+	}
+}
+
