80eeec8fbc41b0637806a355db4fcc6bec9e9a13
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-04-11 01:03:37 +0300

    [Chat] Respond to keyboard appearance 
    Xamarin.iOS 8.8.2.4

--- Chat/Chat/ChatViewController.cs
+++ Chat/Chat/ChatViewController.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Drawing;
 
-using Foundation;
 using UIKit;
+using Foundation;
+using CoreGraphics;
 
 namespace Chat
 {
 	public partial class ChatViewController : UIViewController
 	{
+		NSObject willShowToken;
+		NSObject willHideToken;
+
 		public ChatViewController (IntPtr handle)
 			: base (handle)
 		{
@@ -16,6 +20,38 @@ namespace Chat
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
+		}
+
+		public override void ViewWillAppear (bool animated)
+		{
+			base.ViewWillAppear (animated);
+
+			willShowToken = UIKeyboard.Notifications.ObserveWillShow (KeyboardWillShowHandler);
+			willHideToken = UIKeyboard.Notifications.ObserveWillHide (KeyboardWillHideHandler);
+		}
+
+		void KeyboardWillShowHandler (object sender, UIKeyboardEventArgs e)
+		{
+			UpdateButtomLayoutConstraint (e);
+		}
+
+		void KeyboardWillHideHandler (object sender, UIKeyboardEventArgs e)
+		{
+			UpdateButtomLayoutConstraint (e);
+		}
+
+		void UpdateButtomLayoutConstraint(UIKeyboardEventArgs e)
+		{
+			BottomConstraint.Constant = View.Bounds.GetMaxY () - e.FrameEnd.GetMinY ();
+			View.LayoutIfNeeded ();
+		}
+
+		public override void ViewWillDisappear (bool animated)
+		{
+			base.ViewWillDisappear (animated);
+
+			willShowToken.Dispose ();
+			willHideToken.Dispose ();
 		}
 	}
 }
--- Chat/Chat/ChatViewController.designer.cs
+++ Chat/Chat/ChatViewController.designer.cs
@@ -1,17 +1,26 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using Foundation;
+using System.CodeDom.Compiler;
 
 namespace Chat
 {
 	[Register ("ChatViewController")]
 	partial class ChatViewController
 	{
+		[Outlet]
+		UIKit.NSLayoutConstraint BottomConstraint { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (BottomConstraint != null) {
+				BottomConstraint.Dispose ();
+				BottomConstraint = null;
+			}
 		}
 	}
 }
