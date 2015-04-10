78b341dc8c44e73ee51153b126cd9578e053d3df
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-04-11 00:22:35 +0300

    [Chat] initial commit for BubbleCell replacement sample

---
+++ Chat/Chat/ChatViewController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+using Foundation;
+using UIKit;
+
+namespace Chat
+{
+	public partial class ChatViewController : UIViewController
+	{
+		public ChatViewController (IntPtr handle)
+			: base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+		}
+	}
+}
