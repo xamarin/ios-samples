56153a868f33216d3e8373cc09fd86b6faabffad
Amy Burns <amy.burns87@gmail.com>
2015-04-20 19:28:09 +0100

    [ScratchTicket]Add new sample
    Relates to Custom controls doc

---
+++ ScratchTicket/README.md
@@ -0,0 +1,4 @@
+Scratch Ticket
+==============
+
+This sample shows how to create and use a custom component with Xamarin's Designer for iOS. It is the related sample for the walkthrough – [Using Custom Controls with the Xamarin Designer for iOS](/guides/ios/user_interface/designer/ios_designable_controls_walkthrough/).
---
+++ ScratchTicket/ScratchTicket/ScratchTicketView.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.ComponentModel;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+
+namespace ScratchTicket
+{
+	[Register("ScratchTicketView"), DesignTimeVisible(true)]
+	public class ScratchTicketView : UIView
+	{
+		CGPath path;
+		CGPoint initialPoint;
+		CGPoint latestPoint;
+		bool startNewPath = false;
+		UIImage image;
+
+		[Export("Image"), Browsable(true)]
+		public UIImage Image
+		{
+			get { return image; }
+			set
+			{
+				image = value;
+				SetNeedsDisplay();
+			}
+		}
+
+		public ScratchTicketView(IntPtr p)
+			: base(p)
+		{
+			Initialize();
+		}
+
+		public ScratchTicketView()
+		{
+			Initialize();
+		}
+
+		void Initialize()
+		{
+			initialPoint = CGPoint.Empty;
+			latestPoint = CGPoint.Empty;
+			BackgroundColor = UIColor.Clear;
+			Opaque = false;
+			path = new CGPath();
+			SetNeedsDisplay();
+		}
+
+		public override void TouchesBegan(NSSet touches, UIEvent evt)
+		{
+			base.TouchesBegan(touches, evt);
+
+			var touch = touches.AnyObject as UITouch;
+
+			if (touch != null)
+			{
+				initialPoint = touch.LocationInView(this);
+			}
+		}
+
+		public override void TouchesMoved(NSSet touches, UIEvent evt)
+		{
+			base.TouchesMoved(touches, evt);
+
+			var touch = touches.AnyObject as UITouch;
+
+			if (touch != null)
+			{
+				latestPoint = touch.LocationInView(this);
+				SetNeedsDisplay();
+			}
+		}
+
+		public override void TouchesEnded(NSSet touches, UIEvent evt)
+		{
+			base.TouchesEnded(touches, evt);
+			startNewPath = true;
+		}
+
+		public override void Draw(CGRect rect)
+		{
+			base.Draw(rect);
+
+			using (var g = UIGraphics.GetCurrentContext())
+			{
+				if (image != null)
+					g.SetFillColor((UIColor.FromPatternImage(image).CGColor));
+				else
+					g.SetFillColor(UIColor.LightGray.CGColor);
+				g.FillRect(rect);
+
+				if (!initialPoint.IsEmpty)
+				{
+					g.SetLineWidth(20);
+					g.SetBlendMode(CGBlendMode.Clear);
+					UIColor.Clear.SetColor();
+
+					if (path.IsEmpty || startNewPath)
+					{
+						path.AddLines(new CGPoint[] { initialPoint, latestPoint });
+						startNewPath = false;
+					}
+					else
+					{
+						path.AddLineToPoint(latestPoint);
+					}
+
+					g.SetLineCap(CGLineCap.Round);
+					g.AddPath(path);		
+					g.DrawPath(CGPathDrawingMode.Stroke);
+				}
+			}
+		}
+	}
+}
+
