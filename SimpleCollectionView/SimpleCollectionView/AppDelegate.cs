eedf0b3803d00f34317e44af77ca1c240d131651
Aaron Sky <aaron.sky@xamarin.com>
2015-05-05 16:57:03 -0400

    Resolved warnings in SimpleCollectionView sample

--- SimpleCollectionView/SimpleCollectionView/AppDelegate.cs
+++ SimpleCollectionView/SimpleCollectionView/AppDelegate.cs
@@ -11,13 +11,14 @@ namespace SimpleCollectionView
     [Register ("AppDelegate")]
     public partial class AppDelegate : UIApplicationDelegate
     {
+		#pragma warning disable 414
         // class-level declarations
         UIWindow window;
         UICollectionViewController simpleCollectionViewController;
         CircleLayout circleLayout;
         LineLayout lineLayout;
         UICollectionViewFlowLayout flowLayout;
-
+		#pragma warning restore 414
         public override bool FinishedLaunching (UIApplication app, NSDictionary options)
         {
             window = new UIWindow (UIScreen.MainScreen.Bounds);
