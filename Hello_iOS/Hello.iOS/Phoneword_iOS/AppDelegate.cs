dcb3ab6d92329d09298a78a1b0e0fbb56aa29493
Dylan Kelly <dylan.kelly@xamarin.com>
2015-05-05 11:31:05 -0400

    Add Calabash.Start even if it already exists

--- Hello_iOS/Hello.iOS/Phoneword_iOS/AppDelegate.cs
+++ Hello_iOS/Hello.iOS/Phoneword_iOS/AppDelegate.cs
@@ -1,4 +1,4 @@
-﻿using Foundation;
+using Foundation;
 using UIKit;
 
 namespace Phoneword_iOS
@@ -17,6 +17,7 @@ namespace Phoneword_iOS
 
 		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 		{
+Xamarin.Calabash.Start();
 			// Override point for customization after application launch.
 			// If not required for your application you can safely delete this method
 			return true;
