ef5da227e1baffc2f5d172f10fa07170d2397a61
olegoid <olegoid91@gmail.com>
2015-07-21 21:52:22 +0300

    [MetalPerformanceShadersHelloWorld] Add MetalPerformanceShadersHelloWorld iOS 9 sample

---
+++ ios9/MetalPerformanceShadersHelloWorld/MetalPerformanceShadersHelloWorld/GameViewController.cs
@@ -0,0 +1,99 @@
+using CoreAnimation;
+using Foundation;
+using Metal;
+using MetalKit;
+using MetalPerformanceShaders;
+using UIKit;
+
+namespace MetalPerformanceShadersHelloWorld {
+	public partial class GameViewController : UIViewController, IMTKViewDelegate, INSCoding {
+
+		MTKView metalView;
+		IMTLCommandQueue commandQueue;
+		IMTLTexture sourceTexture;
+
+		[Export ("initWithCoder:")]
+		public GameViewController (NSCoder coder) : base (coder)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			metalView = (MTKView)View;
+
+			// Make sure the current device supports MetalPerformanceShaders.
+			if (!metalView.Device.SupportsFeatureSet (MTLFeatureSet.iOS_GPUFamily2_v1))
+				return;
+
+			MetalPerformanceShadersDisabledLabel.Hidden = true;
+			SetupView ();
+			SetupMetal ();
+			LoadAssets ();
+		}
+
+		void SetupMetal ()
+		{
+			// Set the view to use the default device.
+			metalView.Device = MTLDevice.SystemDefault;
+
+			// Create a new command queue.
+			commandQueue = metalView.Device.CreateCommandQueue ();
+		}
+
+		void SetupView ()
+		{
+			metalView.Delegate = this;
+
+			// Setup the render target, choose values based on your app.
+			metalView.DepthStencilPixelFormat = MTLPixelFormat.Stencil8 | MTLPixelFormat.Depth32Float;
+
+			// Set up pixel format as your input/output texture.
+			metalView.ColorPixelFormat = MTLPixelFormat.BGRA8Unorm;
+
+			// Allow to access to currentDrawable.texture write mode.
+			metalView.FramebufferOnly = false;
+		}
+
+		void LoadAssets ()
+		{
+			var textureLoader = new MTKTextureLoader (metalView.Device);
+			NSUrl url = NSBundle.MainBundle.GetUrlForResource ("AnimalImage", "png");
+
+			NSError error;
+			sourceTexture = textureLoader.TextureWithContentsOfUrl (url, null, out error);
+		}
+
+		void Render ()
+		{
+			// Create a new command buffer for each renderpass to the current drawable.
+			IMTLCommandBuffer commandBuffer = commandQueue.CommandBuffer ();
+
+			// Initialize MetalPerformanceShaders gaussianBlur with Sigma = 10.0f.
+			var gaussianblur = new MPSImageGaussianBlur (metalView.Device, 10f);
+
+			var drawable = ((CAMetalLayer)metalView.Layer).NextDrawable ();
+
+			// Run MetalPerformanceShader gaussianblur.
+			gaussianblur.EncodeToCommandBuffer (commandBuffer, sourceTexture, drawable.Texture);
+
+			// Schedule a present using the current drawable.
+			commandBuffer.PresentDrawable (drawable);
+			// Finalize command buffer.
+			commandBuffer.Commit ();
+			commandBuffer.WaitUntilCompleted ();
+
+			// To reuse a CAMetalDrawable object’s texture, you must deallocate the drawable after presenting it.
+			drawable.Dispose ();
+		}
+
+		public void WillLayout (MTKView view, CoreGraphics.CGSize size)
+		{
+			// Called whenever view changes orientation or layout is changed
+		}
+
+		public void Draw (MTKView view)
+		{
+			Render ();
+		}
+	}
+}
---
+++ ios9/MetalPerformanceShadersHelloWorld/MetalPerformanceShadersHelloWorld/GameViewController.designer.cs
@@ -0,0 +1,26 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace MetalPerformanceShadersHelloWorld
+{
+	[Register ("GameViewController")]
+	partial class GameViewController
+	{
+		[Outlet]
+		UIKit.UILabel MetalPerformanceShadersDisabledLabel { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (MetalPerformanceShadersDisabledLabel != null) {
+				MetalPerformanceShadersDisabledLabel.Dispose ();
+				MetalPerformanceShadersDisabledLabel = null;
+			}
+		}
+	}
+}
