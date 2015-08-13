4fb8e497dd7b6a8d90d38f9592e64de413664624
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-08-13 20:38:39 +0300

    [MTGKTapper] fix method names. remove system version check because we have higher target version

--- MTGKTapper/MTGKTapper/MTGKTapperViewController.cs
+++ MTGKTapper/MTGKTapper/MTGKTapperViewController.cs
@@ -8,17 +8,17 @@ namespace MTGKTapper
 {
 	public partial class MTGKTapperViewController : UIViewController
 	{
-		public const string EasyLeaderboardID = "com.appledts.EasyTapList";
-		public const string HardLeaderboardID = "com.appledts.HardTapList";
-		public const string AwesomeLeaderboardID = "com.appledts.AwesomeTapList";
+		const string EasyLeaderboardID = "com.appledts.EasyTapList";
+		const string HardLeaderboardID = "com.appledts.HardTapList";
+		const string AwesomeLeaderboardID = "com.appledts.AwesomeTapList";
 
-		public const string AchievementGotOneTap = "com.appletest.one_tap";
-		public const string AchievementHidden20Taps = "com.appledts.twenty_taps";
-		public const string AchievementBigOneHundred = "com.appledts.one_hundred_taps";
+		const string AchievementGotOneTap = "com.appletest.one_tap";
+		const string AchievementHidden20Taps = "com.appledts.twenty_taps";
+		const string AchievementBigOneHundred = "com.appledts.one_hundred_taps";
 
-		public const string AchievementNameGotOneTap = "Just One Tap";
-		public const string AchievementNameHidden20Taps = "Twenty Taps In";
-		public const string AchievementNameBigOneHundred = "The Big One Hundred";
+		const string AchievementNameGotOneTap = "Just One Tap";
+		const string AchievementNameHidden20Taps = "Twenty Taps In";
+		const string AchievementNameBigOneHundred = "The Big One Hundred";
 
 		GameCenterManager gameCenterManager;
 		GKLeaderboard currentLeaderBoard;
@@ -29,23 +29,15 @@ namespace MTGKTapper
 		{
 		}
 
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
-
-			// Release any cached data, images, etc that aren't in use.
-		}
-
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 
 			// Perform any additional setup after loading the view, typically from a nib.
-			initGameCenter ();
+			InitGameCenter ();
 
 			string[] categories = { "Easy", "Hard", "Awesome" };
-			UIActionSheet selectCategory = new UIActionSheet ("Choose Leaderboard", null, "Cancel", null, categories);
+			var selectCategory = new UIActionSheet ("Choose Leaderboard", null, "Cancel", null, categories);
 			selectCategory.Dismissed += (sender, e) => {
 				switch (e.ButtonIndex) {
 				case 0:
@@ -69,46 +61,38 @@ namespace MTGKTapper
 				default:
 					break;
 				}
-				currentLeaderBoard = gameCenterManager.reloadLeaderboard (currentCategory);
-				updateHighScore ();
+				currentLeaderBoard = gameCenterManager.ReloadLeaderboard (currentCategory);
+				UpdateHighScore ();
 			};
 
-			this.selectLeaderBoardButton.TouchUpInside += (sender, e) => {
-				selectCategory.ShowInView (this.View);
-			};
+			selectLeaderBoardButton.TouchUpInside += (sender, e) => selectCategory.ShowInView (this.View);
 
-			this.showLeaderboardButton.TouchUpInside += (sender, e) => {
-				GKLeaderboardViewController leaderboardController = new GKLeaderboardViewController ();
+			showLeaderboardButton.TouchUpInside += (sender, e) => {
+				var leaderboardController = new GKLeaderboardViewController ();
 				leaderboardController.Category = currentCategory;
 				leaderboardController.TimeScope = GKLeaderboardTimeScope.AllTime;
-				leaderboardController.DidFinish += (senderLeaderboard, eLeaderboard) => {
-					leaderboardController.DismissViewController (true, null);
-				};
+				leaderboardController.DidFinish += (senderLeaderboard, eLeaderboard) => leaderboardController.DismissViewController (true, null);
 				PresentViewController (leaderboardController, true, null);
 			};
 
-			this.showAchievementButton.TouchUpInside += (sender, e) => {
-				GKAchievementViewController achievementController = new GKAchievementViewController ();
-				achievementController.DidFinish += (senderAchievement, eAchievement) => {
-					achievementController.DismissViewController (true, null);
-				};
+			showAchievementButton.TouchUpInside += (sender, e) => {
+				var achievementController = new GKAchievementViewController ();
+				achievementController.DidFinish += (senderAchievement, eAchievement) => achievementController.DismissViewController (true, null);
 				PresentViewController (achievementController, true, null);
 			};
 
-			this.incrementScoreButton.TouchUpInside += (sender, e) => {
+			incrementScoreButton.TouchUpInside += (sender, e) => {
 				currentScore++;
 				currentScoreTextField.Text = currentScore.ToString ();
-				checkAchievements ();
+				CheckAchievements ();
 			};
 
-			this.submitScoreButton.TouchUpInside += (sender, e) => {
+			submitScoreButton.TouchUpInside += (sender, e) => {
 				if (currentScore > 0)
-					gameCenterManager.reportScore (currentScore, currentCategory, this);
+					gameCenterManager.ReportScore (currentScore, currentCategory, this);
 			};
 
-			this.resetButton.TouchUpInside += (sender, e) => {
-				gameCenterManager.resetAchievement ();
-			};
+			resetButton.TouchUpInside += (sender, e) => gameCenterManager.ResetAchievement ();
 		}
 
 		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation)
@@ -117,51 +101,31 @@ namespace MTGKTapper
 			return (toInterfaceOrientation != UIInterfaceOrientation.PortraitUpsideDown);
 		}
 
-		void initGameCenter ()
+		void InitGameCenter ()
 		{
-			if (GameCenterManager.isGameCenterAvailable ()) {
-				this.gameCenterManager = new GameCenterManager ();
-				setAuthenticateHandler ();
-			} else {
-				new UIAlertView ("Game Center Support Required", "The current device does not support Game Center, which this sample requires.", null, "OK", null).Show ();
-			}
+			gameCenterManager = new GameCenterManager ();
+			SetAuthenticateHandler ();
 		}
 
-		void setAuthenticateHandler ()
+		void SetAuthenticateHandler ()
 		{
-			if (UIDevice.CurrentDevice.CheckSystemVersion (6, 0)) {
-				GKLocalPlayer.LocalPlayer.AuthenticateHandler = (ui, error) => {
-					if (ui != null) {
-						this.PresentViewController (ui, true, null);
-					} else if (GKLocalPlayer.LocalPlayer.Authenticated) {
-						currentLeaderBoard = gameCenterManager.reloadLeaderboard (currentCategory);
-						updateHighScore ();
-					} else {
-						var alert = new UIAlertView ("Game Center Account Required", "Need login the game center!", null, "Retry", null);
-						alert.Clicked += (sender, e) => {
-							//GKLocalPlayer.LocalPlayer.Authenticated();
-						};
-						alert.Show ();
-					}
-				};
-			} else {
-				GKLocalPlayer.LocalPlayer.Authenticate (new Action<NSError> ((error) => {
-					if (GKLocalPlayer.LocalPlayer.Authenticated) {
-						currentLeaderBoard = gameCenterManager.reloadLeaderboard (currentCategory);
-						updateHighScore ();
-					} else {
-						var alert = new UIAlertView ("Game Center Account Required", "Need login the game center!", null, "Retry", null);
-						alert.Clicked += (sender, e) => {
-							setAuthenticateHandler ();
-						};
-						alert.Show ();
-
-					}
-				}));
-			}
+			GKLocalPlayer.LocalPlayer.AuthenticateHandler = (ui, error) => {
+				if (ui != null) {
+					PresentViewController (ui, true, null);
+				} else if (GKLocalPlayer.LocalPlayer.Authenticated) {
+					currentLeaderBoard = gameCenterManager.ReloadLeaderboard (currentCategory);
+					UpdateHighScore ();
+				} else {
+					var alert = new UIAlertView ("Game Center Account Required", "Need login the game center!", null, "Retry", null);
+					alert.Clicked += (sender, e) => {
+						//GKLocalPlayer.LocalPlayer.Authenticated();
+					};
+					alert.Show ();
+				}
+			};
 		}
 
-		public void updateHighScore ()
+		public void UpdateHighScore ()
 		{
 			currentLeaderBoard.LoadScores ((scoreArray, error) => {
 				if (error == null) {
@@ -183,7 +147,7 @@ namespace MTGKTapper
 			});
 		}
 
-		void checkAchievements ()
+		void CheckAchievements ()
 		{
 			string identifier = null;
 			string achievementName = null;
@@ -220,10 +184,9 @@ namespace MTGKTapper
 				achievementName = AchievementNameBigOneHundred;
 				break;
 			}
-			if (identifier != null) {
-				gameCenterManager.submitAchievement (identifier, percentComplete, achievementName);
-			}
+
+			if (identifier != null)
+				gameCenterManager.SubmitAchievement (identifier, percentComplete, achievementName);
 		}
 	}
 }
-
