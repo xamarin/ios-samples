5962af94750f913c88814da96554ff29902744ee
Rustam Zaitov <rustam.zaitov@xamarin.com>
2015-09-03 16:19:09 +0300

    [PhotoFilter] fix crash on iOS9 (window must have rootview controller)

--- ios8/PhotoFilter/PhotoFilter/AppDelegate.cs
+++ ios8/PhotoFilter/PhotoFilter/AppDelegate.cs
@@ -1,20 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-using Foundation;
 using UIKit;
-using CoreGraphics;
+using Foundation;
 
 namespace PhotoFilter
 {
 	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate
+	public class AppDelegate : UIApplicationDelegate
 	{
 		UIWindow window;
-		UILabel note;
 
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 		{
 			// check is it 64bit or 32bit
 			Console.WriteLine (IntPtr.Size);
@@ -23,22 +19,10 @@ namespace PhotoFilter
 			// Go to Photo > Edit then choose PhotoFilter to start app extension
 
 			window = new UIWindow (UIScreen.MainScreen.Bounds);
-			window.BackgroundColor = UIColor.White;
-
-			note = new UILabel ();
-			note.Text = "Note that the app in this sample only serves as a host for the extension. To use the sample extension, edit a photo or video using the Photos app, and tap the extension icon";
-			note.Lines = 0;
-			note.LineBreakMode = UILineBreakMode.WordWrap;
-			var frame = note.Frame;
-			note.Frame = new CGRect (0, 0, UIScreen.MainScreen.Bounds.Width * 0.75f, 0);
-			note.SizeToFit ();
-
-			window.AddSubview (note);
-			note.Center = window.Center;
+			window.RootViewController = new ViewController ();
 
 			window.MakeKeyAndVisible ();
 			return true;
 		}
 	}
 }
-
---
+++ ios8/PhotoFilter/PhotoFilter/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using UIKit;
+
+namespace PhotoFilter
+{
+	public class ViewController : UIViewController
+	{
+		UILabel note;
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Main app do nothing
+			// Go to Photo > Edit then choose PhotoFilter to start app extension
+
+			note = new UILabel {
+				Text = "Note that the app in this sample only serves as a host for the extension. To use the sample extension, edit a photo or video using the Photos app, and tap the extension icon",
+				Lines = 0,
+				LineBreakMode = UILineBreakMode.WordWrap,
+				TranslatesAutoresizingMaskIntoConstraints = false,
+			};
+			View.AddSubview (note);
+			View.BackgroundColor = UIColor.White;
+
+			View.AddConstraint (NSLayoutConstraint.Create (note, NSLayoutAttribute.CenterY, NSLayoutRelation.Equal, View, NSLayoutAttribute.CenterY, 1, 0));
+			View.AddConstraints (NSLayoutConstraint.FromVisualFormat ("|-[note]-|", (NSLayoutFormatOptions)0, "note", note));
+		}
+	}
+}
