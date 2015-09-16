c709434cd6089dcc7135063559d01c48688d3357
Kevin Mullins <kevin.mullins@xamarin.com>
2015-09-16 13:23:59 -0500

    iOS 9: MultiTask
    Added an iPad multitasking support ready Xamarin.iOS app with a custom
    hardware keyboard shortcut.

---
+++ ios9/MultiTask/MultiTask/DetailViewController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using UIKit;
+
+namespace MultiTask
+{
+	public partial class DetailViewController : UIViewController
+	{
+		public object DetailItem { get; set; }
+
+		public DetailViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public void SetDetailItem (object newDetailItem)
+		{
+			if (DetailItem != newDetailItem) {
+				DetailItem = newDetailItem;
+				
+				// Update the view
+				ConfigureView ();
+			}
+		}
+
+		void ConfigureView ()
+		{
+			// Update the user interface for the detail item
+			if (IsViewLoaded && DetailItem != null)
+				detailDescriptionLabel.Text = DetailItem.ToString ();
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+			ConfigureView ();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
+
