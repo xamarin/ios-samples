71c1145bce4a0cc6944e40e54de621d39d631027
Amy Burns <amy.burns87@gmail.com>
2015-12-10 16:18:28 -0800

    adding PausesLocationUpdatesAutomatically property

--- Location/Location/LocationManager.cs
+++ Location/Location/LocationManager.cs
@@ -8,13 +8,15 @@ namespace Location
 	{
 
 		protected CLLocationManager locMgr;
+
 		public event EventHandler<LocationUpdatedEventArgs> LocationUpdated = delegate { };
 
 		public LocationManager ()
 		{
-			this.locMgr = new CLLocationManager();
+			this.locMgr = new CLLocationManager ();
+			this.locMgr.PausesLocationUpdatesAutomatically = false;
+
 			// iOS 8 has additional permissions requirements
-			//
 			if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0)) {
 				locMgr.RequestAlwaysAuthorization (); // works in background
 				//locMgr.RequestWhenInUseAuthorization (); // only in foreground
@@ -30,11 +32,12 @@ namespace Location
 
 		}
 
-		public CLLocationManager LocMgr{
+		public CLLocationManager LocMgr {
 			get { return this.locMgr; }
 		}
 
-		public void StartLocationUpdates(){
+		public void StartLocationUpdates ()
+		{
 
 			// We need the user's permission for our app to use the GPS in iOS. This is done either by the user accepting
 			// the popover when the app is first launched, or by changing the permissions for the app in Settings
@@ -43,18 +46,19 @@ namespace Location
 				//set the desired accuracy, in meters
 				LocMgr.DesiredAccuracy = 1;
 
-				LocMgr.LocationsUpdated += (object sender, CLLocationsUpdatedEventArgs e) =>
-				{
+				LocMgr.LocationsUpdated += (object sender, CLLocationsUpdatedEventArgs e) => {
 					// fire our custom Location Updated event
 					LocationUpdated (this, new LocationUpdatedEventArgs (e.Locations [e.Locations.Length - 1]));
 				};
 
-				LocMgr.StartUpdatingLocation();
+				LocMgr.StartUpdatingLocation ();
 			}
 		}
 
 		//This will keep going in the background and the foreground
-		public void PrintLocation (object sender, LocationUpdatedEventArgs e) {
+		public void PrintLocation (object sender, LocationUpdatedEventArgs e)
+		{
+			
 			CLLocation location = e.Location;
 			Console.WriteLine ("Altitude: " + location.Altitude + " meters");
 			Console.WriteLine ("Longitude: " + location.Coordinate.Longitude);
