d0a3b8b02905b5ac9e74d425c4d0a26b773fd4cf
olegoid <olegoid91@gmail.com>
2016-02-05 18:48:21 -0300

    [WalkingGame] Update sample code

--- WalkingGameCompleteiOS/WalkingGame.iOS/Main.cs
+++ WalkingGameCompleteiOS/WalkingGame.iOS/Main.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using UIKit;
 
-using Foundation;
-using UIKit;
-
-namespace WalkingGame.iOS
-{
-	public class Application
-	{
+namespace WalkingGame.iOS {
+	public class Application {
 		// This is the main entry point of the application.
 		static void Main (string[] args)
 		{
