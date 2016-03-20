1913612fd48f35cc47584e7471da467044f5e10e
olegoid <olegoid91@gmail.com>
2016-03-20 18:11:17 -0300

    [AVCaptureFrames] Use CVOptionFlags in CVPixelBuffer.Lock: * Fixes 39794

--- AVCaptureFrames/AppDelegate.cs
+++ AVCaptureFrames/AppDelegate.cs
@@ -120,7 +120,7 @@ namespace avcaptureframes {
 				// Get the CoreVideo image
 				using (var pixelBuffer = sampleBuffer.GetImageBuffer () as CVPixelBuffer) {
 					// Lock the base address
-					pixelBuffer.Lock ((CVPixelBufferLock)0);
+					pixelBuffer.Lock (CVOptionFlags.None);
 					// Get the number of bytes per row for the pixel buffer
 					var baseAddress = pixelBuffer.BaseAddress;
 					var bytesPerRow = (int)pixelBuffer.BytesPerRow;
@@ -131,7 +131,7 @@ namespace avcaptureframes {
 					using (var cs = CGColorSpace.CreateDeviceRGB ()) {
 						using (var context = new CGBitmapContext (baseAddress, width, height, 8, bytesPerRow, cs, (CGImageAlphaInfo)flags)) {
 							using (CGImage cgImage = context.ToImage ()) {
-								pixelBuffer.Unlock ((CVPixelBufferLock)0);
+								pixelBuffer.Unlock (CVOptionFlags.None);
 								return UIImage.FromImage (cgImage);
 							}
 						}
