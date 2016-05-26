98580071c4981d81739b3a0c608a252327d68367
olegoid <olegoid91@gmail.com>
2016-05-25 22:37:18 -0300

    [ImageView] Code refactoring

--- iOS7-ui-updates/AppDelegate.cs
+++ iOS7-ui-updates/AppDelegate.cs
@@ -1,33 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 using Foundation;
 using UIKit;
 
 namespace ImageView {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the 
-	// User Interface of the application, as well as listening (and optionally responding) to 
-	// application events from iOS.
 	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate {
-		// class-level declarations
+	public class AppDelegate : UIApplicationDelegate {
 		UIWindow window;
 		UINavigationController navigationController;
 		UIViewController viewController;
 
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this 
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
 		{
 			viewController = new ImageViewController ();
-			
-			navigationController = new UINavigationController();
+
+			navigationController = new UINavigationController ();
 			navigationController.PushViewController (viewController, false);
 
 			window = new UIWindow (UIScreen.MainScreen.Bounds) {
@@ -35,10 +20,8 @@ namespace ImageView {
 			};
 
 			window.AddSubview (navigationController.View);
-			
-			// make the window visible
 			window.MakeKeyAndVisible ();
-			
+
 			return true;
 		}
 	}
--- iOS7-ui-updates/ImageViewController.cs
+++ iOS7-ui-updates/ImageViewController.cs
@@ -1,43 +1,27 @@
-using CoreGraphics;
-using UIKit;
 using System;
 
+using CoreGraphics;
+using UIKit;
+
 namespace ImageView {
-
 	public class ImageViewController : UIViewController {
-		
-		UIImageView imageView;
-
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 			Title = "ImageView";
 			View.BackgroundColor = UIColor.White;
-
 		}
 
 		public override void ViewDidLayoutSubviews ()
 		{
 			base.ViewDidLayoutSubviews ();
 
-			nfloat y = 10;
+			nfloat offset = 10f;
 
-			//EdgeForExtendedLayout example
-//			if (UIDevice.CurrentDevice.CheckSystemVersion (7, 0)) { 
-//				this.EdgesForExtendedLayout = UIRectEdge.None;
-//			}
+			var imageView = new UIImageView (UIImage.FromBundle ("MonkeySFO.png"));
+			imageView.Frame = new CGRect (offset, offset, imageView.Image.CGImage.Width, imageView.Image.CGImage.Height);
 
-			//TopLayoutGuide example
-//			if (UIDevice.CurrentDevice.CheckSystemVersion (7, 0)) { 
-//				y = this.TopLayoutGuide.Length;
-//
-//			}
-
-			// a simple image
-			imageView = new UIImageView (UIImage.FromBundle ("MonkeySFO.png")); // Build Action:Content
-			imageView.Frame = new CGRect (10, y, imageView.Image.CGImage.Width, imageView.Image.CGImage.Height);
 			View.AddSubview (imageView);
-
 		}
 	}
 }
