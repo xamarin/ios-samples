69153e371c7650401f7d764340da163e6965d5d1
olegoid <olegoid91@gmail.com>
2016-06-20 14:41:40 -0300

    [ZoomingPdfViewer] Missing parts of implementation

--- ZoomingPdfViewer/ModelController.cs
+++ ZoomingPdfViewer/ModelController.cs
@@ -1,10 +1,59 @@
 ﻿using System;
-namespace ZoomingPdfViewer
-{
-	public class ModelController
-	{
-		public ModelController()
+
+using Foundation;
+using CoreGraphics;
+using UIKit;
+
+namespace ZoomingPdfViewer {
+	public class ModelController : NSObject, IUIPageViewControllerDataSource {
+		readonly nint numberOfPages;
+		CGPDFDocument pdf;
+
+		public ModelController ()
 		{
+			NSUrl pdfURL = NSBundle.MainBundle.GetUrlForResource ("Tamarin", "pdf");
+			pdf = CGPDFDocument.FromUrl (pdfURL.AbsoluteString);
+			numberOfPages = pdf.Pages;
+			if (numberOfPages % 2 != 0)
+				numberOfPages++;
+		}
+
+		public DataViewController GetViewController (nint index, UIStoryboard storyboard)
+		{
+			var dataViewController = (DataViewController)storyboard.InstantiateViewController ("DataViewController");
+			dataViewController.PageNumber = index + 1;
+			dataViewController.Pdf = pdf;
+			return dataViewController;
+		}
+
+		public nint IndexOfViewController (DataViewController viewController)
+		{
+			return viewController.PageNumber - 1;
+		}
+
+		public UIViewController GetNextViewController (UIPageViewController pageViewController, UIViewController referenceViewController)
+		{
+			nint index = IndexOfViewController ((DataViewController)referenceViewController);
+
+			if (index == 0)
+				return null;
+
+			index--;
+			return GetViewController (index, referenceViewController.Storyboard);
+		}
+
+		public UIViewController GetPreviousViewController (UIPageViewController pageViewController, UIViewController referenceViewController)
+		{
+			nint index = IndexOfViewController ((DataViewController)referenceViewController);
+
+			if (index == 0)
+				return null;
+
+			index++;
+			if (index == numberOfPages)
+				return null;
+
+			return GetViewController (index, referenceViewController.Storyboard);
 		}
 	}
 }
