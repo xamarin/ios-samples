3eb9e0f911d4d06e3b2de612bb0e6379bf5e2037
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-06-21 21:33:13 +0300

    [CloudKitAtlas] port SubmenuTableViewController

--- ios8/CloudKitAtlas/CloudKitAtlas/Menu/SubmenuTableViewController.cs
+++ ios8/CloudKitAtlas/CloudKitAtlas/Menu/SubmenuTableViewController.cs
@@ -10,8 +10,59 @@ namespace CloudKitAtlas
 		public string GroupTitle { get; set; }
 		public CodeSample [] CodeSamples { get; set; }
 
-		public SubmenuTableViewController (IntPtr handle) : base (handle)
+		public SubmenuTableViewController (IntPtr handle)
+			: base (handle)
 		{
 		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			var groupTitle = GroupTitle;
+			if (!string.IsNullOrWhiteSpace (groupTitle))
+				NavigationItem.Title = groupTitle;
+
+			NavigationItem.HidesBackButton = NavigationController.ViewControllers [0].NavigationItem.HidesBackButton;
+		}
+
+		#region Table view data source
+
+		public override nint NumberOfSections (UITableView tableView)
+		{
+			return 1;
+		}
+
+		public override nint RowsInSection (UITableView tableView, nint section)
+		{
+			return CodeSamples.Length;
+		}
+
+		public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
+		{
+			var codeSample = CodeSamples [indexPath.Row];
+			var cell = (SubmenuTableViewCell)tableView.DequeueReusableCell ("SubmenuItem", indexPath);
+			cell.SubmenuLabel.Text = codeSample.Title;
+
+			return cell;
+		}
+
+		#endregion
+
+		#region Navigation
+
+		public override void PrepareForSegue (UIStoryboardSegue segue, NSObject sender)
+		{
+			if (segue.Identifier == "ShowCodeSampleFromSubmenu") {
+				var codeSampleViewController = (CodeSampleViewController)segue.DestinationViewController;
+				var selectedCell = sender as SubmenuTableViewCell;
+				if (selectedCell != null) {
+					var indexPath = TableView.IndexPathForCell (selectedCell);
+					codeSampleViewController.SelectedCodeSample = CodeSamples [indexPath.Row];
+				}
+			}
+		}
+
+		#endregion
 	}
 }
