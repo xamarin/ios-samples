74d3ea1e5fbeabcf95501629482aace4929368bb
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-06-21 17:43:36 +0300

    [CloudKitAtlas] port DiscoverUserInfoWithUserRecordIdSample

--- ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Discoverability/DiscoverUserInfoWithUserRecordIdSample.cs
+++ ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Discoverability/DiscoverUserInfoWithUserRecordIdSample.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Threading.Tasks;
+using CloudKit;
 
 namespace CloudKitAtlas
 {
 	public class DiscoverUserInfoWithUserRecordIdSample : CodeSample
 	{
 		public DiscoverUserInfoWithUserRecordIdSample ()
+			: base (title: "discoverUserInfoWithUserRecordID",
+					className: "CKContainer",
+					methodName: ".discoverUserInfoWithUserRecordID()", // TODO: fix method name
+					descriptionKey: "Discoverability.DiscoverUserInfoWithUserRecordID",
+					inputs: new Input [] {
+						new TextInput ("recordName", string.Empty, isRequired: true),
+						new TextInput ("zoneName", CKRecordZone.DefaultName, isRequired: true)
+					})
 		{
 		}
 
-		public override Task<Results> Run ()
+		public async override Task<Results> Run ()
 		{
-			throw new NotImplementedException ();
+			object recordName, zoneName;
+			if (Data.TryGetValue ("recordName", out recordName) && Data.TryGetValue ("zoneName", out zoneName)) {
+				var container = CKContainer.DefaultContainer;
+				var zoneId = new CKRecordZoneID ((string)zoneName, CKContainer.OwnerDefaultName);
+				var userRecordID = new CKRecordID ((string)recordName, zoneId);
+
+				CKDiscoveredUserInfo userInfo = await container.DiscoverUserInfoAsync (userRecordID);
+				return new Results (new IResult [] { new CKDiscoveredUserInfoWrapper (userInfo) });
+			}
+			throw new InvalidProgramException ("there are no recordName and zoneName");
 		}
 	}
 }
-
--- ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Discoverability/RequestApplicationPermissionSample.cs
+++ ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Discoverability/RequestApplicationPermissionSample.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+
 using CloudKit;
-using Foundation;
 
 namespace CloudKitAtlas
 {
