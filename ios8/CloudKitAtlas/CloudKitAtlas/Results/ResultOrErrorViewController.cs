d2072877f818a35bff50b73d53d5568a61a13526
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-06-22 16:24:41 +0300

    [CloudKitAtlas] port ResultOrErrorViewController

---
+++ ios8/CloudKitAtlas/CloudKitAtlas/Results/ResultOrErrorViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using UIKit;
+
+namespace CloudKitAtlas
+{
+	public class ResultOrErrorViewController : UIViewController
+	{
+		protected UIBarButtonItem DoneButton { get; set; }
+
+		protected bool IsDrilldown { get; set; } = false;
+
+		public ResultOrErrorViewController (IntPtr handle)
+			: base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			NavigationItem.Title = "Error";
+			DoneButton = new UIBarButtonItem ("Done", UIBarButtonItemStyle.Done, BackToCodeSample);
+
+			if (!IsDrilldown) {
+				NavigationItem.HidesBackButton = true;
+				NavigationItem.RightBarButtonItem = DoneButton;
+			}
+		}
+
+		void BackToCodeSample (object sender, EventArgs e)
+		{
+			DismissViewController (true, null);
+		}
+	}
+}
