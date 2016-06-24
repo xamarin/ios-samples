8bfb4791273d3f9ead998390e7b1e3f6b47e2336
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-06-24 16:46:37 +0300

    [CloudKitAtlas] put FetchAllRecordZonesSample code in right place

--- ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Zones/SaveRecordZoneSample.cs
+++ ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Zones/SaveRecordZoneSample.cs
@@ -8,29 +8,11 @@ namespace CloudKitAtlas
 	public class SaveRecordZoneSample : CodeSample
 	{
 		public SaveRecordZoneSample ()
-			: base (title: "FetchAllRecordZones",
-					className: "CKDatabase",
-					methodName: ".FetchAllRecordZones()",
-					descriptionKey: "Zones.FetchAllRecordZones")
 		{
 		}
 
 		public async override Task<Results> Run ()
 		{
-			var container = CKContainer.DefaultContainer;
-			var privateDB = container.PrivateCloudDatabase;
-
-			var zones = await privateDB.FetchAllRecordZonesAsync ();
-			var results = new Results (alwaysShowAsList: true);
-
-			if(zones != null && zones.Length > 0) {
-				results.Items.AddRange (zones.Select (z => new CKRecordZoneWrapper (z)));
-				ListHeading = "Zones:";
-			} else {
-				ListHeading = "No Zones";
-			}
-
-			return results;
 		}
 	}
 }
