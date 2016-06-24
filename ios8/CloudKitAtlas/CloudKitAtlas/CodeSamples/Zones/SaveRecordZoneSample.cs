dcddf513eb690892247f7aa8fbf350c33e6538b2
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-06-24 16:54:09 +0300

    [CloudKitAtlas] port SaveRecordZoneSample

--- ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Zones/SaveRecordZoneSample.cs
+++ ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Zones/SaveRecordZoneSample.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 using CloudKit;
 
@@ -8,11 +7,35 @@ namespace CloudKitAtlas
 	public class SaveRecordZoneSample : CodeSample
 	{
 		public SaveRecordZoneSample ()
+			: base (title: "SaveRecordZone",
+					className: "CKDatabase",
+					methodName: ".SaveRecordZone()",
+					descriptionKey: "Zones.SaveRecordZone",
+					inputs: new Input [] {
+						new TextInput (label: "zoneName", value: string.Empty, isRequired: true)
+					})
 		{
 		}
 
 		public async override Task<Results> Run ()
 		{
+			object obj;
+			Data.TryGetValue ("zoneName", out obj);
+			var zoneName = obj as string;
+
+			if (zoneName == null)
+				return null;
+
+			var container = CKContainer.DefaultContainer;
+			var privateDB = container.PrivateCloudDatabase;
+
+			CKRecordZone recordZone = await privateDB.SaveRecordZoneAsync (new CKRecordZone (zoneName));
+			var results = new Results ();
+
+			if (recordZone != null)
+				results.Items.Add (new CKRecordZoneWrapper (recordZone));
+
+			return results;
 		}
 	}
 }
