7c6d83a581a42319e34420f2c693dc25a46747cb
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-06-27 16:22:35 +0300

    [CloudKitAtlas] port DeleteSubscriptionSample

--- ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Subscriptions/DeleteSubscriptionSample.cs
+++ ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Subscriptions/DeleteSubscriptionSample.cs
@@ -1,18 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using CloudKit;
 
 namespace CloudKitAtlas
 {
+	class SubscriptionIDResult : IResult
+	{
+		readonly string subscriptionId;
+
+		public string SummaryField { get; set; }
+
+		public List<AttributeGroup> AttributeList {
+			get {
+				return new List<AttributeGroup> {
+					new AttributeGroup (title: string.Empty, attributes: new Attribute[] {
+						new Attribute (key: "subscriptionID", value: subscriptionId)
+					})
+				};
+			}
+		}
+
+		public SubscriptionIDResult  (string subscriptionId)
+		{
+			this.subscriptionId = subscriptionId;
+		}
+	}
+
 	public class DeleteSubscriptionSample : CodeSample
 	{
 		public DeleteSubscriptionSample ()
+			: base (title: "DeleteSubscription",
+					className: "CKDatabase",
+					methodName: ".DeleteSubscription()",
+					descriptionKey: "Subscriptions.DeleteSubscription",
+					inputs: new Input [] {
+					new TextInput (label: "subscriptionID", value: string.Empty, isRequired: true)
+					})
 		{
 		}
 
-		public override Task<Results> Run ()
+		public async override Task<Results> Run ()
 		{
-			throw new NotImplementedException ();
+			string subscriptionID;
+			if (!TryGetString ("subscriptionID", out subscriptionID))
+				throw new InvalidProgramException ();
+
+			var container = CKContainer.DefaultContainer;
+			var privateDB = container.PrivateCloudDatabase;
+
+			var id = await privateDB.DeleteSubscriptionAsync (subscriptionID);
+			var results = new Results ();
+
+			if (id != null)
+				results.Items.Add (new SubscriptionIDResult (id));
+
+			return results;
 		}
 	}
 }
-
