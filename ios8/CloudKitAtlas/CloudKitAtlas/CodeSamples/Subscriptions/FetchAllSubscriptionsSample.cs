d29c6818ee3db57e7228f885a93da39860c1a444
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-06-27 16:06:19 +0300

    [CloudKitAtlas] port FetchAllSubscriptionsSample

--- ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Subscriptions/FetchAllSubscriptionsSample.cs
+++ ios8/CloudKitAtlas/CloudKitAtlas/CodeSamples/Subscriptions/FetchAllSubscriptionsSample.cs
@@ -1,18 +1,38 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading.Tasks;
+
+using CloudKit;
 
 namespace CloudKitAtlas
 {
 	public class FetchAllSubscriptionsSample : CodeSample
 	{
 		public FetchAllSubscriptionsSample ()
+			: base (title: "FetchAllSubscriptions",
+					className: "CKDatabase",
+					methodName: ".FetchAllSubscriptions()",
+					descriptionKey: "Subscriptions.FetchAllSubscriptions")
 		{
 		}
 
-		public override Task<Results> Run ()
+		public async override Task<Results> Run ()
 		{
-			throw new NotImplementedException ();
+			var container = CKContainer.DefaultContainer;
+			var privateDB = container.PrivateCloudDatabase;
+
+			var subscriptions = await privateDB.FetchAllSubscriptionsAsync ();
+			var results = new Results (alwaysShowAsList: true);
+
+			if (subscriptions != null) {
+				if (subscriptions.Length == 0) {
+					ListHeading = "No Subscriptions";
+				} else {
+					ListHeading = "Subscriptions:";
+					results.Items.AddRange (subscriptions.Select (s => new CKSubscriptionWrapper (s)));
+				}
+			}
+
+			return results;
 		}
 	}
 }
-
