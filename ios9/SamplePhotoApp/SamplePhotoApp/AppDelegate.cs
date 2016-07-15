11b92ec5bab9746640f2e72355fb755588b0bb54
Rajneesh <Rajneesh@360-MACMINIs-Mac-mini.local>
2016-07-15 23:51:41 +0530

    Sample Photo App Automated

--- ios9/SamplePhotoApp/SamplePhotoApp/AppDelegate.cs
+++ ios9/SamplePhotoApp/SamplePhotoApp/AppDelegate.cs
@@ -5,6 +5,18 @@ namespace SamplePhotoApp {
 	[Register ("AppDelegate")]
 	public class AppDelegate : UIApplicationDelegate {
 		public override UIWindow Window { get; set; }
+
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			// Override point for customization after application launch.
+			// If not required for your application you can safely delete this method
+
+			// Code to start the Xamarin Test Cloud Agent
+
+			Xamarin.Calabash.Start ();
+
+			return true;
+		}
 	}
 }
 
