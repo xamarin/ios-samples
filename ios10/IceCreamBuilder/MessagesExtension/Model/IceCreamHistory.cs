e2734f9ab336651a644ec26ca043991e1c75e59a
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-07-22 18:08:38 +0300

    [IceCreamBuilder] use Linq instead of loops

--- ios10/IceCreamBuilder/MessagesExtension/Model/IceCreamHistory.cs
+++ ios10/IceCreamBuilder/MessagesExtension/Model/IceCreamHistory.cs
@@ -1,50 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-
+using System.Linq;
 using Foundation;
+using Newtonsoft.Json;
 
 namespace MessagesExtension {
 	public class IceCreamHistory : IEnumerable<IceCream> {
 		const string UserDefaultsKey = "iceCreams";
 
-		List<IceCream> IceCreams { get; set; }
+		readonly List<IceCream> iceCreams;
 
 		public int Count {
 			get {
-				return IceCreams.Count;
+				return iceCreams.Count;
 			}
 		}
 
 		public IceCream this [int i] {
 			get {
-				return IceCreams [i];
+				return iceCreams [i];
 			}
 			set {
-				IceCreams.Insert (i, value);
+				iceCreams.Insert (i, value);
 			}
 		}
 
-		IceCreamHistory (IceCream[] iceCreams)
+		IceCreamHistory (List<IceCream> iceCreams)
 		{
-			IceCreams = new List<IceCream> (iceCreams);
+			this.iceCreams = iceCreams;
 		}
 
 		public static IceCreamHistory Load ()
 		{
-			var iceCreams = new List<IceCream> ();
 			var defaults = NSUserDefaults.StandardUserDefaults;
+			var serializedList = defaults.StringForKey (UserDefaultsKey);
 
-			var serializedList = defaults.ValueForKey ((NSString)UserDefaultsKey) as NSString;
+			var savedIceCreams = string.IsNullOrWhiteSpace (serializedList)
+									   ? new string [0]
+									   : JsonConvert.DeserializeObject<string []> (serializedList);
 
-			var savedIceCreams = new string [0];
-			if (!string.IsNullOrEmpty (serializedList))
-				savedIceCreams = Newtonsoft.Json.JsonConvert.DeserializeObject<string[]> (serializedList);
-
-			foreach (var savedIceCream in savedIceCreams) {
-				var url = new NSUrl (savedIceCream);
-				var components = NSUrlComponents.FromUrl (url, false);
-				iceCreams.Add (new IceCream (components.QueryItems));
-			}
+			var iceCreams = savedIceCreams.Select (sic => {
+				var components = NSUrlComponents.FromUrl (new NSUrl (sic), false);
+				return new IceCream (components.QueryItems);
+			}).ToList ();
 
 			if (iceCreams.Count == 0) {
 				iceCreams.Add (new IceCream (BaseType.base01, ScoopsType.scoops05, ToppingType.topping09));
@@ -53,39 +51,31 @@ namespace MessagesExtension {
 				iceCreams.Add (new IceCream (BaseType.base02, ScoopsType.scoops03, ToppingType.topping10));
 				iceCreams.Add (new IceCream (BaseType.base01, ScoopsType.scoops01, ToppingType.topping05));
 
-				var historyToSave = new IceCreamHistory (iceCreams.ToArray ());
+				var historyToSave = new IceCreamHistory (iceCreams);
 				historyToSave.Save ();
 			}
 
-			return new IceCreamHistory (iceCreams.ToArray());
+			return new IceCreamHistory (iceCreams);
 		}
 
 		public void Append (IceCream item)
 		{
-			IceCreams.Add (item);
+			iceCreams.Add (item);
 		}
 
 		public void Save ()
 		{
-			var iceCreamsToSave = IceCreams;
-			var iceCreamURLStrings = new List<string> ();
+			var iceCreamUrls = iceCreams.Select (ic => new NSUrlComponents { QueryItems = ic.QueryItems }.Url.AbsoluteString)
+			                            .ToArray ();
 
-			foreach (var iceCream in iceCreamsToSave) {
-				var components = new NSUrlComponents {
-					QueryItems = iceCream.QueryItems
-				};
-
-				iceCreamURLStrings.Add (components.Url.AbsoluteString);
-			}
-
+			var serializedList = JsonConvert.SerializeObject (iceCreamUrls);
 			var defaults = NSUserDefaults.StandardUserDefaults;
-			var serializedList = Newtonsoft.Json.JsonConvert.SerializeObject (iceCreamURLStrings);
 			defaults.SetString (serializedList, UserDefaultsKey);
 		}
 
 		public IEnumerator<IceCream> GetEnumerator ()
 		{
-			return IceCreams.GetEnumerator ();
+			return iceCreams.GetEnumerator ();
 		}
 
 		IEnumerator IEnumerable.GetEnumerator ()
