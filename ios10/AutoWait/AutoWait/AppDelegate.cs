3d623d59d30c6bb1e7616bdbaccc8e46836cace8
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-08-08 17:37:39 +0300

    [AutoWait] initial commit

---
+++ ios10/AutoWait/AutoWait/AppDelegate.cs
@@ -0,0 +1,21 @@
+﻿using Foundation;
+using UIKit;
+
+namespace AutoWait
+{
+	[Register ("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		// class-level declarations
+
+		public override UIWindow Window {
+			get;
+			set;
+		}
+
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			return true;
+		}
+	}
+}
