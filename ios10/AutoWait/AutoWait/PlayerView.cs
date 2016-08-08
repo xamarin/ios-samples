86fba119bc920e6a314ccd3b9304b6c0bf24ce82
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-08-08 18:00:07 +0300

    [AutoWait] port PlayerView

--- ios10/AutoWait/AutoWait/PlayerView.cs
+++ ios10/AutoWait/AutoWait/PlayerView.cs
@@ -2,12 +2,33 @@ using System;
 
 using UIKit;
 using Foundation;
+using ObjCRuntime;
+using AVFoundation;
 
 namespace AutoWait
 {
 	public partial class PlayerView : UIView
 	{
-		public PlayerView (IntPtr handle) : base (handle)
+		static Class layerClass;
+
+		public static Class LayerClass {
+			[Export ("layerClass")]
+			get {
+				return layerClass = layerClass ?? new Class (typeof (AVPlayerLayer));
+			}
+		}
+
+		public AVPlayer Player {
+			set {
+				((AVPlayerLayer)Layer).Player = value;
+			}
+			get {
+				return ((AVPlayerLayer)Layer).Player;
+			}
+		}
+
+		public PlayerView (IntPtr handle)
+			: base (handle)
 		{
 		}
 	}
