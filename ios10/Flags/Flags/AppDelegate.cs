b300395483d4e26684fdcee14ac5340b0cd5be0b
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-08-09 15:51:36 +0300

    [Flags] initial commit

---
+++ ios10/Flags/Flags/AppDelegate.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using UIKit;
+
+namespace Flags
+{
+	[Register ("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		public override UIWindow Window {
+			get;
+			set;
+		}
+
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			return true;
+		}
+	}
+}
