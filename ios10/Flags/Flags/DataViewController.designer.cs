53fbf9a1981f569a53fd6225ccbc1a28ae73b78c
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-08-12 14:16:02 +0300

    [Flags] add arabic strings file

--- ios10/Flags/Flags/DataViewController.designer.cs
+++ ios10/Flags/Flags/DataViewController.designer.cs
@@ -1,20 +1,35 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
+using UIKit;
 
 namespace Flags
 {
-	[Register ("DataViewController")]
-	partial class DataViewController
-	{
-		
-		void ReleaseDesignerOutlets ()
-		{
-		}
-	}
+    [Register ("DataViewController")]
+    partial class DataViewController
+    {
+        void ReleaseDesignerOutlets ()
+        {
+            if (answerLabel != null) {
+                answerLabel.Dispose ();
+                answerLabel = null;
+            }
+
+            if (flagLabel != null) {
+                flagLabel.Dispose ();
+                flagLabel = null;
+            }
+
+            if (revealButton != null) {
+                revealButton.Dispose ();
+                revealButton = null;
+            }
+        }
+    }
 }
