1c37d7782cd2f7bb0e0f88ab7f4fb183a5d2d03f
olegoid <olegoid91@gmail.com>
2016-08-23 12:51:56 -0300

    [Sound] Fix #43584 + remove redundant s

--- Sound/Sound/AppDelegate.cs
+++ Sound/Sound/AppDelegate.cs
@@ -1,21 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 using Foundation;
 using UIKit;
 
 namespace Sound
 {
 	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate
+	public class AppDelegate : UIApplicationDelegate
 	{
 		public override UIWindow Window { get; set; }
-
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-			return true;
-		}
 	}
 }
 
