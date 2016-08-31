72ef5e19a5831d67a6749373df243ba0c1327fd7
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-08-31 22:46:20 +0300

    [WatchPuzzle] remove custom module from storyboard file

--- ios10/WatchPuzzle/WatchPuzzle.WatchKitAppExtension/InterfaceController.cs
+++ ios10/WatchPuzzle/WatchPuzzle.WatchKitAppExtension/InterfaceController.cs
@@ -58,7 +58,7 @@ namespace WatchPuzzle.WatchKitAppExtension
 			};
 		}
 
-		public class InterfaceController : WKInterfaceController
+		public partial class InterfaceController : WKInterfaceController
 		{
 			[Outlet ("sceneInterface")]
 			WKInterfaceSCNScene sceneInterface { get; set; }
