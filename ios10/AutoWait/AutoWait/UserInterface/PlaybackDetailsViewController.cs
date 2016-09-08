a119ea5cb4ba3c56880086a0f4fbbed212175474
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-09-08 20:31:29 +0300

    [AutoWait] port PlaybackDetailsViewController

--- ios10/AutoWait/AutoWait/UserInterface/PlaybackDetailsViewController.cs
+++ ios10/AutoWait/AutoWait/UserInterface/PlaybackDetailsViewController.cs
@@ -8,11 +8,101 @@ namespace AutoWait
 {
 	public partial class PlaybackDetailsViewController : UIViewController
 	{
-		// TODOL port
-		public AVPlayer Player { get; set; }
+		[Outlet("playerView")]
+		PlayerView PlayerView { get; set; }
 
-		public PlaybackDetailsViewController (IntPtr handle) : base (handle)
+		[Outlet("waitingIndicatorView")]
+		UIView WaitingIndicatorView { get; set; }
+
+		[Outlet ("pauseButton")]
+		UIButton PauseButton { get; set; }
+
+		[Outlet("playButton")]
+		UIButton PlayButton { get; set; }
+
+		[Outlet ("playImmediatelyButton")]
+		UIButton PlayImmediatelyButton { get; set; }
+
+		[Outlet ("automaticWaitingSwitch")]
+		UISwitch AutomaticWaitingSwitch { get; set; }
+
+		AVPlayer player;
+		public AVPlayer Player {
+			get {
+				return player;
+			}
+			set {
+				player = value;
+
+				if (PlayerView != null)
+					PlayerView.Player = player;
+
+				// Make sure the players automaticallyWaitsToMinimizeStalling follows the switch in the UI.
+				if (player != null && IsViewLoaded)
+					AutomaticWaitingSwitch.On = player.AutomaticallyWaitsToMinimizeStalling;
+			}
+		}
+
+		public PlaybackDetailsViewController (IntPtr handle)
+			: base (handle)
 		{
 		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Load value for the automatic waiting switch from user defaults.
+			AutomaticWaitingSwitch.On = NSUserDefaults.StandardUserDefaults.BoolForKey ("disableAutomaticWaiting");
+			if (Player != null)
+				Player.AutomaticallyWaitsToMinimizeStalling = AutomaticWaitingSwitch.On;
+
+			if (PlayerView != null)
+				PlayerView.Player = player;
+
+			// We will use this to toggle our waiting indicator view.
+			// TODO: add observer
+			//addObserver (self, forKeyPath: #keyPath(PlaybackViewController.player.reasonForWaitingToPlay), options: [.new, .initial], context: &observerContext)
+		}
+
+		protected override void Dispose (bool disposing)
+		{
+			// TODO: unsubscribe
+			base.Dispose (disposing);
+		}
+
+		#region Actions
+
+		[Action ("toggleAutomaticWaiting:")]
+		void toggleAutomaticWaiting (UISwitch sender)
+		{
+			// Check for the new value of the switch and update AVPlayer property and user defaults
+			if (Player != null)
+				Player.AutomaticallyWaitsToMinimizeStalling = AutomaticWaitingSwitch.On;
+
+			NSUserDefaults.StandardUserDefaults.SetBool (AutomaticWaitingSwitch.On, "disableAutomaticWaiting");
+		}
+
+		[Action ("pause:")]
+		void Pause (NSObject sender)
+		{
+			Player?.Pause ();
+		}
+
+		[Action ("play:")]
+		void Play (NSObject sender)
+		{
+			Player.Play ();
+		}
+
+		[Action ("playImmediately:")]
+		void PlayImmediately (NSObject sender)
+		{
+			Player?.PlayImmediatelyAtRate (1);
+		}
+
+		#endregion
+
+		// TODO: do KVO here
 	}
 }
