d05810255bfd15065990fafbfc517e73aa9636c5
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-09-19 19:46:58 +0300

    [SamplePhotoApp] replace storyboard

--- ios9/SamplePhotoApp/SamplePhotoApp/Main.cs
+++ ios9/SamplePhotoApp/SamplePhotoApp/Main.cs
@@ -1,12 +1,11 @@
 ﻿using UIKit;
 
-namespace SamplePhotoApp {
-	public class Application {
-		// This is the main entry point of the application.
+namespace SamplePhotoApp
+{
+	public class Application
+	{
 		static void Main (string[] args)
 		{
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
 			UIApplication.Main (args, null, "AppDelegate");
 		}
 	}
