b88fa6332e82bd93a837952cc21cc02b5f2c670a
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-09-22 16:27:21 +0300

    [AVCam] start port PhotoCaptureDelegate

--- AVCam/AVCam/PhotoCaptureDelegate.cs
+++ AVCam/AVCam/PhotoCaptureDelegate.cs
@@ -1,14 +1,43 @@
 ﻿using System;
 using AVFoundation;
+using Foundation;
 
 namespace AVCam
 {
 	public class PhotoCaptureDelegate : IAVCapturePhotoCaptureDelegate
 	{
-		public AVCapturePhotoSettings RequestedPhotoSettings { get; private set; } 
+		public AVCapturePhotoSettings RequestedPhotoSettings { get; private set; }
 
-		public PhotoCaptureDelegate (AVCapturePhotoSettings settings, Action a1, Action<bool> a2, Action<PhotoCaptureDelegate> a3)
+		Action willCapturePhotoAnimation;
+		Action<bool> capturingLivePhoto;
+		Action<PhotoCaptureDelegate> completed;
+
+		NSData photoData;
+		NSUrl livePhotoCompanionMovieURL; // TODO: rename URL -> Url
+
+		public PhotoCaptureDelegate (AVCapturePhotoSettings settings,
+									 Action willCapturePhotoAnimation,
+									 Action<bool> capturingLivePhoto,
+									 Action<PhotoCaptureDelegate> completed)
 		{
+			RequestedPhotoSettings = settings;
+			this.willCapturePhotoAnimation = willCapturePhotoAnimation;
+			this.capturingLivePhoto = capturingLivePhoto;
+			this.completed = completed;
+		}
+
+		void DidFinish ()
+		{
+			var path = livePhotoCompanionMovieURL?.Path;
+			if (path != null) {
+				if (NSFileManager.DefaultManager.FileExists (path)) {
+					NSError error;
+					if (!NSFileManager.DefaultManager.Remove (path, out error))
+						Console.WriteLine ($"Could not remove file at url: {path}");
+				}
+			}
+
+			completed (this);
 		}
 	}
 }
