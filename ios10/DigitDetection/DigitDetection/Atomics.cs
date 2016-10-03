01707b770e852e680cdea066017ad29e76fb6cac
Rustam Zaitov <rustam.zaitov@xamarin.com>
2016-10-03 20:23:03 +0300

    [DigitDetection] port atomics utils

--- ios10/DigitDetection/DigitDetection/Atomics.cs
+++ ios10/DigitDetection/DigitDetection/Atomics.cs
@@ -1,25 +1,24 @@
-﻿using System;
+﻿using System.Threading;
 
 namespace DigitDetection
 {
 	public static class Atomics
 	{
+		static int cnt;
+
 		public static void Increment ()
 		{
-			throw new NotImplementedException ();
-			//atomic_fetch_add (&cnt, 1);
+			Interlocked.Increment (ref cnt);
 		}
 
 		public static void Reset ()
 		{
-			throw new NotImplementedException ();
-			//cnt = ATOMIC_VAR_INIT (0);
+			Interlocked.Exchange (ref cnt, 0);
 		}
 
 		public static int GetCount ()
 		{
-			throw new NotImplementedException ();
-			//return atomic_load (&cnt);
+			return cnt;
 		}
 	}
 }
