2b293172b55f43dece8027eaf1370c32441d74ec
iherrera <ivan@39inc.com>
2016-11-04 03:03:17 -0300

    WatchBackgroundRefresh Sample First Commit

---
+++ WatchBackgroundRefresh/WatchBackgroundRefresh/AppDelegate.cs
@@ -0,0 +1,31 @@
+﻿using Foundation;
+using UIKit;
+
+namespace WatchBackgroundRefresh
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+	[Register("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		// class-level declarations
+
+		public override UIWindow Window { get; set; }
+
+		public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+		{
+			// Override point for customization after application launch.
+			// If not required for your application you can safely delete this method
+
+			// Code to start the Xamarin Test Cloud Agent
+#if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start();
+#endif
+
+			return true;
+		}
+
+
+	}
+}
+
