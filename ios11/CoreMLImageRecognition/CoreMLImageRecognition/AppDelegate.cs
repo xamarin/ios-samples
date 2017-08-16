911eb915e3122a80573e78df3a122044e0a75c3b
Craig Dunn <craig.dunn@conceptdevelopment.net>
2017-08-16 15:23:41 -0700

    [ios11] CoreML Image Recognition models

---
+++ ios11/CoreMLImageRecognition/CoreMLImageRecognition/AppDelegate.cs
@@ -0,0 +1,20 @@
+﻿using Foundation;
+using UIKit;
+
+namespace CoreMLImageRecognition
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+	[Register("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		// class-level declarations
+
+		public override UIWindow Window
+		{
+			get;
+			set;
+		}
+	}
+}
+
---
+++ ios11/CoreMLImageRecognition/CoreMLImageRecognition/ClassExtensions/UIImageOrientationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UIKit;
+using CoreGraphics;
+using CoreImage;
+using ImageIO;
+
+namespace CoreMLImageRecognition
+{
+	public static class UIImageOrientationExtensions
+	{
+		public static CIImageOrientation ToCIImageOrientation(this UIImageOrientation self) {
+
+			// Take action based on value
+			switch (self)
+			{
+				case UIImageOrientation.Up:
+					return CIImageOrientation.TopLeft;
+				case UIImageOrientation.UpMirrored:
+					return CIImageOrientation.TopRight;
+				case UIImageOrientation.Down:
+					return CIImageOrientation.BottomLeft;
+				case UIImageOrientation.DownMirrored:
+					return CIImageOrientation.BottomRight;
+				case UIImageOrientation.Left:
+					return CIImageOrientation.LeftTop;
+				case UIImageOrientation.LeftMirrored:
+					return CIImageOrientation.LeftBottom;
+				case UIImageOrientation.Right:
+					return CIImageOrientation.RightTop;
+				case UIImageOrientation.RightMirrored:
+					return CIImageOrientation.RightBottom;
+			}
+
+			// Default to up
+			return CIImageOrientation.TopLeft;
+		}
+
+	}
+}
