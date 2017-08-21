8bccea1f6a0dc869f3c36ef374c9cd54231d6b99
Amy Burns <amy.burns87@gmail.com>
2017-08-21 16:10:59 -0400

    Adding Picker Control Sample

---
+++ PickerControl/PickerControl/PickerViewController.cs
@@ -0,0 +1,85 @@
+using Foundation;
+using System;
+using UIKit;
+using System.Collections.Generic;
+
+namespace PickerControl
+{
+    public partial class PickerViewController : UIViewController
+    {
+        public PickerViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+
+            var pickerModel = new PeopleModel(personLabel);
+
+            personPicker.Model = pickerModel;
+            personPicker.ShowSelectionIndicator = true;
+        }
+    }
+
+    public class PeopleModel : UIPickerViewModel
+    {
+        public string[] names = new string[] {
+                "Amy Burns",
+                "Kevin Mullins",
+                "Craig Dunn",
+                "Joel Martinez",
+                "Charles Petzold",
+                "David Britch",
+                "Mark McLemore",
+                "Tom Opegenorth",
+                "Joseph Hill",
+                "Miguel De Icaza"
+            };
+
+        private UILabel personLabel;
+
+        public PeopleModel(UILabel personLabel)
+        {
+            this.personLabel = personLabel;
+        }
+
+        public override nint GetComponentCount(UIPickerView pickerView)
+		{
+			return 2;
+		}
+
+		public override nint GetRowsInComponent(UIPickerView pickerView, nint component)
+		{
+			return names.Length;
+		}
+
+		public override string GetTitle(UIPickerView pickerView, nint row, nint component)
+		{
+			if (component == 0)
+				return names[row];
+			else
+				return row.ToString();
+		}
+
+		public override void Selected(UIPickerView pickerView, nint row, nint component)
+		{
+            
+            personLabel.Text = $"This person is: {names[pickerView.SelectedRowInComponent(0)]},\n they are number {pickerView.SelectedRowInComponent(1)}";
+		}
+
+		public override nfloat GetComponentWidth(UIPickerView picker, nint component)
+		{
+			if (component == 0)
+				return 240f;
+			else
+				return 40f;
+		}
+
+		public override nfloat GetRowHeight(UIPickerView picker, nint component)
+		{
+			return 40f;
+		}
+    }
+}
---
+++ PickerControl/README.md
@@ -0,0 +1,4 @@
+Picker Controls
+================
+
+This sample shows how to create and use a UIPicker and UIDatePicker in Xamarin.iOS. It is the related sample for the [Picker](/guides/ios/user_interface/controls/picker/) guide.
