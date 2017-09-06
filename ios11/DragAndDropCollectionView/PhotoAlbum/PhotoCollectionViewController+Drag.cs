72acdbf09792b1856509fe0323740ccb6fea8e6f
Craig Dunn <craig.dunn@conceptdevelopment.net>
2017-09-06 14:33:29 -0700

    [ios11] DragAndDrop CollectionView (forgot a few methods!)

--- ios11/DragAndDropCollectionView/PhotoAlbum/PhotoCollectionViewController+Drag.cs
+++ ios11/DragAndDropCollectionView/PhotoAlbum/PhotoCollectionViewController+Drag.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Foundation;
 using UIKit;
 
@@ -6,37 +7,80 @@ namespace PhotoAlbum
 {
     public partial class PhotoCollectionViewController
     {
-		// Stores the album state when the drag begins.
-		PhotoAlbum albumBeforeDrag;
+        public UIDragItem[] GetItemsForBeginningDragSession(UICollectionView collectionView, IUIDragSession session, NSIndexPath indexPath)
+		{
+			var dragItem = DragItem(indexPath);
+			return new UIDragItem[] { dragItem };
+		}
 
-
-		public UIDragItem[] GetItemsForBeginningDragSession(UICollectionView collectionView, IUIDragSession session, NSIndexPath indexPath)
+		[Export("collectionView:itemsForAddingToDragSession:atIndexPath:point:")]
+		public UIDragItem[] GetItemsForAddingToDragSession(UICollectionView collectionView, IUIDragSession session, Foundation.NSIndexPath indexPath, CoreGraphics.CGPoint point)
 		{
             var dragItem = DragItem(indexPath);
             return new UIDragItem[] { dragItem };
 		}
 
-        [Export("collectionView:itemsForAddingToDragSession:atIndexPath:point:")]
-        public UIDragItem[] GetItemsForAddingToDragSession(UICollectionView collectionView, IUIDragSession session, Foundation.NSIndexPath indexPath, CoreGraphics.CGPoint point)
-        {
-            throw new System.NotImplementedException();
-        }
-
 		// Helper method to obtain a drag item for the photo at the index path.
-        UIDragItem DragItem (NSIndexPath indexPath)
+		UIDragItem DragItem(NSIndexPath indexPath)
+		{
+			var photo = Photo(indexPath);
+			var itemProvider = photo.ItemProvider;
+			var dragItem = new UIDragItem(itemProvider);
+			dragItem.LocalObject = photo;
+			return dragItem;
+		}
+
+		[Export("collectionView:dragPreviewParametersForItemAtIndexPath:")]
+		public UIDragPreviewParameters GetDragPreviewParameters(UICollectionView collectionView, NSIndexPath indexPath)
+		{
+			return PreviewParameters(indexPath);
+		}
+
+		/// <summary>Stores the album state when the drag begins.</summary>
+		PhotoAlbum albumBeforeDrag;
+
+
+        [Export("collectionView:dragSessionWillBegin:")]
+        public void DragSessionWillBegin(UICollectionView collectionView, IUIDragSession session)
         {
-            var photo = Photo(indexPath);
-            var itemProvider = photo.ItemProvider;
-            var dragItem = new UIDragItem(itemProvider);
-            dragItem.LocalObject = photo;
-            return dragItem;
+            albumBeforeDrag = album;
         }
 
-        [Export("collectionView:dragPreviewParametersForItemAtIndexPath:")]
-        public UIDragPreviewParameters GetDragPreviewParameters(UICollectionView collectionView, NSIndexPath indexPath)
+        [Export("collectionView:dragSessionDidEnd:")]
+        public void DragSessionDidEnd(UICollectionView collectionView, IUIDragSession session)
         {
-            return PreviewParameters(indexPath);
+            ReloadAlbumFromPhotoLibrary();
+            DeleteItems(collectionView);
+            albumBeforeDrag = null;
         }
+
+		/// <summary>
+		/// Compares the album state before and after the drag to delete items in the collection view that represent photos moved elsewhere.
+		/// </summary>
+		void DeleteItems (UICollectionView collectionView)
+        {
+            var albumAfterDrag = album;
+            if (albumBeforeDrag is null || albumAfterDrag is null) return;
+
+            var indexPathsToDelete = new List<NSIndexPath> ();
+
+            for (var i = 0; i < albumBeforeDrag.Photos.Count; i++)
+            {
+                var photo = albumBeforeDrag.Photos[i];
+                if (!albumAfterDrag.Contains(photo))
+                {
+                    indexPathsToDelete.Add(NSIndexPath.FromItemSection(i, 0));
+                }
+            }
+            if (indexPathsToDelete.Count > 0)
+            {
+                collectionView.PerformBatchUpdates(() =>{
+                    collectionView.DeleteItems(indexPathsToDelete.ToArray());
+                }, (finished) => {});
+            }
+        }
+
+		
 
 	}
 }
