e50372594e872cc18923bd63b0b90f7296bc278b
SotoiGhost <issoto@microsoft.com>
2017-10-07 21:58:53 -0500

    [ios11][MusicKitSample] Added Utilities classes

---
+++ ios11/MusicKitSample/MusicKitSample/Utilities/JsonKeys.cs
@@ -0,0 +1,30 @@
+﻿/*
+Abstract:
+Various JSON keys needed when making calls to the Apple Music API.
+*/
+
+using System;
+namespace MusicKitSample.Utilities
+{
+	// Keys related to the `Response Root` JSON object in the Apple Music API.
+	public struct ResponseRootJsonKeys
+	{
+		public static readonly string Data = "data";
+		public static readonly string Results = "results";
+	}
+
+	// Keys related to the `Resource` JSON object in the Apple Music API.
+	public struct ResourceJsonKeys
+	{
+		public static readonly string Id = "id";
+		public static readonly string Attributes = "attributes";
+		public static readonly string Type = "type";
+	}
+
+	// The various keys needed for parsing a JSON response from the Apple Music Web Service.
+	public struct ResourceTypeJsonKeys
+	{
+		public static readonly string Songs = "songs";
+		public static readonly string Albums = "albums";
+	}
+}
