8b43995bafa2c391f6903da8801a6d8f073f0ab7
Craig Dunn <craig.dunn@conceptdevelopment.net>
2017-11-08 16:22:43 -0800

    [ios11] add SafeArea demo

--- ios11/LargeTitlesSample/LargeTitles/DetailViewController.cs
+++ ios11/LargeTitlesSample/LargeTitles/DetailViewController.cs
@@ -1,4 +1,3 @@
-using Foundation;
 using System;
 using UIKit;
 
@@ -6,21 +5,37 @@ namespace largetitles
 {
     public partial class DetailViewController : UIViewController
     {
-        public void SetTitle (string title)
+        public override void ViewDidLoad()
+        {
+            NavigationItem.LargeTitleDisplayMode = UINavigationItemLargeTitleDisplayMode.Never;
+
+            // to demonstrate layout
+            LargeLabel.BackgroundColor = UIColor.LightGray;
+
+            var safeGuide = View.SafeAreaLayoutGuide;
+            LargeLabel.TopAnchor.ConstraintEqualTo(safeGuide.TopAnchor).Active = true;
+            LargeLabel.BottomAnchor.ConstraintEqualTo(safeGuide.BottomAnchor).Active = true;
+            //LargeLabel.LeadingAnchor.ConstraintEqualTo(safeGuide.LeadingAnchor).Active = true;
+            //LargeLabel.TrailingAnchor.ConstraintEqualTo(safeGuide.TrailingAnchor).Active = true;
+
+
+            var margins = View.LayoutMarginsGuide;
+            LargeLabel.TopAnchor.ConstraintEqualTo(margins.TopAnchor).Active = true;
+            LargeLabel.BottomAnchor.ConstraintEqualTo(margins.BottomAnchor).Active = true;
+            //LargeLabel.LeadingAnchor.ConstraintEqualTo(margins.LeadingAnchor).Active = true;
+            //LargeLabel.TrailingAnchor.ConstraintEqualTo(margins.TrailingAnchor).Active = true;
+        }
+
+        public void SetTitle(string title)
         {
             Title = title;
             LargeLabel.Text = $"The title \"{title}\" is {title.Length} characters long.";
         }
 
-        public DetailViewController (IntPtr handle) : base (handle)
+        public DetailViewController(IntPtr handle) : base(handle)
         {
             LargeLabel = new UILabel();
             LargeLabel.Frame = new CoreGraphics.CGRect(10, 100, View.Bounds.Width - 20, View.Bounds.Height / 2);
-        }
-
-        public override void ViewDidLoad()
-        {
-            NavigationItem.LargeTitleDisplayMode = UINavigationItemLargeTitleDisplayMode.Never;
         }
     }
 }
