2c637f9745237e3f095d5d64ab3c8f79c9330eff
Mykyta Bondarenko <32954580+mykyta-bondarenko-gl@users.noreply.github.com>
2018-08-16 10:49:02 +0300

    return xcode source back

---
+++ ios12/SoupChef/OrderSoupIntentCodeGen/OrderSoupIntentCodeGen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OrderSoupIntentCodeGen
+//
+//  Created by Brad Umbaugh on 7/26/18.
+//  Copyright © 2018 Brad Umbaugh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
