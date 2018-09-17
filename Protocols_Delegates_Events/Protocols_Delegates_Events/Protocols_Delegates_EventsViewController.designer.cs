799eb2794aeb796b41af09e02c9e51302527c3e9
Craig Dunn <craig.dunn@conceptdevelopment.net>
2018-09-17 13:56:46 -0700

    modernize protocol-delegate-event sample (#276)
    * remove icons, use assets, add launchscreen
    
    * remove icons, use assets, add launchscreen (XIB sample)

--- Protocols_Delegates_Events/Protocols_Delegates_Events/Protocols_Delegates_EventsViewController.designer.cs
+++ Protocols_Delegates_Events/Protocols_Delegates_Events/Protocols_Delegates_EventsViewController.designer.cs
@@ -1,29 +1,29 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
+// This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
-using System;
 using Foundation;
-using UIKit;
+using System;
 using System.CodeDom.Compiler;
+using UIKit;
 
 namespace Protocols_Delegates_Events
 {
-	[Register ("Protocols_Delegates_EventsViewController")]
-	partial class Protocols_Delegates_EventsViewController
-	{
-		[Outlet]
-		[GeneratedCode ("iOS Designer", "1.0")]
-		MapKit.MKMapView map { get; set; }
+    [Register ("Protocols_Delegates_EventsViewController")]
+    partial class Protocols_Delegates_EventsViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        MapKit.MKMapView map { get; set; }
 
-		void ReleaseDesignerOutlets ()
-		{
-			if (map != null) {
-				map.Dispose ();
-				map = null;
-			}
-		}
-	}
+        void ReleaseDesignerOutlets ()
+        {
+            if (map != null) {
+                map.Dispose ();
+                map = null;
+            }
+        }
+    }
 }
