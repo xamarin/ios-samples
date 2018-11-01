fb553cdcb67c61af537998e2fe67f4fc954eec32
Mykyta Bondarenko <32954580+mykyta-bondarenko-gl@users.noreply.github.com>
2018-10-31 22:20:33 -0400

    Update 'ZoomingPdfViewer' sample (#290)
    * updated structure and files
    
    * fixed sliding
    
    * fixed zooming
    
    * updated colors
    
    * fixed nre
    
    * Update ZoomingPdfViewer/ZoomingPdfViewer/Main.storyboard

---
+++ ZoomingPdfViewer/ZoomingPdfViewer/DataViewController.cs
@@ -0,0 +1,64 @@
+using CoreGraphics;
+using System;
+using UIKit;
+
+namespace ZoomingPdfViewer
+{
+    public partial class DataViewController : UIViewController
+    {
+        private CGPDFPage page;
+
+        private nfloat myScale;
+
+        public DataViewController(IntPtr handle) : base(handle) { }
+
+        public CGPDFDocument Pdf { get; set; }
+
+        public nint PageNumber { get; set; }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view, typically from a nib.
+            page = Pdf.GetPage(PageNumber);
+            ScrollView.SetPDFPage(page);
+
+            // Disable zooming if our pages are currently shown in landscape, for new views
+            ScrollView.UserInteractionEnabled = UIApplication.SharedApplication.StatusBarOrientation.IsPortrait();
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            RestoreScale();
+        }
+
+        public override void ViewWillTransitionToSize(CGSize toSize, IUIViewControllerTransitionCoordinator coordinator)
+        {
+            base.ViewWillTransitionToSize(toSize, coordinator);
+            coordinator.AnimateAlongsideTransition((param) => { }, (context) =>
+            {
+                // Disable zooming if our pages are currently shown in landscape after orientation changes
+                ScrollView.UserInteractionEnabled = UIApplication.SharedApplication.StatusBarOrientation.IsPortrait();
+            });
+        }
+
+        private void RestoreScale()
+        {
+            // Called on orientation change.
+            // We need to zoom out and basically reset the scrollview to look right in two-page spline view.
+            var pageRect = page.GetBoxRect(CGPDFBox.Media);
+            var yScale = View.Frame.Height / pageRect.Height;
+            var xScale = View.Frame.Width / pageRect.Width;
+            myScale = NMath.Min(xScale, yScale);
+
+            ScrollView.Bounds = View.Bounds;
+            ScrollView.ZoomScale = 1f;
+            ScrollView.PdfScale = myScale;
+
+            ScrollView.TiledPdfView.Bounds = View.Bounds;
+            ScrollView.TiledPdfView.MyScale = myScale;
+            ScrollView.TiledPdfView.Layer.SetNeedsDisplay();
+        }
+    }
+}
---
+++ ZoomingPdfViewer/ZoomingPdfViewer/TiledPdfView.cs
@@ -0,0 +1,79 @@
+using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using System;
+using UIKit;
+
+namespace ZoomingPdfViewer
+{
+    /// <summary>
+    /// This view is backed by a CATiledLayer into which the PDF page is rendered into.
+    /// </summary>
+    public class TiledPdfView : UIView
+    {
+        private CATiledLayer tiledLayer;
+
+        /// <summary>
+        /// Create a new TiledPDFView with the desired frame and scale.
+        /// </summary>
+        public TiledPdfView(CGRect frame, nfloat scale) : base(frame)
+        {
+            tiledLayer = Layer as CATiledLayer;
+
+            // levelsOfDetail and levelsOfDetailBias determine how the layer is rendered at different zoom levels.
+            // This only matters while the view is zooming, because once the the view is done zooming a new TiledPDFView is created at the correct size and scale.
+            tiledLayer.LevelsOfDetail = 4;
+            tiledLayer.LevelsOfDetailBias = 3;
+            tiledLayer.TileSize = new CGSize(512f, 512f);
+
+            MyScale = scale;
+            Layer.BorderColor = UIColor.LightGray.CGColor;
+            Layer.BorderWidth = 5;
+        }
+
+        public nfloat MyScale { get; set; }
+
+        public CGPDFPage PdfPage { get; set; }
+
+        [Export("layerClass")]
+        public static Class LayerClass()
+        {
+            // instruct that we want a CATileLayer (not the default CALayer) for the Layer property
+            return new Class(typeof(CATiledLayer));
+        }
+
+        public override void DrawLayer(CALayer layer, CGContext context)
+        {
+            // Fill the background with white.
+            context.SetFillColor(1f, 1f, 1f, 1f);
+            context.FillRect(Bounds);
+
+            // Print a blank page and return if our page is nil.
+            if (PdfPage != null)
+            {
+                context.SaveState();
+
+                // Flip page so we render it as it's meant to be read
+                context.TranslateCTM(0f, Bounds.Height);
+                context.ScaleCTM(1f, -1f);
+
+                // Scale page at the view-zoom level
+                context.ScaleCTM(MyScale, MyScale);
+
+                // draw the page, restore and exit
+                context.DrawPDFPage(PdfPage);
+                context.RestoreState();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if(disposing)
+            {
+                tiledLayer.Dispose();
+            }
+        }
+    }
+}
