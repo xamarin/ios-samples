44c863767f0a7a0c6ee42b666a0cf3e7b3c49f11
Mykyta Bondarenko <mykyta_bondarenko@Mykytas-mac-mini.local>
2018-11-07 16:08:28 +0200

    added new project

---
+++ MapKitSearch/MapKitSearch/SearchResultsController.cs
@@ -0,0 +1,46 @@
+using System;
+using Foundation;
+using MapKit;
+using UIKit;
+
+namespace MapKitSearch
+{
+    public partial class SearchResultsController : UITableViewController
+    {
+        private const string CellIdentifier = "cellIdentifier";
+
+        public SearchResultsController (IntPtr handle) : base (handle) { }
+
+        public MKMapItem[] Items { get; set; }
+
+        public string Query { get; set; }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            this.NavigationItem.Title = Query;
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return Items.Length;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell(CellIdentifier, indexPath);
+
+            var item = Items[indexPath.Row];
+            cell.TextLabel.Text = item.Name;
+            cell.DetailTextLabel.Text = item.PhoneNumber;
+
+            return cell;
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            Items[indexPath.Row].OpenInMaps();
+            tableView.DeselectRow(indexPath, false);
+        }
+    }
+}
---
+++ MapKitSearch/MapKitSearch/ViewController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Threading.Tasks;
+using CoreLocation;
+using Foundation;
+using MapKit;
+using UIKit;
+
+namespace MapKitSearch
+{
+    public partial class ViewController : UIViewController, IUITextFieldDelegate
+    {
+        private const string SearchSegueIdentifier = "openSearchResults";
+
+        private CLLocationManager locationManager;
+
+        private MKMapItem[] searchItems;
+
+        protected ViewController(IntPtr handle) : base(handle) { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            SearchButton.Enabled = false;
+            QueryTextField.Delegate = this;
+            LocationTextField.Delegate = this;
+
+            GetLocationButton.Enabled = CLLocationManager.LocationServicesEnabled;
+            QueryTextField.AddTarget((sender, e) => SearchButton.Enabled = !string.IsNullOrEmpty(QueryTextField.Text),
+                                     UIControlEvent.EditingChanged);
+
+            if (CLLocationManager.LocationServicesEnabled)
+            {
+                locationManager = new CLLocationManager();
+                locationManager.RequestWhenInUseAuthorization();
+                locationManager.LocationsUpdated += (sender, e) =>
+                {
+                    locationManager.StopUpdatingLocation();
+                    var location = e.Locations[e.Locations.Length - 1];
+                    LocationTextField.Text = string.Format("{0:F4}, {1:F4}", location.Coordinate.Latitude, location.Coordinate.Longitude);
+                };
+            }
+        }
+
+        partial void GetCurrentLocation(UIButton sender)
+        {
+            locationManager.StartUpdatingLocation();
+        }
+
+        async partial void Search(UIButton sender)
+        {
+            await SearchAsync(QueryTextField.Text, LocationTextField.Text).ConfigureAwait(false);
+        }
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+        {
+            if(!string.IsNullOrEmpty(segue?.Identifier) && segue.Identifier == SearchSegueIdentifier)
+            {
+                if(segue.DestinationViewController is SearchResultsController searchResultsController)
+                {
+                    searchResultsController.Query = QueryTextField.Text;
+                    searchResultsController.Items = searchItems;
+                }
+            }
+        }
+
+        private async Task SearchAsync(string what, string where)
+        {
+            var span = new MKCoordinateSpan(0.25, 0.25);
+            var request = new MKLocalSearchRequest
+            {
+                NaturalLanguageQuery = what,
+                Region = new MKCoordinateRegion(ParseCoordinates(where), span),
+            };
+
+            var search = new MKLocalSearch(request);
+            MKLocalSearchResponse response = null;
+
+            try
+            {
+                response = await search.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+
+            if (response != null)
+            {
+                searchItems = response.MapItems;
+                this.PerformSegue(SearchSegueIdentifier, this);
+            }
+        }
+
+        private CLLocationCoordinate2D ParseCoordinates(string coordinates)
+        {
+            var result = new CLLocationCoordinate2D();
+
+            if (!string.IsNullOrEmpty(coordinates))
+            {
+                var values = coordinates.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                if (values.Length == 2)
+                {
+                    if (double.TryParse(values[0], out double latitude) &&
+                        double.TryParse(values[1], out double longitude))
+                    {
+                        result = new CLLocationCoordinate2D(latitude, longitude);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        #region IUITextFieldDelegate
+
+        [Export("textFieldShouldReturn:")]
+        public bool ShouldReturn(UITextField textField)
+        {
+            if (textField == QueryTextField)
+            {
+                LocationTextField.BecomeFirstResponder();
+            }
+            else
+            {
+                textField.ResignFirstResponder();
+            }
+
+            return true;
+        }
+
+        #endregion
+    }
+}
