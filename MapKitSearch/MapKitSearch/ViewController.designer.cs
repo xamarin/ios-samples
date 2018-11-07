13b8e63b470664c7395a564f551be1c4b666e58b
Mykyta Bondarenko <32954580+mykyta-bondarenko-gl@users.noreply.github.com>
2018-11-07 12:22:43 -0500

    Update 'MapKitSeach' sample (#299)
    * added new project
    
    * deleted old project
    
    * updated info.plist
    
    * added docs

---
+++ MapKitSearch/MapKitSearch/ViewController.designer.cs
@@ -0,0 +1,63 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace MapKitSearch
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton GetLocationButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField LocationTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField QueryTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton SearchButton { get; set; }
+
+        [Action ("GetCurrentLocation:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void GetCurrentLocation (UIKit.UIButton sender);
+
+        [Action ("Search:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Search (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (GetLocationButton != null) {
+                GetLocationButton.Dispose ();
+                GetLocationButton = null;
+            }
+
+            if (LocationTextField != null) {
+                LocationTextField.Dispose ();
+                LocationTextField = null;
+            }
+
+            if (QueryTextField != null) {
+                QueryTextField.Dispose ();
+                QueryTextField = null;
+            }
+
+            if (SearchButton != null) {
+                SearchButton.Dispose ();
+                SearchButton = null;
+            }
+        }
+    }
+}
