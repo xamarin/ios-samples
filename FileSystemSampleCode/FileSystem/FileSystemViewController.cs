8408f4e127d3b69c7e5e0daa2d5cccab2653b199
Craig <crdun@Brads-MacBook-Pro.local>
2018-11-12 09:57:47 -0800

    modernize sample (launchscreen, layout for X)

--- FileSystemSampleCode/FileSystem/FileSystemViewController.cs
+++ FileSystemSampleCode/FileSystem/FileSystemViewController.cs
@@ -19,32 +19,40 @@ namespace FileSystem
 		{
 			base.ViewDidLoad ();
 
+            var topMargin = 50;
+            var margin = 10;
+            var width = View.Bounds.Width;
+            var buttonwidth = width / 2 - 30;
+            var buttonHeight = 50;
+            var height = View.Bounds.Height;
+
 			// Create the buttons and TextView to run the sample code
 			btnFiles = UIButton.FromType(UIButtonType.RoundedRect);
-			btnFiles.Frame = new CGRect(10,10,145,50);
+			btnFiles.Frame = new CGRect(margin, topMargin, buttonwidth, buttonHeight);
 			btnFiles.SetTitle("Open 'ReadMe.txt'", UIControlState.Normal);
 
 			btnDirectories = UIButton.FromType(UIButtonType.RoundedRect);
-			btnDirectories.Frame = new CGRect(10,70,145,50);
+			btnDirectories.Frame = new CGRect(margin, topMargin+buttonHeight+margin, buttonwidth, buttonHeight);
 			btnDirectories.SetTitle("List Directories", UIControlState.Normal);
 
 			btnXml = UIButton.FromType(UIButtonType.RoundedRect);
-			btnXml.Frame = new CGRect(165,10,145,50);
+			btnXml.Frame = new CGRect(buttonwidth + 2 * margin, topMargin, buttonwidth, buttonHeight);
 			btnXml.SetTitle("Open 'Test.xml'", UIControlState.Normal);
 
 			btnAll = UIButton.FromType(UIButtonType.RoundedRect);
-			btnAll.Frame = new CGRect(165,70,145,50);
+			btnAll.Frame = new CGRect(buttonwidth + 2 * margin, topMargin + buttonHeight + margin, buttonwidth, buttonHeight);
 			btnAll.SetTitle("List All", UIControlState.Normal);
 
 			btnWrite = UIButton.FromType(UIButtonType.RoundedRect);
-			btnWrite.Frame = new CGRect(10,130,145,50);
+			btnWrite.Frame = new CGRect(margin, topMargin + 2*(buttonHeight + margin), buttonwidth, buttonHeight);
 			btnWrite.SetTitle("Write File", UIControlState.Normal);
 
 			btnDirectory = UIButton.FromType(UIButtonType.RoundedRect);
-			btnDirectory.Frame = new CGRect(165,130,145,50);
+			btnDirectory.Frame = new CGRect(buttonwidth + 2 * margin, topMargin + 2 * (buttonHeight + margin), buttonwidth, buttonHeight);
 			btnDirectory.SetTitle("Create Directory", UIControlState.Normal);
 
-			txtView = new UITextView(new CGRect(10, 190, 300, 250));
+            var topText = topMargin + 3 * (buttonHeight + margin);
+            txtView = new UITextView(new CGRect(margin, topText, width - 2*margin, height - topText - margin));
 			txtView.Editable = false;
 			txtView.ScrollEnabled = true;
 
