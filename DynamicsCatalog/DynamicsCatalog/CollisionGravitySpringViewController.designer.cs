570af990e0e2f71277d8f4e8afc345795174d7d0
Mykyta Bondarenko <mykyta_bondarenko@Mykytas-mac-mini.local>
2018-11-13 17:53:10 +0200

    fixed launch crash
    - renamed root fodler
    - updated assets
    - fixed launch crash

---
+++ DynamicsCatalog/DynamicsCatalog/CollisionGravitySpringViewController.designer.cs
@@ -0,0 +1,46 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace DynamicsCatalog
+{
+    [Register ("CollisionGravitySpringViewController")]
+    partial class CollisionGravitySpringViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView blueSquare { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView redSquare { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView square { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (blueSquare != null) {
+                blueSquare.Dispose ();
+                blueSquare = null;
+            }
+
+            if (redSquare != null) {
+                redSquare.Dispose ();
+                redSquare = null;
+            }
+
+            if (square != null) {
+                square.Dispose ();
+                square = null;
+            }
+        }
+    }
+}
