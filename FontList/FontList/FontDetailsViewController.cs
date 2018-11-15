c36f2b2fc9db7fe3ccd8f88118956ac1d92ed19e
Mykyta Bondarenko <mykyta_bondarenko@Mykytas-mac-mini.local>
2018-11-15 15:48:16 +0200

    removed an old sampel and added new one

---
+++ FontList/FontList/FontDetailsViewController.cs
@@ -0,0 +1,21 @@
+using FontList.Models;
+using System;
+using UIKit;
+
+namespace FontList
+{
+    public partial class FontDetailsViewController : UIViewController
+    {
+        public FontDetailsViewController(IntPtr handle) : base(handle) { }
+
+        public FontItem FontItem { get; internal set; }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            Title = FontItem.Name;
+            textView.Font = FontItem.Font;
+        }
+    }
+}
