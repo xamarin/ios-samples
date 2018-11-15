71716aaf17af76ea678ebec6b963c98413a31a68
Mykyta Bondarenko <32954580+mykyta-bondarenko-gl@users.noreply.github.com>
2018-11-15 12:49:30 -0500

    Update 'CoreLocation' sample (#309)
    * added new project
    
    * updated UI
    
    * added assets
    
    * removed old sample
    
    * updated ReadMe
    
    * removed extra file

---
+++ CoreLocation/CoreLocation/ViewController.designer.cs
@@ -0,0 +1,91 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace CoreLocation
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel altitudeLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel courseLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel distanceLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel latitudeLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel longitudeLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel magneticHeadingLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel speedLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel trueHeadingLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (altitudeLabel != null) {
+                altitudeLabel.Dispose ();
+                altitudeLabel = null;
+            }
+
+            if (courseLabel != null) {
+                courseLabel.Dispose ();
+                courseLabel = null;
+            }
+
+            if (distanceLabel != null) {
+                distanceLabel.Dispose ();
+                distanceLabel = null;
+            }
+
+            if (latitudeLabel != null) {
+                latitudeLabel.Dispose ();
+                latitudeLabel = null;
+            }
+
+            if (longitudeLabel != null) {
+                longitudeLabel.Dispose ();
+                longitudeLabel = null;
+            }
+
+            if (magneticHeadingLabel != null) {
+                magneticHeadingLabel.Dispose ();
+                magneticHeadingLabel = null;
+            }
+
+            if (speedLabel != null) {
+                speedLabel.Dispose ();
+                speedLabel = null;
+            }
+
+            if (trueHeadingLabel != null) {
+                trueHeadingLabel.Dispose ();
+                trueHeadingLabel = null;
+            }
+        }
+    }
+}
--- CoreLocation/README.md
+++ CoreLocation/README.md
@@ -1,11 +1,12 @@
 Core Location
 =============
 
-This sample illustrates how to retrieve location, orientation, and velocity
-data from the device using the Core Location API exposed in MonoTouch. It
-covers initializing the CLLocationManager, configuring update frequency and
-accuracy, and receiving updates using both the event and delegate pattern.
+This sample illustrates how to retrieve location, orientation, and velocity data from the device using the Core Location API. It covers initializing the CLLocationManager, configuring update frequency and accuracy, and receiving updates using delegate pattern.
 It also illustrates how to retrieve "Distance To" information.
+
+License
+-------
+Code is released under the MIT license
 
 Authors
 -------
