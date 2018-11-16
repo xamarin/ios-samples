20a57a73f204715a3366090140ccc155b3e46b33
Mykyta Bondarenko <mykyta_bondarenko@Mykytas-mac-mini.local>
2018-11-16 16:18:33 +0200

    fixed UI and updated launch stroyboard

--- LocalNotifications/LocalNotifications/ViewController.cs
+++ LocalNotifications/LocalNotifications/ViewController.cs
@@ -6,10 +6,7 @@ namespace LocalNotifications
 {
     public partial class ViewController : UIViewController
     {
-        protected ViewController(IntPtr handle) : base(handle)
-        {
-            // Note: this .ctor should not contain any initialization logic.
-        }
+        protected ViewController(IntPtr handle) : base(handle) { }
 
         public override void ViewDidLoad()
         {
