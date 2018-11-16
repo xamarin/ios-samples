d80329c9d9770c0a090fadb2596ea0db31bd6b0b
Mykyta Bondarenko <mykyta_bondarenko@Mykytas-mac-mini.local>
2018-11-16 16:26:44 +0200

    added an action

--- LocalNotifications/LocalNotifications/ViewController.cs
+++ LocalNotifications/LocalNotifications/ViewController.cs
@@ -8,38 +8,27 @@ namespace LocalNotifications
     {
         protected ViewController(IntPtr handle) : base(handle) { }
 
-        public override void ViewDidLoad()
+        partial void SendNotification(UIButton sender)
         {
-            base.ViewDidLoad();
+            // create the notification
+            var notification = new UILocalNotification();
 
-            button.TouchUpInside += (sender, e) =>
-                {
-                    // create the notification
-                    var notification = new UILocalNotification();
+            // set the fire date (the date time in which it will fire)
+            notification.FireDate = NSDate.FromTimeIntervalSinceNow(10);
 
-                    // set the fire date (the date time in which it will fire)
-                    notification.FireDate = NSDate.FromTimeIntervalSinceNow(10);
+            // configure the alert
+            notification.AlertAction = "View Alert";
+            notification.AlertBody = "Your 10 second alert has fired!";
 
-                    // configure the alert
-                    notification.AlertAction = "View Alert";
-                    notification.AlertBody = "Your 10 second alert has fired!";
+            // modify the badge
+            notification.ApplicationIconBadgeNumber = 1;
 
-                    // modify the badge
-                    notification.ApplicationIconBadgeNumber = 1;
+            // set the sound to be the default sound
+            notification.SoundName = UILocalNotification.DefaultSoundName;
 
-                    // set the sound to be the default sound
-                    notification.SoundName = UILocalNotification.DefaultSoundName;
-
-                    // schedule it
-                    UIApplication.SharedApplication.ScheduleLocalNotification(notification);
-                    Console.WriteLine("Scheduled...");
-                };
-        }
-
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
+            // schedule it
+            UIApplication.SharedApplication.ScheduleLocalNotification(notification);
+            Console.WriteLine("Scheduled...");
         }
     }
 }
