2526a2a0920cd101c639e89981cf4bfcec460120
Mykyta Bondarenko <mykyta_bondarenko@Mykytas-mac-mini.local>
2018-11-16 17:25:37 +0200

    added new project

---
+++ PrintSample/Print/ViewController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UIKit;
+
+namespace Print
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle) { }
+
+        partial void Print(UIButton sender)
+        {
+            var printInfo = UIPrintInfo.PrintInfo;
+            printInfo.JobName = "My first Print Job";
+            printInfo.OutputType = UIPrintInfoOutputType.General;
+
+            var textFormatter = new UISimpleTextPrintFormatter("Once upon a time...")
+            {
+                StartPage = 0,
+                MaximumContentWidth = 6 * 72,
+                ContentInsets = new UIEdgeInsets(72, 72, 72, 72),
+            };
+
+            var printer = UIPrintInteractionController.SharedPrintController;
+            printer.PrintInfo = printInfo;
+            printer.PrintFormatter = textFormatter;
+            printer.ShowsPageRange = true;
+            printer.Present(true, (handler, completed, error) =>
+            {
+                if (!completed && error != null)
+                {
+                    Console.WriteLine($"Error: {error.LocalizedDescription ?? ""}");
+                }
+            });
+
+            printInfo.Dispose();
+            textFormatter.Dispose();
+        }
+    }
+}
