8c9315644fe9d3b3f89ec6c32f75cbca3a3414b6
Mykyta Bondarenko <mykyta_bondarenko@Mykytas-mac-mini.local>
2018-11-16 18:03:20 +0200

    updated project
    - updated assets
    - added launch storyboard
    - updated manifest
    - updated main storyboard

--- PrintBanner/PrintBanner/PrintBannerViewController.cs
+++ PrintBanner/PrintBanner/PrintBannerViewController.cs
@@ -3,111 +3,109 @@ using CoreGraphics;
 using Foundation;
 using UIKit;
 
-namespace PrintBanner {
+namespace PrintBanner
+{
+    public partial class PrintBannerViewController : UIViewController
+    {
+        private const float DefaultFontSize = 48f;
 
-	public partial class PrintBannerViewController : UIViewController {
+        private const float PaddingFactor = 0.1f;
 
-		const float DefaultFontSize = 48f;
-		const float PaddingFactor = 0.1f;
+        UISimpleTextPrintFormatter textformatter;
 
-		UISimpleTextPrintFormatter textformatter;
+        public PrintBannerViewController(IntPtr handle) : base(handle) { }
 
-		public PrintBannerViewController (IntPtr handle) : base (handle)
-		{
-		}
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            textField.EditingDidEndOnExit += (sender, e) => textField.ResignFirstResponder();
+        }
 
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
+        partial void Print(UIButton sender)
+        {
+            UIPrintInteractionController controller = UIPrintInteractionController.SharedPrintController;
+            if (controller == null)
+            {
+                Console.WriteLine("Couldn't get shared UIPrintInteractionController");
+                return;
+            }
 
-			textField.EditingDidEndOnExit += (sender, e) => {
-				textField.ResignFirstResponder ();
-			};
+            controller.CutLengthForPaper = delegate (UIPrintInteractionController printController, UIPrintPaper paper) {
+                // Create a font with arbitrary size so that you can calculate the approximate
+                // font points per screen point for the height of the text.
+                UIFont font = textformatter.Font;
 
-			printButton.TouchUpInside += Print;
-		}
+                NSString str = new NSString(textField.Text);
+                UIStringAttributes attributes = new UIStringAttributes();
+                attributes.Font = font;
+                CGSize size = str.GetSizeUsingAttributes(attributes);
 
-		public void Print (object sender, EventArgs args)
-		{
-			UIPrintInteractionController controller = UIPrintInteractionController.SharedPrintController;
-			if (controller == null) {
-				Console.WriteLine ("Couldn't get shared UIPrintInteractionController");
-				return;
-			}
+                nfloat approximateFontPointPerScreenPoint = font.PointSize / size.Height;
 
-			controller.CutLengthForPaper = delegate (UIPrintInteractionController printController, UIPrintPaper paper) {
-				// Create a font with arbitrary size so that you can calculate the approximate
-  				// font points per screen point for the height of the text.
-				UIFont font = textformatter.Font;
+                // Create a new font using a size  that will fill the width of the paper
+                font = SelectFont((float)(paper.PrintableRect.Size.Width * approximateFontPointPerScreenPoint));
 
-				NSString str = new NSString (textField.Text);
-				UIStringAttributes attributes = new UIStringAttributes ();
-				attributes.Font = font;
-				CGSize size = str.GetSizeUsingAttributes (attributes);
+                // Calculate the height and width of the text with the final font size
+                attributes.Font = font;
+                CGSize finalTextSize = str.GetSizeUsingAttributes(attributes);
 
-				nfloat approximateFontPointPerScreenPoint = font.PointSize / size.Height;
+                // Set the UISimpleTextFormatter font to the font with the size calculated
+                textformatter.Font = font;
 
-				// Create a new font using a size  that will fill the width of the paper
-				font = SelectFont ((float)(paper.PrintableRect.Size.Width * approximateFontPointPerScreenPoint));
+                // Calculate the margins of the roll. Roll printers may have unprintable areas
+                // before and after the cut.  We must add this to our cut length to ensure the
+                // printable area has enough room for our text.
+                nfloat lengthOfMargins = paper.PaperSize.Height - paper.PrintableRect.Size.Height;
 
-				// Calculate the height and width of the text with the final font size
-				attributes.Font = font;
-				CGSize finalTextSize = str.GetSizeUsingAttributes (attributes);
+                // The cut length is the width of the text, plus margins, plus some padding
+                return finalTextSize.Width + lengthOfMargins + paper.PrintableRect.Size.Width * PaddingFactor;
+            };
 
-				// Set the UISimpleTextFormatter font to the font with the size calculated
-				textformatter.Font = font;
+            UIPrintInfo printInfo = UIPrintInfo.PrintInfo;
+            printInfo.OutputType = UIPrintInfoOutputType.General;
+            printInfo.Orientation = UIPrintInfoOrientation.Landscape;
+            printInfo.JobName = textField.Text;
 
-				// Calculate the margins of the roll. Roll printers may have unprintable areas
-			    // before and after the cut.  We must add this to our cut length to ensure the
-			    // printable area has enough room for our text.
-				nfloat lengthOfMargins = paper.PaperSize.Height - paper.PrintableRect.Size.Height;
+            textformatter = new UISimpleTextPrintFormatter(textField.Text)
+            {
+                Color = SelectedColor,
+                Font = SelectFont()
+            };
 
-				// The cut length is the width of the text, plus margins, plus some padding
-				return finalTextSize.Width + lengthOfMargins + paper.PrintableRect.Size.Width * PaddingFactor;
-			};
+            controller.PrintInfo = printInfo;
+            controller.PrintFormatter = textformatter;
 
-			UIPrintInfo printInfo = UIPrintInfo.PrintInfo;
-			printInfo.OutputType = UIPrintInfoOutputType.General;
-			printInfo.Orientation = UIPrintInfoOrientation.Landscape;
-			printInfo.JobName = textField.Text;
+            if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Pad)
+                controller.PresentFromRectInView(printButton.Frame, View, true, PrintingComplete);
+            else
+                controller.Present(true, PrintingComplete);
+        }
 
-			textformatter = new UISimpleTextPrintFormatter (textField.Text) {
-				Color = SelectedColor,
-				Font = SelectFont ()
-			};
+        void PrintingComplete(UIPrintInteractionController printInteractionController, bool completed, NSError error)
+        {
+            if (completed && error != null)
+            {
+                string message = String.Format("Due to error in domain `{0}` with code: {1}", error.Domain, error.Code);
+                Console.WriteLine("FAILED! {0}", message);
+                new UIAlertView("Failed!", message, null, "OK", null).Show();
+            }
+        }
 
-			controller.PrintInfo = printInfo;
-			controller.PrintFormatter = textformatter;
+        static string[] font_names = { "American Typewriter", "Snell Roundhand", "Courier", "Arial" };
 
-			if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Pad)
-				controller.PresentFromRectInView (printButton.Frame, View, true, PrintingComplete);
-			else
-				controller.Present (true, PrintingComplete);
-		}
+        UIFont SelectFont(float size = DefaultFontSize)
+        {
+            var font = UIFont.FromName(font_names[fontSelection.SelectedSegment], size);
+            if (font == null)
+                font = UIFont.SystemFontOfSize(size);
+            return font;
+        }
 
-		void PrintingComplete (UIPrintInteractionController printInteractionController, bool completed, NSError error)
-		{
-			if (completed && error != null) {
-				string message = String.Format ("Due to error in domain `{0}` with code: {1}", error.Domain, error.Code);
-				Console.WriteLine ("FAILED! {0}", message);
-				new UIAlertView ("Failed!", message, null, "OK", null).Show ();
-			}
-		}
+        static UIColor[] colors = { UIColor.Black, UIColor.Orange, UIColor.Purple, UIColor.Red };
 
-		static string[] font_names = { "American Typewriter", "Snell Roundhand", "Courier", "Arial" };
-
-		UIFont SelectFont (float size = DefaultFontSize)
-		{
-			var font = UIFont.FromName (font_names [fontSelection.SelectedSegment], size);
-			if (font == null)
-				font = UIFont.SystemFontOfSize (size);
-			return font;
-		}
-
-		static UIColor[] colors = { UIColor.Black, UIColor.Orange, UIColor.Purple, UIColor.Red };
-
-		UIColor SelectedColor {
-			get { return colors [colorSelection.SelectedSegment]; }
-		}
-	}
+        UIColor SelectedColor
+        {
+            get { return colors[colorSelection.SelectedSegment]; }
+        }
+    }
 }
