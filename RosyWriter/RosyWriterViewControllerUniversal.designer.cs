f15d51ff4106238a30ed5beaa4c4fc1042275147
Mykyta Bondarenko <mykyta_bondarenko@Mykytas-mac-mini.local>
2018-11-19 12:04:17 +0200

    Add new project

--- RosyWriter/RosyWriterViewControllerUniversal.designer.cs
+++ RosyWriter/RosyWriterViewControllerUniversal.designer.cs
@@ -1,33 +1,36 @@
 // WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
 
 namespace RosyWriter
 {
-	[Register ("RosyWriterViewControllerUniversal")]
-	partial class RosyWriterViewControllerUniversal
-	{
-		[Outlet]
-		UIKit.UIView previewView { get; set; }
+    [Register ("RosyWriterViewControllerUniversal")]
+    partial class RosyWriterViewControllerUniversal
+    {
+        [Outlet]
+        UIKit.UIView previewView { get; set; }
 
-		[Outlet]
-		UIKit.UIBarButtonItem btnRecord { get; set; }
 
-		void ReleaseDesignerOutlets ()
-		{
-			if (previewView != null) {
-				previewView.Dispose ();
-				previewView = null;
-			}
+        [Outlet]
+        UIKit.UIBarButtonItem btnRecord { get; set; }
 
-			if (btnRecord != null) {
-				btnRecord.Dispose ();
-				btnRecord = null;
-			}
-		}
-	}
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnRecord != null) {
+                btnRecord.Dispose ();
+                btnRecord = null;
+            }
+
+            if (previewView != null) {
+                previewView.Dispose ();
+                previewView = null;
+            }
+        }
+    }
 }
