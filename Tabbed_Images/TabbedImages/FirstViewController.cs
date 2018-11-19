a7f9701c9642454497c35fc5f72319edbb994409
Mykyta Bondarenko <mykyta_bondarenko@Mykytas-mac-mini.local>
2018-11-19 18:34:42 +0200

    updated code related to readme

--- Tabbed_Images/TabbedImages/FirstViewController.cs
+++ Tabbed_Images/TabbedImages/FirstViewController.cs
@@ -6,5 +6,11 @@ namespace TabbedImages
     public partial class FirstViewController : UIViewController
     {
         public FirstViewController(IntPtr handle) : base(handle) { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            this.TabBarItem.Image = UIImage.FromBundle("first");
+        }
     }
 }
