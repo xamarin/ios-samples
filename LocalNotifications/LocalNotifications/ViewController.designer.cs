42a57a6f43c99c7f0a89e90905cdc7b041cc0200
Mykyta Bondarenko <32954580+mykyta-bondarenko-gl@users.noreply.github.com>
2018-11-20 14:36:58 -0500

    Updated "LocalNotifications" sample (#312)
    * fixed UI and updated launch stroyboard
    
    * updated assets
    
    * updated assets
    
    * added an action
    
    * updated deprecated code
    
    * updated screenshots

--- LocalNotifications/LocalNotifications/ViewController.designer.cs
+++ LocalNotifications/LocalNotifications/ViewController.designer.cs
@@ -1,6 +1,6 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
+// This file has been generated automatically by Visual Studio from the outlets and
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
@@ -14,16 +14,12 @@ namespace LocalNotifications
     [Register ("ViewController")]
     partial class ViewController
     {
-        [Outlet]
+        [Action ("SendNotification:")]
         [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton button { get; set; }
+        partial void SendNotification (UIKit.UIButton sender);
 
         void ReleaseDesignerOutlets ()
         {
-            if (button != null) {
-                button.Dispose ();
-                button = null;
-            }
         }
     }
 }
