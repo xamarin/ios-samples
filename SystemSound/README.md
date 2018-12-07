0cfc6cc9622f2fca894aac8e4ff03252910562b2
Mykyta Bondarenko <32954580+mykyta-bondarenko-gl@users.noreply.github.com>
2018-12-07 21:29:37 +0200

    Update 'SystemSounds' sample (#329)
    * added new project
    
    * deleted old project
    
    * added assets
    
    * renamed the sln file
    
    * renamed root folder
    
    * updated screenshots
    
    * updated readme

---
+++ SystemSound/README.md
@@ -0,0 +1,20 @@
+System Sound
+============
+
+This sample demonstrates playing sound via AudioToolkit.  
+Note: not all devices support vibration.
+
+Build Requirements
+------------------
+
+Building this sample requires Xcode 5.0 and iOS 7.0 SDK
+
+Related Links
+-------
+
+- [System Sound Services](https://developer.apple.com/documentation/audiotoolbox/system_sound_services#//apple_ref/c/func/AudioServicesPlayAlertSound)
+
+License
+-------
+
+Xamarin port changes are released under the MIT license.
---
+++ SystemSound/SystemSound/ViewController.designer.cs
@@ -0,0 +1,32 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace SystemSound
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Action ("PlayAlert:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void PlayAlert (UIKit.UIButton sender);
+
+        [Action ("PlaySystem:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void PlaySystem (UIKit.UIButton sender);
+
+        [Action ("Vibrate:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void Vibrate (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}
