2e9220aba8024e7f6731255a7f2707d4ce9b6455
Mykyta Bondarenko <32954580+mykyta-bondarenko-gl@users.noreply.github.com>
2018-12-07 06:25:53 +0200

    Update 'FileSystem' sample (#310)
    * added new sample
    
    * added assets
    
    * deleted old project
    
    * updated screenshot

---
+++ FileSystemSampleCode/FileSystem/ViewController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UIKit;
+
+namespace FileSystem
+{
+    public partial class ViewController : UIViewController
+    {
+        private bool writeJson = true;
+
+        protected ViewController(IntPtr handle) : base(handle) { }
+
+        partial void OpenReadMe(UIButton sender)
+        {
+            textView.Text = DataManager.ReadText();
+        }
+
+        partial void OpenTestFile(UIButton sender)
+        {
+            textView.Text = DataManager.ReadXml();
+        }
+
+        partial void ListDirectories(UIButton sender)
+        {
+            textView.Text = DataManager.ReadDirectories();
+        }
+
+        partial void ListAll(UIButton sender)
+        {
+            textView.Text = DataManager.ReadAll();
+        }
+
+        partial void WriteFile(UIButton sender)
+        {
+            var text = string.Empty;
+            if (writeJson)
+            {
+                text = DataManager.WriteJson();
+            }
+            else
+            {
+                text = DataManager.WriteFile();
+            }
+
+            textView.Text = text;
+        }
+
+        partial void CreateDirectory(UIButton sender)
+        {
+            textView.Text = DataManager.CreateDirectory();
+        }
+    }
+}
