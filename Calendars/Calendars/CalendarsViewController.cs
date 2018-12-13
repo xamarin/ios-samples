91415a3cd7e7e448308ff04e421375b58a398173
Mykyta Bondarenko <mykyta_bondarenko@HRK1-DMC-F49706.local>
2018-12-13 16:53:06 +0200

    added new project

---
+++ Calendars/Calendars/CalendarsViewController.cs
@@ -0,0 +1,36 @@
+using EventKit;
+using Foundation;
+using System;
+using UIKit;
+
+namespace Calendars
+{
+    public partial class CalendarsViewController : UITableViewController
+    {
+        private EKCalendar[] calendars;
+
+        public CalendarsViewController (IntPtr handle) : base (handle) { }
+
+        public EKEntityType EntityType { get; set; }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // get calendars of the particular type (either events or reminders)
+            this.calendars = MainViewController.EventStore.GetCalendars(this.EntityType);
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return this.calendars.Length;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("calendarCellIdentifier") ?? new UITableViewCell();
+            cell.TextLabel.Text = this.calendars[indexPath.Row].Title;
+            return cell;
+        }
+    }
+}
---
+++ Calendars/Calendars/RemindersController.cs
@@ -0,0 +1,43 @@
+using EventKit;
+using Foundation;
+using System;
+using UIKit;
+
+namespace Calendars
+{
+    public partial class RemindersController : UITableViewController
+    {
+        private EKReminder[] reminders;
+
+        public RemindersController (IntPtr handle) : base (handle) { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // create our NSPredicate which we'll use for the query
+            using (var query = MainViewController.EventStore.PredicateForReminders(null))
+            {
+                // execute the query
+                MainViewController.EventStore.FetchReminders(query, (collection) =>
+                {
+                    this.reminders = collection;
+                });
+            }
+
+            this.TableView.ReloadData();
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return this.reminders?.Length ?? 0;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell("eventCellIdentifier") ?? new UITableViewCell();
+            cell.TextLabel.Text = this.reminders[indexPath.Row].Title;
+            return cell;
+        }
+    }
+}
