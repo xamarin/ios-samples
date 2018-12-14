429ff9d4f61d17f6331ed8560cc73836a8221bc3
Mykyta Bondarenko <mykyta_bondarenko@HRK1-DMC-F49706.local>
2018-12-14 15:09:22 +0200

    added new project

---
+++ GLCameraRipple/GLCameraRipple/ViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using UIKit;
+
+namespace GLCameraRipple
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle) { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+    }
+}
