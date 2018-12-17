27f370afedbb8df8e7b2c2feffadf57d90a3c7d6
Mykyta Bondarenko <mykyta_bondarenko@HRK1-DMC-F49706.local>
2018-12-17 16:41:08 +0200

    added new extension

---
+++ ios8/Share/ShareExtension/ShareViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Foundation;
+using Social;
+
+namespace ShareExtension
+{
+    public partial class ShareViewController : SLComposeServiceViewController
+    {
+        protected ShareViewController(IntPtr handle) : base(handle) { }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
+
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+        }
+
+        public override bool IsContentValid()
+        {
+            // Do validation of contentText and/or NSExtensionContext attachments here
+            return true;
+        }
+
+        public override void DidSelectPost()
+        {
+            // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+
+            // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+            ExtensionContext.CompleteRequest(new NSExtensionItem[0], null);
+        }
+
+        public override SLComposeSheetConfigurationItem[] GetConfigurationItems()
+        {
+            // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+            return new SLComposeSheetConfigurationItem[0];
+        }
+    }
+}
