d1dbb93cbb9edd69474d4e97861fd151397ee544
Mykyta Bondarenko <mykyta_bondarenko@HRK1-DMC-F49706.local>
2018-12-17 17:36:42 +0200

    added an alert to the complete method

--- ios8/Share/ShareExtension/ShareViewController.cs
+++ ios8/Share/ShareExtension/ShareViewController.cs
@@ -1,6 +1,8 @@
-﻿using Foundation;
+﻿using CoreFoundation;
+using Foundation;
 using Social;
 using System;
+using UIKit;
 
 namespace ShareExtension
 {
@@ -32,9 +34,15 @@ namespace ShareExtension
         public override void DidSelectPost()
         {
             // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-
-            // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-            ExtensionContext.CompleteRequest(new NSExtensionItem[0], null);
+            var alert = UIAlertController.Create("Share extension", $"This is the step where you should post the ContentText value: '{ContentText}' to your targeted service.", UIAlertControllerStyle.Alert);
+            PresentViewController(alert, true, () =>
+            {
+                DispatchQueue.MainQueue.DispatchAfter(new DispatchTime(DispatchTime.Now, 5000000000), () =>
+                {
+                    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+                    ExtensionContext.CompleteRequest(new NSExtensionItem[0], null);
+                });
+            });
         }
 
         public override SLComposeSheetConfigurationItem[] GetConfigurationItems()
