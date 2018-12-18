c5868b7fd314f399010dd42f5c2b00491d620de9
Mykyta Bondarenko <mykyta_bondarenko@HRK1-DMC-F49706.local>
2018-12-18 17:38:19 +0200

    complete slider controller

--- ios8/UICatalog/UICatalog/SliderViewController.designer.cs
+++ ios8/UICatalog/UICatalog/SliderViewController.designer.cs
@@ -1,21 +1,54 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace UICatalog
 {
-    [Register ("SliderViewController")]
-    partial class SliderViewController
-    {
-        void ReleaseDesignerOutlets ()
-        {
-        }
-    }
+	[Register ("SliderViewController")]
+	partial class SliderViewController
+	{
+		[Outlet]
+		UIKit.UISlider customSlider { get; set; }
+
+		[Outlet]
+		UIKit.UISlider defaultSlider { get; set; }
+
+		[Outlet]
+		UIKit.UISlider tintedSlider { get; set; }
+
+		[Action ("CustomSliderValueChanged:")]
+		partial void CustomSliderValueChanged (Foundation.NSObject sender);
+
+		[Action ("CustomSliderValurChanged:")]
+		partial void CustomSliderValurChanged (Foundation.NSObject sender);
+
+		[Action ("DefaultSliderValueChanged:")]
+		partial void DefaultSliderValueChanged (Foundation.NSObject sender);
+
+		[Action ("TintedSliderValueChanged:")]
+		partial void TintedSliderValueChanged (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (defaultSlider != null) {
+				defaultSlider.Dispose ();
+				defaultSlider = null;
+			}
+
+			if (tintedSlider != null) {
+				tintedSlider.Dispose ();
+				tintedSlider = null;
+			}
+
+			if (customSlider != null) {
+				customSlider.Dispose ();
+				customSlider = null;
+			}
+		}
+	}
 }
