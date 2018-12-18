7d01a1b3624be8b24386b16af2d69bae638fafbc
Mykyta Bondarenko <mykyta_bondarenko@HRK1-DMC-F49706.local>
2018-12-18 11:17:32 +0200

    added new project

--- ios8/UICatalog/UICatalog/AppDelegate.cs
+++ ios8/UICatalog/UICatalog/AppDelegate.cs
@@ -1,25 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿using Foundation;
 using UIKit;
 
 namespace UICatalog
 {
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate
-	{
-		public override UIWindow Window { get; set; }
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        public override UIWindow Window { get; set; }
 
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-			UISplitViewController splitViewController = (UISplitViewController)Window.RootViewController;
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            // Override point for customization after application launch.
+            // If not required for your application you can safely delete this method
 
-			splitViewController.PreferredDisplayMode = UISplitViewControllerDisplayMode.AllVisible;
-
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 }
-
