945f5a275175f64d7f5162bd91d516a4f5494608
Mykyta Bondarenko <mykyta_bondarenko@HRK1-DMC-F49706.local>
2018-12-18 18:37:45 +0200

    complete StepperViewController

--- ios8/UICatalog/UICatalog/StepperViewController.cs
+++ ios8/UICatalog/UICatalog/StepperViewController.cs
@@ -6,8 +6,63 @@ namespace UICatalog
 {
     public partial class StepperViewController : UITableViewController
     {
-        public StepperViewController (IntPtr handle) : base (handle)
+        public StepperViewController (IntPtr handle) : base (handle) { }
+
+        public override void ViewDidLoad()
         {
+            base.ViewDidLoad();
+            ConfigureCustomStepper();
+
+            UpdateDefaultLabel();
+            UpdateTintedLabel();
+            UpdateCustomLabel();
+        }
+
+        private void ConfigureCustomStepper()
+        {
+            // Set the background image.
+            customStepper.SetBackgroundImage(UIImage.FromBundle("stepper_and_segment_background"), UIControlState.Normal);
+            customStepper.SetBackgroundImage(UIImage.FromBundle("stepper_and_segment_background_highlighted"), UIControlState.Highlighted);
+            customStepper.SetBackgroundImage(UIImage.FromBundle("stepper_and_segment_background_disabled"), UIControlState.Disabled);
+
+            // Set the image which will be painted in between the two stepper segments (depends on the states of both segments).
+            customStepper.SetDividerImage(UIImage.FromBundle("stepper_and_segment_divider"), UIControlState.Normal, UIControlState.Normal);
+
+            // Set the image for the + button.
+            customStepper.SetIncrementImage(UIImage.FromBundle("stepper_increment"), UIControlState.Normal);
+
+            // Set the image for the - button.
+            customStepper.SetDecrementImage(UIImage.FromBundle("stepper_decrement"), UIControlState.Normal);
+        }
+
+        partial void CustomValueChanged(NSObject sender)
+        {
+            UpdateCustomLabel();
+        }
+
+        partial void DefaultValueChanged(NSObject sender)
+        {
+            UpdateDefaultLabel();
+        }
+
+        partial void TintedValueChanged(NSObject sender)
+        {
+            UpdateTintedLabel();
+        }
+
+        private void UpdateDefaultLabel()
+        {
+            defaultLabel.Text = defaultStepper.Value.ToString();
+        }
+
+        private void UpdateTintedLabel()
+        {
+            tintedLabel.Text = tintedStepper.Value.ToString();
+        }
+
+        private void UpdateCustomLabel()
+        {
+            customLabel.Text = customStepper.Value.ToString();
         }
     }
 }
--- ios8/UICatalog/UICatalog/StepperViewController.designer.cs
+++ ios8/UICatalog/UICatalog/StepperViewController.designer.cs
@@ -1,21 +1,75 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace UICatalog
 {
-    [Register ("StepperViewController")]
-    partial class StepperViewController
-    {
-        void ReleaseDesignerOutlets ()
-        {
-        }
-    }
+	[Register ("StepperViewController")]
+	partial class StepperViewController
+	{
+		[Outlet]
+		UIKit.UILabel customLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIStepper customStepper { get; set; }
+
+		[Outlet]
+		UIKit.UILabel defaultLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIStepper defaultStepper { get; set; }
+
+		[Outlet]
+		UIKit.UILabel tintedLabel { get; set; }
+
+		[Outlet]
+		UIKit.UIStepper tintedStepper { get; set; }
+
+		[Action ("CustomValueChanged:")]
+		partial void CustomValueChanged (Foundation.NSObject sender);
+
+		[Action ("DefaultValueChanged:")]
+		partial void DefaultValueChanged (Foundation.NSObject sender);
+
+		[Action ("TintedValueChanged:")]
+		partial void TintedValueChanged (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (customStepper != null) {
+				customStepper.Dispose ();
+				customStepper = null;
+			}
+
+			if (defaultStepper != null) {
+				defaultStepper.Dispose ();
+				defaultStepper = null;
+			}
+
+			if (tintedStepper != null) {
+				tintedStepper.Dispose ();
+				tintedStepper = null;
+			}
+
+			if (defaultLabel != null) {
+				defaultLabel.Dispose ();
+				defaultLabel = null;
+			}
+
+			if (tintedLabel != null) {
+				tintedLabel.Dispose ();
+				tintedLabel = null;
+			}
+
+			if (customLabel != null) {
+				customLabel.Dispose ();
+				customLabel = null;
+			}
+		}
+	}
 }
