eab633b5a214f09bf8dbb985a97b470663ff4301
Mykyta Bondarenko <mykyta_bondarenko@HRK1-DMC-F49706.local>
2018-12-18 17:56:21 +0200

    complete ProgressViewController

--- ios8/UICatalog/UICatalog/ProgressViewController.designer.cs
+++ ios8/UICatalog/UICatalog/ProgressViewController.designer.cs
@@ -1,21 +1,42 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace UICatalog
 {
-    [Register ("ProgressViewController")]
-    partial class ProgressViewController
-    {
-        void ReleaseDesignerOutlets ()
-        {
-        }
-    }
+	[Register ("ProgressViewController")]
+	partial class ProgressViewController
+	{
+		[Outlet]
+		UIKit.UIProgressView barProgressView { get; set; }
+
+		[Outlet]
+		UIKit.UIProgressView defaultProgressView { get; set; }
+
+		[Outlet]
+		UIKit.UIProgressView tintedProgressView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (defaultProgressView != null) {
+				defaultProgressView.Dispose ();
+				defaultProgressView = null;
+			}
+
+			if (barProgressView != null) {
+				barProgressView.Dispose ();
+				barProgressView = null;
+			}
+
+			if (tintedProgressView != null) {
+				tintedProgressView.Dispose ();
+				tintedProgressView = null;
+			}
+		}
+	}
 }
