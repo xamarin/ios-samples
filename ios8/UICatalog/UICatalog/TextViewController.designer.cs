6ca9f7e05be13609a0faba250425e24563d60870
Mykyta Bondarenko <mykyta_bondarenko@HRK1-DMC-F49706.local>
2018-12-19 15:08:37 +0200

    complete TextViewController

--- ios8/UICatalog/UICatalog/TextViewController.designer.cs
+++ ios8/UICatalog/UICatalog/TextViewController.designer.cs
@@ -1,21 +1,34 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace UICatalog
 {
-    [Register ("TextViewController")]
-    partial class TextViewController
-    {
-        void ReleaseDesignerOutlets ()
-        {
-        }
-    }
+	[Register ("TextViewController")]
+	partial class TextViewController
+	{
+		[Outlet]
+		UIKit.NSLayoutConstraint textBottomConstraint { get; set; }
+
+		[Outlet]
+		UIKit.UITextView textView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (textView != null) {
+				textView.Dispose ();
+				textView = null;
+			}
+
+			if (textBottomConstraint != null) {
+				textBottomConstraint.Dispose ();
+				textBottomConstraint = null;
+			}
+		}
+	}
 }
