0b54c06b58efbce72723561b4910947ba152e7f1
Mykyta Bondarenko <mykyta_bondarenko@HRK1-DMC-F49706.local>
2018-12-19 18:55:43 +0200

    complete search controllers

---
+++ ios8/UICatalog/UICatalog/Controllers/Search/SearchControllers/SearchPresentOverNavigationBarViewController.cs
@@ -0,0 +1,22 @@
+using System;
+using Foundation;
+using UIKit;
+
+namespace UICatalog
+{
+	public partial class SearchPresentOverNavigationBarViewController : BaseSearchController
+    {
+        public SearchPresentOverNavigationBarViewController(IntPtr handle) : base(handle) { }
+
+        partial void SearchButtonClicked(NSObject sender)
+        {
+            // Create the search controller and make it perform the results updating.
+            var searchController = new UISearchController((UIViewController)null);
+            searchController.SetSearchResultsUpdater(UpdateSearchResultsForSearchController);
+            searchController.HidesNavigationBarDuringPresentation = false;
+
+            // Present the view controller.
+            PresentViewController(searchController, true, null);
+        }
+    }
+}
