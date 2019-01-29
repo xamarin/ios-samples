0b4bbb25ac3e1adadcda477c34c46bdfe6775164
Mykyta Bondarenko <32954580+mykyta-bondarenko-gl@users.noreply.github.com>
2019-01-29 18:57:10 +0200

    Added new sample `NavigationBar` (#354)

---
+++ NavigationBar/NavigationBar/CustomRightViewController.cs
@@ -0,0 +1,71 @@
+using CoreGraphics;
+using Foundation;
+using System;
+using UIKit;
+
+namespace NavigationBar
+{
+    /// <summary>
+    /// Demonstrates configuring various types of controls as the right bar item of the navigation bar.
+    /// </summary>
+	public partial class CustomRightViewController : UIViewController
+	{
+		public CustomRightViewController (IntPtr handle) : base (handle) { }
+
+        public override UIInterfaceOrientationMask GetSupportedInterfaceOrientations()
+        {
+            return UIInterfaceOrientationMask.Portrait;
+        }
+
+        /// <summary>
+        /// Action for the segemented control.
+        /// </summary>
+        partial void ChangeRightBarItem(UISegmentedControl sender)
+        {
+            switch (sender.SelectedSegment)
+            {
+                case 0:
+                    // Add a custom add button as the nav bar's custom right view
+                    var addButton = new UIBarButtonItem(NSBundle.MainBundle.GetLocalizedString("AddTitle"),
+                                                        UIBarButtonItemStyle.Plain,
+                                                        this.Action);
+
+                    base.NavigationItem.RightBarButtonItem = addButton;
+                    break;
+
+                case 1:
+                    // Add a custom add button as the nav bar's custom right view
+                    var emailButton = new UIBarButtonItem(UIImage.FromBundle("Email"),
+                                                          UIBarButtonItemStyle.Plain,
+                                                          this.Action);
+
+                    base.NavigationItem.RightBarButtonItem = emailButton;
+                    break;
+
+                case 2:
+                    // "Segmented" control to the right
+                    var segmentedControl = new UISegmentedControl(new UIImage[] { UIImage.FromBundle("UpArrow"), UIImage.FromBundle("DownArrow") });
+
+                    segmentedControl.AddTarget(this.Action, UIControlEvent.ValueChanged);
+                    segmentedControl.Frame = new CGRect(0f, 0f, 90f, 30f);
+                    segmentedControl.Momentary = true;
+
+                    // Add a custom add button as the nav bar's custom right view
+                    var segmentBarItem = new UIBarButtonItem(segmentedControl);
+
+                    base.NavigationItem.RightBarButtonItem = segmentBarItem;
+                    break;
+            }
+        }
+
+        private void Action(object sender, EventArgs e)
+        {
+            Console.WriteLine("Custom Action was invoked");
+        }
+
+        partial void RightAction(UIBarButtonItem sender)
+        {
+            Console.WriteLine("RightAction was invoked");
+        }
+    }
+}
---
+++ NavigationBar/NavigationBar/LargeTitleViewController.cs
@@ -0,0 +1,41 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace NavigationBar
+{
+    /// <summary>
+    /// Demonstrates applying a large title to the UINavigationBar.
+    /// </summary>
+    public partial class LargeTitleViewController : UITableViewController
+    {
+        // Our data source is an array of city names, populated from Cities.json.
+        private CitiesDataSource dataSource = new CitiesDataSource();
+
+        public LargeTitleViewController(IntPtr handle) : base(handle) { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            base.TableView.DataSource = this.dataSource;
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(11, 0))
+            {
+                base.NavigationController.NavigationBar.PrefersLargeTitles = true;
+            }
+        }
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+        {
+            if (segue.Identifier == "pushSeque")
+            {
+                // This segue is pushing a detailed view controller.
+                segue.DestinationViewController.Title = dataSource[base.TableView.IndexPathForSelectedRow.Row];
+                if (UIDevice.CurrentDevice.CheckSystemVersion(11, 0))
+                {
+                    segue.DestinationViewController.NavigationItem.LargeTitleDisplayMode = UINavigationItemLargeTitleDisplayMode.Never;
+                }
+            }
+        }
+    }
+}
