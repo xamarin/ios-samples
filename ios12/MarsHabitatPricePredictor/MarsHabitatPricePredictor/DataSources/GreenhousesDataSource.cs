bfdd59ce271ea2c4bc99c2447ad93970fb49fa31
Mykyta Bondarenko <32954580+mykyta-bondarenko-gl@users.noreply.github.com>
2019-01-29 19:00:17 +0200

    Added new sample `MarsHabitatPricePredictor` (#358)

---
+++ ios12/MarsHabitatPricePredictor/MarsHabitatPricePredictor/DataSources/GreenhousesDataSource.cs
@@ -0,0 +1,36 @@
+﻿
+namespace MarsHabitatPricePredictor.DataSources
+{
+    /// <summary>
+    /// Data source for the number of greenhouses.
+    /// </summary>
+    public class GreenhousesDataSource
+    {
+        /// <summary>
+        /// Possible values for greenhouses in the habitat
+        /// </summary>
+        public double[] Values { get; } = { 1, 2, 3, 4, 5 };
+
+        public string Title(int index)
+        {
+            string result = null;
+            if (index < this.Values.Length)
+            {
+                result = this.Values[index].ToString();
+            }
+
+            return result;
+        }
+
+        public double? Value(int index)
+        {
+            double? result = null;
+            if (index < this.Values.Length)
+            {
+                result = this.Values[index];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ios12/MarsHabitatPricePredictor/MarsHabitatPricePredictor/ViewController.designer.cs
@@ -0,0 +1,37 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace MarsHabitatPricePredictor
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIPickerView pickerView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel priceLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (pickerView != null) {
+                pickerView.Dispose ();
+                pickerView = null;
+            }
+
+            if (priceLabel != null) {
+                priceLabel.Dispose ();
+                priceLabel = null;
+            }
+        }
+    }
+}
