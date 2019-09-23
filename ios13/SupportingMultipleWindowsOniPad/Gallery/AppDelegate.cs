cbe43e8c52287787e53375b25d66cebc3137d13f
SotoiGhost <israel.spgh@gmail.com>
2019-09-23 10:56:51 -0500

    [iOS13] Ported SupportingMultipleWindowsOniPad sample

---
+++ ios13/SupportingMultipleWindowsOniPad/Gallery/AppDelegate.cs
@@ -0,0 +1,30 @@
+﻿/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A basic app delegate implementation.
+ Demonstrates how to return a particular UISceneConfiguration for a new scene session.
+*/
+
+using Foundation;
+using UIKit;
+
+namespace Gallery {
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+	[Register (nameof (AppDelegate))]
+	public class AppDelegate : UIApplicationDelegate {
+		// class-level declarations
+		public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+		{
+			// Override point for customization after application launch.
+			// If not required for your application you can safely delete this method
+
+			return true;
+		}
+
+		public override UISceneConfiguration GetConfiguration (UIApplication application, UISceneSession connectingSceneSession, UISceneConnectionOptions options)
+			=> new UISceneConfiguration ("Default Configuration", connectingSceneSession.Role);
+	}
+}
+
---
+++ ios13/SupportingMultipleWindowsOniPad/Gallery/View Controllers/PhotoDetailViewController/PhotoDetailViewController.designer.cs
@@ -0,0 +1,26 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace Gallery
+{
+	[Register ("PhotoDetailViewController")]
+	partial class PhotoDetailViewController
+	{
+		[Outlet]
+		UIKit.UIImageView imageView { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (imageView != null) {
+				imageView.Dispose ();
+				imageView = null;
+			}
+		}
+	}
+}
