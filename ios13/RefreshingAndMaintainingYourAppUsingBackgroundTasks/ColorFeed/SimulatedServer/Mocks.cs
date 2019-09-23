d777d42438d2f6d68173c1d707d0e5bea2fd7ddc
SotoiGhost <israel.spgh@gmail.com>
2019-09-23 10:56:25 -0500

    [iOS13] Ported RefreshingAndMaintainingYourAppUsingBackgroundTasks sample

---
+++ ios13/RefreshingAndMaintainingYourAppUsingBackgroundTasks/ColorFeed/SimulatedServer/Mocks.cs
@@ -0,0 +1,69 @@
+﻿/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+A set of extensions, a class, and a function that simulate data coming from a server.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Foundation;
+
+namespace ColorFeed {
+	// Simulates a remote server by generating randomized ServerEntry results
+	public class MockServer : IServer {
+		public event EventHandler<ServerSuccessEventArgs> Success;
+		public event EventHandler Cancelled;
+
+		private class MockDownloadTask : IDownloadTask {
+			TimeSpan delay;
+			DateTime startDate;
+			Action<Post []> success;
+			Action cancelled;
+
+			public CancellationToken DownloadToken { get; private set; }
+
+			public MockDownloadTask (TimeSpan delay, DateTime startDate, Action<Post []> success, Action cancelled, CancellationToken cancellationToken)
+			{
+				this.delay = delay;
+				this.startDate = startDate;
+				this.success = success;
+				this.cancelled = cancelled;
+				DownloadToken = cancellationToken;
+			}
+
+			public void Start ()
+			{	
+				Task.Factory.StartNew (async () => {
+					await Task.Delay (delay);
+					var posts = await ServerUtils.GenerateFakePosts (startDate, DateTime.Now);
+					if (!DownloadToken.IsCancellationRequested)
+						success?.Invoke (posts);
+				}, DownloadToken);
+			}
+
+			public void Cancel ()
+			{
+				cancelled?.Invoke ();
+			}
+		}
+
+		public IDownloadTask GetFetchEntriesSinceTask (DateTime startDate, CancellationToken cancellationToken)
+		{
+			var random = new Random (DateTime.Now.Millisecond);
+			var delay = random.Next (0, 26) / 10d;
+			var downloadTask = new MockDownloadTask (TimeSpan.FromSeconds (delay), startDate, posts => {
+				Success?.Invoke (this, new ServerSuccessEventArgs (posts));
+			}, () => {
+				Cancelled?.Invoke (this, new EventArgs ());
+			}, cancellationToken);
+
+			return downloadTask;
+		}
+	}
+
+	public class DownloadCancelledException : Exception { }
+}
---
+++ ios13/RefreshingAndMaintainingYourAppUsingBackgroundTasks/ColorFeed/UI/FeedEntryTableViewCell.cs
@@ -0,0 +1,25 @@
+using System;
+
+using Foundation;
+using UIKit;
+
+namespace ColorFeed
+{
+	public partial class FeedEntryTableViewCell : UITableViewCell
+	{
+		Post post;
+		public Post Post {
+			get => post;
+			set {
+				post = value;
+				UpdateCell ();
+			}
+		}
+
+		public FeedEntryTableViewCell (IntPtr handle) : base (handle)
+		{
+		}
+
+		void UpdateCell () => colorView.Post = Post;
+	}
+}
