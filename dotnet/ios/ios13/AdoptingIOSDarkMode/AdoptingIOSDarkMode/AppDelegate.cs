b72710aaedda474b8117ab6634207d4e1aa71498
TJ Lambert <tjlambert@microsoft.com>
2022-01-03 14:49:31 -0600

    [Dotnet] Porting over AdoptingIOSDarkMode to dotnet

---
+++ dotnet/ios/ios13/AdoptingIOSDarkMode/AdoptingIOSDarkMode/AppDelegate.cs
@@ -0,0 +1,15 @@
+namespace AdoptingIOSDarkMode;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+	{
+		return true;
+	}
+
+}
