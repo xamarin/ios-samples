ce60781b7960066a57f1c54932c1420777db197d
TJ Lambert <tjlambert@microsoft.com>
2022-01-11 12:51:02 -0600

    Initial working port

---
+++ dotnet/ios/ios12/XamarinNL/XamarinNL/LanguageTaggerTableViewController.cs
@@ -0,0 +1,48 @@
+using Foundation;
+using NaturalLanguage;
+using System;
+using UIKit;
+
+namespace XamarinNL
+{
+    public partial class LanguageTaggerTableViewController : UITableViewController
+    {
+        const string EntityCell = "EntityCell";
+
+        public NSValue[] TokenRanges { get; set; }
+        public NSString[] Tags { get; set; }
+        public string Text { get; set; }
+
+        public LanguageTaggerTableViewController(IntPtr handle) : base(handle) { }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return Tags.Length;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = TableView.DequeueReusableCell(EntityCell);
+
+            var range = TokenRanges[indexPath.Row].RangeValue;
+            var token = Text.Substring((int)range.Location, (int)range.Length);
+            var tag = Tags[indexPath.Row];
+
+            cell.TextLabel.Text = token;
+            cell.DetailTextLabel.Text = tag;
+
+            return cell;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+            TableView.ReloadData();
+        }
+    }
+}
---
+++ dotnet/ios/ios12/XamarinNL/XamarinNL/LanguageTaggerViewController.cs
@@ -0,0 +1,70 @@
+using Foundation;
+using NaturalLanguage;
+using System;
+using UIKit;
+
+namespace XamarinNL
+{
+    public partial class LanguageTaggerViewController : UIViewController, IUITextFieldDelegate
+    {
+        const string ShowEntitiesSegue = "ShowEntitiesSegue";
+
+        NSString[] tags;
+        NSValue[] tokenRanges;
+        string detailViewTitle;
+
+        public LanguageTaggerViewController(IntPtr handle) : base(handle) { }
+
+        partial void HandlePartsOfSpeechButtonTap(UIButton sender)
+        {
+            ShowTags(NLTagScheme.LexicalClass); 
+        }
+
+        partial void HandleNamedEntitiesButtonTap(UIButton sender)
+        {
+            ShowTags(NLTagScheme.NameType);
+        }
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+        {
+            base.PrepareForSegue(segue, sender);
+            var destination = segue.DestinationViewController as LanguageTaggerTableViewController;
+            if (destination != null)
+            {
+                destination.Text = UserInput.Text;
+                destination.Tags = tags;
+                destination.TokenRanges = tokenRanges;
+                destination.Title = detailViewTitle;
+            }
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            UserInput.Delegate = this;
+        }
+
+        [Export("textFieldShouldReturn:")]
+        public bool ShouldReturn(UITextField textField)
+        {
+            UserInput.ResignFirstResponder();
+            return true;
+        }
+
+        void ShowTags(NLTagScheme tagScheme)
+        {
+            if (!String.IsNullOrWhiteSpace(UserInput.Text))
+            {
+                var tagger = new NLTagger(new NLTagScheme[] { tagScheme });
+                var range = new NSRange(0, UserInput.Text.Length);
+                tagger.String = UserInput.Text;
+
+                tags = tagger.GetTags(range, NLTokenUnit.Word, tagScheme, NLTaggerOptions.OmitWhitespace, out NSValue[] ranges);
+                tokenRanges = ranges;
+                detailViewTitle = tagScheme == NLTagScheme.NameType ? "Named Entities" : "Parts of Speech";
+
+                PerformSegue(ShowEntitiesSegue, this);
+            }
+        }
+    }
+}
