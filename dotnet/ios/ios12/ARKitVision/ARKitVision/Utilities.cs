4196d7226fe47f9e625cff113ea2ea3a03a6b4cf
Steve Hawley <sthawl@microsoft.com>
2022-01-12 10:32:12 -0500

    indents

--- dotnet/ios/ios12/ARKitVision/ARKitVision/Utilities.cs
+++ dotnet/ios/ios12/ARKitVision/ARKitVision/Utilities.cs
@@ -2,66 +2,66 @@
 
 public static class ARTrackingStateExtensions
 {
-public static string GetPresentationString(this ARCamera self)
-{
-    string result = null;
+        public static string GetPresentationString (this ARCamera self)
+        {
+                string result = null;
 
-    switch (self.TrackingState)
-    {
-        case ARTrackingState.NotAvailable:
-            result = "TRACKING UNAVAILABLE";
-            break;
+                switch (self.TrackingState)
+                {
+                        case ARTrackingState.NotAvailable:
+                                result = "TRACKING UNAVAILABLE";
+                                break;
 
-        case ARTrackingState.Normal:
-            result = "TRACKING NORMAL";
-            break;
+                        case ARTrackingState.Normal:
+                                result = "TRACKING NORMAL";
+                                break;
 
-        case ARTrackingState.Limited:
-            switch (self.TrackingStateReason)
-            {
-                case ARTrackingStateReason.ExcessiveMotion:
-                    result = "TRACKING LIMITED\nExcessive motion";
-                    break;
-                case ARTrackingStateReason.InsufficientFeatures:
-                    result = "TRACKING LIMITED\nLow detail";
-                    break;
-                case ARTrackingStateReason.Initializing:
-                    result = "Initializing";
-                    break;
-                case ARTrackingStateReason.Relocalizing:
-                    result = "Recovering from interruption";
-                    break;
-            }
-            break;
-    }
+                        case ARTrackingState.Limited:
+                                switch (self.TrackingStateReason)
+                                {
+                                        case ARTrackingStateReason.ExcessiveMotion:
+                                                result = "TRACKING LIMITED\nExcessive motion";
+                                                break;
+                                        case ARTrackingStateReason.InsufficientFeatures:
+                                                result = "TRACKING LIMITED\nLow detail";
+                                                break;
+                                        case ARTrackingStateReason.Initializing:
+                                                result = "Initializing";
+                                                break;
+                                        case ARTrackingStateReason.Relocalizing:
+                                                result = "Recovering from interruption";
+                                                break;
+                                }
+                                break;
+                }
 
-    return result;
-}
+                return result;
+        }
 
-public static string GetRecommendation(this ARCamera self)
-{
-    string result = null;
+        public static string GetRecommendation (this ARCamera self)
+        {
+                string result = null;
 
-    switch (self.TrackingState)
-    {
-        case ARTrackingState.Limited:
-            switch (self.TrackingStateReason)
-            {
-                case ARTrackingStateReason.ExcessiveMotion:
-                    result = "Try slowing down your movement, or reset the session.";
-                    break;
-                case ARTrackingStateReason.InsufficientFeatures:
-                    result = "Try pointing at a flat surface, or reset the session.";
-                    break;
-                case ARTrackingStateReason.Relocalizing:
-                    result = "Return to the location where you left off or try resetting the session.";
-                    break;
-            }
-            break;
-    }
+                switch (self.TrackingState)
+                {
+                        case ARTrackingState.Limited:
+                                switch (self.TrackingStateReason)
+                                {
+                                        case ARTrackingStateReason.ExcessiveMotion:
+                                                result = "Try slowing down your movement, or reset the session.";
+                                                break;
+                                        case ARTrackingStateReason.InsufficientFeatures:
+                                                result = "Try pointing at a flat surface, or reset the session.";
+                                                break;
+                                        case ARTrackingStateReason.Relocalizing:
+                                                result = "Return to the location where you left off or try resetting the session.";
+                                                break;
+                                }
+                                break;
+                }
 
-    return result;
-}
+                return result;
+        }
 }
 
 /// <summary>
