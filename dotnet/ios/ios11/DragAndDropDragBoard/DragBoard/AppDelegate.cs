93fac96d2da2aac7c3f0ea60e2d067a8a9004cb6
TJ Lambert <tjlambert@microsoft.com>
2022-01-12 09:24:14 -0600

    Working port

---
+++ dotnet/ios/ios11/DragAndDropDragBoard/DragBoard/AppDelegate.cs
@@ -0,0 +1,14 @@
+namespace DragBoard;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	public override bool FinishedLaunching (UIApplication application, NSDictionary launchOptions)
+	{
+		return true;
+	}
+}
---
+++ dotnet/ios/ios11/DragAndDropDragBoard/DragBoard/DragBoardView.cs
@@ -0,0 +1,27 @@
+using System;
+using Foundation;
+using UIKit;
+
+namespace DragBoard
+{
+	/// <summary>
+	/// A custom view that can handle paste events.
+	/// </summary>
+	public partial class DragBoardView : UIView
+	{
+        public DragBoardView(IntPtr handle) : base(handle)
+		{
+		}
+		
+        public override void AwakeFromNib()
+		{
+			base.AwakeFromNib();
+			BackgroundColor = UIColor.FromPatternImage(UIImage.FromBundle("Cork"));
+		}
+
+		public override bool CanBecomeFirstResponder
+		{
+			get { return true; }
+		}
+	}
+}
