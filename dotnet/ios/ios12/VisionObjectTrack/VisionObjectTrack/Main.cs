5996798136800116704ad98d2f8ac99b33165e1c
Steve Hawley <sthawl@microsoft.com>
2022-01-13 14:48:46 -0500

    Update dotnet/ios/ios12/VisionObjectTrack/VisionObjectTrack/Main.cs
    Co-authored-by: Rolf Bjarne Kvinge <rolf@xamarin.com>

--- dotnet/ios/ios12/VisionObjectTrack/VisionObjectTrack/Main.cs
+++ dotnet/ios/ios12/VisionObjectTrack/VisionObjectTrack/Main.cs
@@ -17,7 +17,7 @@ namespace VisionObjectTrack
                 {
                         // if you want to use a different Application Delegate class from "AppDelegate"
                         // you can specify it here.
-                        UIApplication.Main (args, null, "AppDelegate");
+                        UIApplication.Main (args, null, typeof (AppDelegate));
                 }
         }
 }
