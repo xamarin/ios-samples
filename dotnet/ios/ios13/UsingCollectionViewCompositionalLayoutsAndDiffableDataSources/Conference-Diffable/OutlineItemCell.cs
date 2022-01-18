ee7b83fd87ec427728874369f311a800e777dc58
TJ Lambert <tjlambert@microsoft.com>
2022-01-18 10:12:30 -0600

    Adding Global Usings

--- dotnet/ios/ios13/UsingCollectionViewCompositionalLayoutsAndDiffableDataSources/Conference-Diffable/OutlineItemCell.cs
+++ dotnet/ios/ios13/UsingCollectionViewCompositionalLayoutsAndDiffableDataSources/Conference-Diffable/OutlineItemCell.cs
@@ -4,11 +4,6 @@ See LICENSE folder for this sample’s licensing information.
 Abstract:
 A cell for displaying an item in our outline view
 */
-
-global using static Conference_Diffable.AppDelegate;
-global using Conference_Diffable.CompositionalLayout.CellsandSupplementaryViews;
-global using ObjCRuntime;
-
 namespace Conference_Diffable;
 
 public partial class OutlineItemCell : UICollectionViewCell {
