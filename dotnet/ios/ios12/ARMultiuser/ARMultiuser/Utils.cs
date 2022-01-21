eaf0c5b8d5ceb5bb41b9ccab0e7289b683812808
Steve Hawley <sthawl@microsoft.com>
2022-01-21 14:31:23 -0500

    port of ARMultiuser

---
+++ dotnet/ios/ios12/ARMultiuser/ARMultiuser/Utils.cs
@@ -0,0 +1,30 @@
+﻿namespace ARMultiuser;
+
+public static class ARWorldMappingStatusExtensions
+{
+        public static string? GetDescription (this ARWorldMappingStatus status)
+        {
+                string? result = null;
+
+                switch (status)
+                {
+                        case ARWorldMappingStatus.NotAvailable:
+                                result = "Not Available";
+                                break;
+
+                        case ARWorldMappingStatus.Limited:
+                                result = "Limited";
+                                break;
+
+                        case ARWorldMappingStatus.Extending:
+                                result = "Extending";
+                                break;
+
+                        case ARWorldMappingStatus.Mapped:
+                                result = "Mapped";
+                                break;
+                }
+
+                return result;
+        }
+}
