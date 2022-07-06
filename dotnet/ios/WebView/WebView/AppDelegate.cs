ae5bcf27be024af44597a152e228ede712c3937f
test <test-user@tests-MacBook-Pro.local>
2022-07-06 11:01:07 -0700

    New WebView Sample containing porting legacy samples guidelines

---
+++ dotnet/ios/WebView/WebView/AppDelegate.cs
@@ -0,0 +1,11 @@
+namespace WebView;
+
+[Register ("AppDelegate")]
+public class AppDelegate : UIApplicationDelegate {
+	public override UIWindow? Window {
+		get;
+		set;
+	}
+
+	
+}
---
+++ dotnet/ios/WebView/WebView/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using SafariServices;
+using WebKit;
+
+namespace WebView;
+
+public partial class ViewController : UIViewController
+{
+    private readonly NSUrl url = new("https://visualstudio.microsoft.com/xamarin/");
+
+    protected ViewController(IntPtr handle) : base(handle) { }
+
+    partial void OpenWebView(UIButton sender)
+    {
+        
+        if (View == null)
+        {
+            throw new NullReferenceException();
+        }
+        
+        var webView = new WKWebView(View.Frame, new WKWebViewConfiguration());
+        View.AddSubview(webView);
+       
+        webView.LoadRequest(new NSUrlRequest(url));
+    }
+
+    partial void OpenSafari(UIButton sender)
+    {
+        UIApplication.SharedApplication.OpenUrl(url);
+    }
+
+    partial void OpenSafariViewController(UIButton sender)
+    {
+        var viewController = new SFSafariViewController(url);
+        PresentViewController(viewController, true, null);
+    }
+}
