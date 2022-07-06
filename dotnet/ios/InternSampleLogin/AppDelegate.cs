5a8225c3bb3ae73901a72e431ef1b0a5d22e33cc
Admin <admin@Admins-MacBook-Pro.local>
2022-07-06 16:14:17 -0700

    Here is the SimpleLogin sample ported from Xamarin Forms to MAUI/dotnet6

---
+++ dotnet/ios/InternSampleLogin/AppDelegate.cs
@@ -0,0 +1,26 @@
+using CodeOnlyDemo;
+
+namespace InternSampleLogin;
+
+[Register ("AppDelegate")]
+public partial class AppDelegate : UIApplicationDelegate
+{
+    // class-level declarations
+    UIWindow? window;
+
+    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    {
+        // create a new window instance based on the screen size
+        window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+        var controller = new CustomViewController();
+
+        window.RootViewController = controller;
+
+        // make the window visible
+        window.MakeKeyAndVisible();
+
+        return true;
+    }
+}
+
---
+++ dotnet/ios/InternSampleLogin/README.md
@@ -0,0 +1,13 @@
+---
+name: Xamarin.iOS - SimpleLogin
+description: This sample is the application that will be built at the end of the Creating iOS Applications in Code Using Visual Studio doc.
+page_type: sample
+languages:
+- csharp
+products:
+- xamarin
+urlFragment: simplelogin
+---
+# SimpleLogin
+
+This sample is the application that will be built at the end of the [Creating iOS Applications in Code Using Visual Studio](https://docs.microsoft.com/xamarin/ios/app-fundamentals/ios-code-only) doc.
