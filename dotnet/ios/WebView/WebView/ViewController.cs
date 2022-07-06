b208cca03e517d5fc0f3353de0bea6f8257470a1
test <test-user@tests-MacBook-Pro.local>
2022-07-06 14:04:39 -0700

    Changes in ViewController.cs

--- dotnet/ios/WebView/WebView/ViewController.cs
+++ dotnet/ios/WebView/WebView/ViewController.cs
@@ -5,28 +5,29 @@ namespace WebView;
 
 public partial class ViewController : UIViewController
 {
-    private readonly NSUrl url = new("https://visualstudio.microsoft.com/xamarin/");
+    private readonly NSUrl url = new ("https://visualstudio.microsoft.com/xamarin/");
 
-    protected ViewController(IntPtr handle) : base(handle) { }
+    protected ViewController (IntPtr handle) : base (handle) { }
 
     partial void OpenWebView(UIButton sender)
     {
-        if (View == null) throw new NullReferenceException();
+        if (View is null)
+            throw new NullReferenceException ();
         
-        var webView = new WKWebView(View.Frame, new WKWebViewConfiguration());
-        View.AddSubview(webView);
+        var webView = new WKWebView (View.Frame, new WKWebViewConfiguration());
+        View.AddSubview (webView);
        
-        webView.LoadRequest(new NSUrlRequest(url));
+        webView.LoadRequest (new NSUrlRequest (url));
     }
 
-    partial void OpenSafari(UIButton sender)
+    partial void OpenSafari (UIButton sender)
     {
-        UIApplication.SharedApplication.OpenUrl(url);
+        UIApplication.SharedApplication.OpenUrl (url);
     }
 
-    partial void OpenSafariViewController(UIButton sender)
+    partial void OpenSafariViewController (UIButton sender)
     {
-        var viewController = new SFSafariViewController(url);
-        PresentViewController(viewController, true, null);
+        var viewController = new SFSafariViewController (url);
+        PresentViewController (viewController, true, null);
     }
 }
