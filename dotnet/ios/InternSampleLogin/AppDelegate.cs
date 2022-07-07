6a3c3501a952b33f6e104137b53867a80fb07054
Leandra Hall <90208577+LeandraHall@users.noreply.github.com>
2022-07-07 14:19:00 -0700

    Update dotnet/ios/InternSampleLogin/AppDelegate.cs
    Co-authored-by: TJ Lambert <50846373+tj-devel709@users.noreply.github.com>

--- dotnet/ios/InternSampleLogin/AppDelegate.cs
+++ dotnet/ios/InternSampleLogin/AppDelegate.cs
@@ -8,7 +8,7 @@ public partial class AppDelegate : UIApplicationDelegate
     // class-level declarations
     UIWindow? window;
 
-    public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+    public override bool FinishedLaunching (UIApplication app, NSDictionary options)
     {
         // create a new window instance based on the screen size
         window = new UIWindow (UIScreen.MainScreen.Bounds);
