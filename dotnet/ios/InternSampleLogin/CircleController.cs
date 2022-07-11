88cbbfb952b6a214fd102cf4103ef67cdd36bbdc
Leandra Hall <90208577+LeandraHall@users.noreply.github.com>
2022-07-11 14:31:02 -0700

    The SimpleLogin sample ported from Xamarin Forms to MAUI/dotnet6 (#461)

---
+++ dotnet/ios/InternSampleLogin/CircleController.cs
@@ -0,0 +1,12 @@
+namespace CodeOnlyDemo;
+
+class CircleController : UIViewController
+{
+	CircleView? view;
+
+	public override void LoadView ()
+	{
+		view = new CircleView ();
+		View = view;
+	}
+}
