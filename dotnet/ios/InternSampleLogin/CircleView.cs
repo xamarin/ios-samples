447fdb20997d294e27c504762f2cfa1a08b609b9
Leandra Hall <90208577+LeandraHall@users.noreply.github.com>
2022-07-11 10:23:05 -0700

    Update CircleView.cs

--- dotnet/ios/InternSampleLogin/CircleView.cs
+++ dotnet/ios/InternSampleLogin/CircleView.cs
@@ -4,23 +4,23 @@ class CircleView : UIView
 {
 	public override void Draw (CGRect rect)
 	{
-		base.Draw(rect);
+		base.Draw (rect);
 
 		//get graphics context
 		using var g = UIGraphics.GetCurrentContext ()
 		{
 			// set up drawing attributes
-			g.SetLineWidth(10.0f);
-			UIColor.Green.SetFill();
-			UIColor.Blue.SetStroke();
+			g.SetLineWidth (10.0f);
+			UIColor.Green.SetFill ();
+			UIColor.Blue.SetStroke ();
 
 			// create geometry
-			var path = new CGPath();
-			path.AddArc(Bounds.GetMidX(), Bounds.GetMidY(), 50f, 0, 2.0f * (float)Math.PI, true);
+			var path = new CGPath ();
+			path.AddArc (Bounds.GetMidX (), Bounds.GetMidY (), 50f, 0, 2.0f * (float) Math.PI, true);
 
 			// add geometry to graphics context and draw
-			g.AddPath(path);
-			g.DrawPath(CGPathDrawingMode.FillStroke);
+			g.AddPath (path);
+			g.DrawPath (CGPathDrawingMode.FillStroke);
 		}
 	}
 
